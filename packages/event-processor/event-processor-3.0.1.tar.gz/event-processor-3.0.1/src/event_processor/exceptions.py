a811dc7e15ae44990f31b8485fe831bbfb095d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:04 +0000

    Add package event-processor/3.0.1/event-processor-3.0.1.tar.gz

---
+++ packages/event-processor/event-processor-3.0.1.tar.gz/event-processor-3.0.1/src/event_processor/exceptions.py
@@ -0,0 +1,17 @@
+"""Exceptions for event processor."""
+
+
+class EventProcessorError(BaseException):
+    """General exception for the event-processor library."""
+
+
+class FilterError(EventProcessorError):
+    """Exception for failures related to filters."""
+
+
+class InvocationError(EventProcessorError):
+    """Exception for failures in invocation."""
+
+
+class DependencyError(EventProcessorError):
+    """Exceptions for failures while resolving dependencies."""
