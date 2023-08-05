54d122e1748a66ae24f38eb8ed7279170c0c6b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:25 +0000

    Add package chess960/0.1.0/chess960-0.1.0.tar.gz

---
+++ packages/chess960/chess960-0.1.0.tar.gz/chess960-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='chess960',
+    version='0.1.0',
+    description='',
+    url='http://github.com/qe/chess960',
+    author='Alex Ismodes',
+    author_email='helloemailmerighthere@gmail.com',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Games/Entertainment :: Board Games'
+    ],
+    keywords='chess, chess960',
+    packages=['chess960'],
+    install_requires=[''],
+    )
