8a4cb87af0c5367eabbdc9255cb5dcc64a1e8c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:51 +0000

    Add package openbox/0.7.10/openbox-0.7.10.tar.gz

---
+++ packages/openbox/openbox-0.7.10.tar.gz/openbox-0.7.10/openbox/__init__.py
@@ -0,0 +1,8 @@
+# This en code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this en tree.
+
+from .pkginfo import version as __version__, package_name
+
+__all__ = [
+    "__version__", "__package__", "package_name"
+]
