f06b91a7ce94f3319c3208314b7ae7a7e29a8ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:21 +0000

    Add package signalboxclient/0.1.16/signalboxclient-0.1.16-py2.py3-none-any.whl

---
+++ packages/signalboxclient/signalboxclient-0.1.16-py2.py3-none-any.whl/signalboxclient/product_recommenders.py
@@ -0,0 +1,16 @@
+from typing import List
+from .client_functions import get, post
+from .pagination import pageQuery, PaginatedResponse
+from .exceptions import SignalBoxException
+
+
+def invoke_product_recommender(access_token: str, base_url: str, id: int, common_user_id: str):
+    json_params = {
+        "commonUserId": common_user_id
+    }
+    r = post(f'{base_url}/api/recommenders/ProductRecommenders/{id}/invoke',
+             json_params, access_token)
+    if r.ok:
+        return r.json()
+    else:
+        raise SignalBoxException(r.text)
