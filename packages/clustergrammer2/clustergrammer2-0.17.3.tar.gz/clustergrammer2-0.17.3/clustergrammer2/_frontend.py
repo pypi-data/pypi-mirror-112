9ae929dc96bc5b0dac0cf901bb4c45e020a84f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:08 +0000

    Add package clustergrammer2/0.17.3/clustergrammer2-0.17.3.tar.gz

---
+++ packages/clustergrammer2/clustergrammer2-0.17.3.tar.gz/clustergrammer2-0.17.3/clustergrammer2/_frontend.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Nicolas Fernandez.
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "clustergrammer2"
+module_version = "0.17.3"
