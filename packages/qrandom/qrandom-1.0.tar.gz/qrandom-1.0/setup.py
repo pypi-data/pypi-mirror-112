92172568c3ddead07786dd771fd009060959a5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:36 +0000

    Add package qrandom/1.0/qrandom-1.0.tar.gz

---
+++ packages/qrandom/qrandom-1.0.tar.gz/qrandom-1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qrandom",
+    version="1.0",
+    author="Ravin Kumar",
+    author_email="mr.ravin_kumar@hotmail.com",
+    description="This repository contains the source code of research paper titled: \" A generalized quantum algorithm for assuring fairness in random selection among 2^n participants\"",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mr-ravin/QrandomSelection",
+    keywords = ['Quantum Algorithm','Quantum Fairness', 'One to One', 'Quantum Random Number','Random Selection','Lucky Draw'],   # Keywords that define your package best
+    install_requires=['qiskit'        
+      ],
+
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
