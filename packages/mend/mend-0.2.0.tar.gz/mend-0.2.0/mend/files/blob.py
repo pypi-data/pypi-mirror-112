54da40939a7647e6e2db0e65d6c8e57b6432b25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:01 +0000

    Add package mend/0.2.0/mend-0.2.0.tar.gz

---
+++ packages/mend/mend-0.2.0.tar.gz/mend-0.2.0/mend/files/blob.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import BinaryIO, Optional, Type
+
+from mend.protocols import Blob, Tree
+
+
+@dataclass(frozen=True)
+class FileBlob(Blob):
+    data: BinaryIO
+    name: str
+
+    def close(self) -> None:
+        self.data.close()
+
+    def as_tree(self) -> Tree:
+        return {
+            self.name: self.data,
+        }
+
+    @classmethod
+    def open(
+            cls: Type["FileBlob"],
+            path: str,
+            name: Optional[str] = None,
+    ) -> "FileBlob":
+        return cls(
+            data=open(path, "rb"),
+            name=name or path,
+        )
