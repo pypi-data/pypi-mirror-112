6ea7f333fb7be97075001d1c3d670f195a2d81d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:22 +0000

    Add package sf-max/0.1.0/sf_max-0.1.0.tar.gz

---
+++ packages/sf-max/sf_max-0.1.0.tar.gz/sf_max-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sf_max']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['sf = sf_max:main']}
+
+setup_kwargs = {
+    'name': 'sf-max',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Will Max',
+    'author_email': 'maxwillx@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.4,<4.0',
+}
+
+
+setup(**setup_kwargs)
