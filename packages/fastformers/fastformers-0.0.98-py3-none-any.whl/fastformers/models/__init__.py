315712b774d02c06096019dfd15a1ebf62100cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:44 +0000

    Add package fastformers/0.0.98/fastformers-0.0.98-py3-none-any.whl

---
+++ packages/fastformers/fastformers-0.0.98-py3-none-any.whl/fastformers/models/__init__.py
@@ -0,0 +1,6 @@
+from .bart import BartForSequenceClassification
+from .blender import Blender
+from .distilbert import DistilBertForMaskedLM
+from .electra import ElectraForSequenceClassification
+from .roberta import RobertaForQuestionAnswering
+from .coref import CoreferencePredictor
