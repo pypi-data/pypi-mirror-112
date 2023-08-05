ff3790540c18f36535861d82156bcd7b280e8a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:55 +0000

    Add package yarp-middleware/3.4.7.dev862/yarp_middleware-3.4.7.dev862-cp37-cp37m-manylinux_2_24_x86_64.whl

---
+++ packages/yarp-middleware/yarp_middleware-3.4.7.dev862-cp37-cp37m-manylinux_2_24_x86_64.whl/yarp/lib/cmake/YARP_conf/YARP_confConfig.cmake
@@ -0,0 +1,18 @@
+set(YARP_conf_VERSION 3.4.101+20210707.3+gitf7fd1c8dd)
+
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was YARP_confConfig.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+####################################################################################
+
+
+
+include("${CMAKE_CURRENT_LIST_DIR}/YARP_confTargets.cmake")
+
+
+
+
