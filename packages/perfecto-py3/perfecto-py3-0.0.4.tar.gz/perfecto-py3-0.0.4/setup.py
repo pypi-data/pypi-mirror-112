433ec14f35be81cd256e504ac49b27b4ef3d164d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:18 +0000

    Add package perfecto-py3/0.0.4/perfecto-py3-0.0.4.tar.gz

---
+++ packages/perfecto-py3/perfecto-py3-0.0.4.tar.gz/perfecto-py3-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+release_version = '0.0.4'
+
+setup(
+    name='perfecto-py3',
+    packages=['perfecto','perfecto/client', 'perfecto/Exceptions', 'perfecto/model', 'perfecto/test'],  # this must be the same as the name above
+    package_data = {'': ['*.txt']},
+	version=release_version,
+    description='Perfecto Reporting SDK for Python\nPerfecto Reporting is a multiple execution digital report',
+    author='Perfecto',
+    author_email='perfecto@perfectomobile.com',
+    url='https://github.com/PerfectoCode',  # use the URL to the GitHub repo
+    download_url='https://github.com/PerfectoCode',
+    keywords=['Perfecto', 'PerfectoMobile', 'Reporting', 'Selenium', 'Appium', 'Automation testing'],
+    classifiers=[ 'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent']
+)
