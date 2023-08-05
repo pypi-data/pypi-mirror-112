e453d46bba4ec780a2a7e2cfc5e9ebd1f6f30e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:02 +0000

    Add package nfetl/0.9.0/nfetl-0.9.0-py3-none-any.whl

---
+++ packages/nfetl/nfetl-0.9.0-py3-none-any.whl/processes/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""ETL Functions for NFL statistics database."""
+
+
+# %% Imports
+from nfetl.processes import extract, transform, load
+
+# %% Variables
+__all__ = ['extract', 'transform', 'load']
