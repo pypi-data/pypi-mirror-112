da34ff6c717b3ee854ffc201c69b5d5cd3a9329e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:47 +0000

    Add package bqtxflowutils-2/0.0.6/bqtxflowutils_2-0.0.6-py3-none-any.whl

---
+++ packages/bqtxflowutils-2/bqtxflowutils_2-0.0.6-py3-none-any.whl/beepbeep_txflowutils/secrets/config_setting.py
@@ -0,0 +1,32 @@
+config_secret = {
+    'GITHUB_TOKEN':"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",
+    'PROJECT_ID':"beepbeeptechnology",
+    'DATASET_ID':'txflowutils',
+    'GITHUB_REPO_USERNAME':"joseignaciorm",
+    'GITHUB_LOCAL_REMOTE_NAME':"publicAutomationRepo",
+    "CRED_BASE_TEST":{
+        "type": "service_account",
+        "project_id": "beepbeeptechnology",
+        "private_key_id": "b536b5a69d72c6d8e701c58adb2d7dbe4e576c80",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCt/8Ejk6d6lllT\nup7SUNKbpQj9iNTd/GknGa0vbviLJVvhAmoLspOtytOwWoIMH7HYnK9wE0UPaR0/\nP+8cFpbbaqRcwT3KjZY+1Hrv/+NP9O/1JKhwzDAqqq2Ri6Pzh9t41ed3q4sJ28/S\nUToSOJHc70dWqbHIhGntEm0dI4fXsMHK475oC+NWcGWkv7A9S8ei5H7QQBbrxzUC\nPUfnxvjS2RyKQ7eYjJ+f1ZIO1opn8EweVxkIN7RqvqvumnLq1+1nRNSwfnRLqT7v\ncOoDIdxTqwxGk86J45oxEUiLqUw4z5VJZSbaZAF7hgVPxRVeuGBsOmaaLSvaZ//n\noXrLGF0bAgMBAAECggEASbh9pUwu/mJrMG12UxeJVAale34Y6oDCMJ3RYbJ7vcCz\nX+887P3ZUh4IeMSwKuDW4jbooeyYKwJXMPAup9DuU8KoJuB9BK/eg3pVKBBbyAm4\nDT5d24S3t6we4/atHUZnd5fxfSOCQkgOkkZ1+zhanKgIqrA1MgbgNmDDOEBQktKN\nhDIWsyb1pqevKlWGfKH+rW2KtEDfgrYLaxGZiatWzDsPabcwZ/AMwMqh9P5jA/Yp\n9cO+zhCqwHgMDpE23E0cbi1Ih6XXn5B9kTNIYtYBOCvrwOyiB/rNAYBHkGShhfyI\nwEMIHdkr/BrpKMtTbBaF1TBapXZfv4a9VNMCEzMe+QKBgQDymvowOav7SkN2SU4q\nK++9nbMxcKHv0CtDsDX3CKVPy6OSnwZ6AGKLBt02X9FovQo7i5rLsxyJmbeasVyi\noSakTDjN4TbHvC+B4n4t6aqKtZkU5w7xnXUEnWYoXaux+dYu0aQHLCDNR62+NsBT\nH31FSOCbuGXPFPwyqSaILSCphwKBgQC3mxWWzJqqJENgiVwbt/GRkAc2sfrsHZhw\nInlNGSt76Vkl5dS8Tp2Ya6cBxMGzDp2xGI0cbKFQVGiPPM4+WwtFZ+wl47dim0HS\nDj2EwNWuQQ/RntEqJB6o72aPIY03djVLBeKpPOe4NxA89XtbgA3ifIeuC3gxIsYn\np78LYQGEzQKBgCVnL1twCMvdTzlVY++shnG9dnsQ2maWuZTd/fPwKUx8IOPiEKWO\nmaLFlCVl8Gstef5kL6FoI+YzBNOctKN+JO7/sWJMbp2srcZgnQOHGvv3s3o9hVhK\n5lenzLA8gNQXqKXfPGK+su/zNR42YsBuAmpEKhzX+RpMKfXFjmNAUeDRAoGAXXLl\njhcdMTO7x0qiQslgfkHcKg50M/DsQUaKjp8c7rhZ01wi5qTsuUMxgUjvfX8W66gf\nSjRP9l6/yQedwUb9eZnnK3G3SMzUt2DgFs0+9K3Qq1Q2CUjRAhK3FGpzQaM8hK6+\nKn0BQ1VjQmN/IZcPehL/y6myxu94A37xUY1SBJECgYEA7DwqmAKqDuy3yUa0L/WU\neY3It5JwDaypTaLRgx2b/C5MELJbc5wg386MwHTA1iH8zf70rtZ8bO1Idx4j0npO\n2IS66zPOhY11G4b3Ga5KpFsz9hX3uPO3frG4/18VZHAd+G9tjRktYeSVGzIZudQ4\nqVsUSI9bN6xGH7LZXS6vfmQ=\n-----END PRIVATE KEY-----\n",
+        "client_email": "beepbeeptechnology-admin@beepbeeptechnology.iam.gserviceaccount.com",
+        "client_id": "106091118219741117653",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/beepbeeptechnology-admin%40beepbeeptechnology.iam.gserviceaccount.com"
+    },
+    'folder_render_name':"docs",
+    'file_render_name':"data-transformation-functions-template",
+    'dtf_main_h1':'Data Transformation Functions',
+    'dtf_main_description':"These functions form the core of the framework and aim to automate and simplify common data transformation patterns.",
+    'dtf_subheader_h2':"De-Duplication",
+    'dtf_subheader_h2_description':"Robust de-duplication is typically executed by leveraging a row hash function, as unique identifiers are not enforced in BigQuery.",
+}
+
+
+# ******************* PROJECTS FOR TESTING ***********************
+# project_id="beepbeeptechnology", dataset_id='bboperations'
+# project_id="transformationflow", dataset_id='testing'
+# project_id="beepbeeptechnology", dataset_id='txflowutils'
+# project_id="carbon-scene-309911", dataset_id='mydataset'
