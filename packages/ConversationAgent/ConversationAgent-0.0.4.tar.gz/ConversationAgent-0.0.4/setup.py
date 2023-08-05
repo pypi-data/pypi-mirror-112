03817ffb6a9963d16fdf32605253d8e0f37ed45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:05 +0000

    Add package ConversationAgent/0.0.4/ConversationAgent-0.0.4.tar.gz

---
+++ packages/ConversationAgent/ConversationAgent-0.0.4.tar.gz/ConversationAgent-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='ConversationAgent',
+    version='0.0.4',
+    description='ConversationAgent',
+    author='Theta',
+    license='MIT',
+    packages=['ConversationAgent'],
+    zip_safe=False
+)
