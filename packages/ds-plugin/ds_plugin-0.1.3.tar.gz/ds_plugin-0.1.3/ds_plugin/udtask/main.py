c8913b6f215ed53c47dc3806e621cd7ba94ace41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:24 +0000

    Add package ds-plugin/0.1.3/ds_plugin-0.1.3.tar.gz

---
+++ packages/ds-plugin/ds_plugin-0.1.3.tar.gz/ds_plugin-0.1.3/ds_plugin/udtask/main.py
@@ -0,0 +1,13 @@
+import os
+from ds_plugin.common import io_utils
+from ds_plugin.udtask.res import Inherent as inh_class
+
+def main():
+    print ("This is Udtask Main.py")
+    inh = inh_class()
+    inh.print_self()
+
+    fs, path = io_utils.resolve_filesystem_and_path("hdfs://bigo-rt/user/bmlx/bmlx-unittest")
+
+if __name__ == '__main__':
+    main()
---
+++ packages/ds-plugin/ds_plugin-0.1.3.tar.gz/ds_plugin-0.1.3/ds_plugin/udtask/res.py
@@ -0,0 +1,6 @@
+class Inherent(object):
+    def print_self(self):
+        print (self.__class__.__name__)
+
+    def print_self2(self):
+        print (self.__class__.__name__)
