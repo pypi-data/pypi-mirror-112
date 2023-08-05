f46034bb95c221a41e92017e98e5548f8fb9b97d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:30 +0000

    Add package prefixdate/0.1.0/prefixdate-0.1.0-py2.py3-none-any.whl

---
+++ packages/prefixdate/prefixdate-0.1.0-py2.py3-none-any.whl/prefixdate/__init__.py
@@ -0,0 +1,32 @@
+from prefixdate.parse import DatePrefix
+from prefixdate.precision import Precision
+
+
+def parse(raw, precision=Precision.FULL):
+    """Parse the given input date string and return a `PrefixDate` object
+    that holds a datetime, text version and the precision of the date."""
+    return DatePrefix(raw, precision=precision)
+
+
+def normalize_date(raw, precision=Precision.FULL):
+    """Take the given input date string and parse it into the normalised
+    format to the precision given as an argument."""
+    return parse(raw, precision=precision).text
+
+
+def parse_parts(
+    year=None,
+    month=None,
+    day=None,
+    hour=None,
+    minute=None,
+    second=None,
+    precision=Precision.FULL,
+):
+    """Try to build a date prefix from the date components as given until
+    one of them is null."""
+    raw = f"{year}-{month}-{day}T{hour}:{minute}:{second}"
+    return normalize_date(raw, precision=precision)
+
+
+__all__ = ["DatePrefix", "Precision", "parse", "normalize_date"]
