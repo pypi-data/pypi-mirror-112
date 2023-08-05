6e66687dbcce6119092b3e69e2d8752b97d49796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:27 +0000

    Add package promoterExtract/0.8/promoterExtract-0.8.tar.gz

---
+++ packages/promoterExtract/promoterExtract-0.8.tar.gz/promoterExtract-0.8/promoterExtract/command_line.py
@@ -0,0 +1,9 @@
+from promoterExtract import get_prom
+
+
+def main():
+    get_prom()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/promoterExtract/promoterExtract-0.8.tar.gz/promoterExtract-0.8/promoterExtract/parameter.py
@@ -0,0 +1,39 @@
+import argparse
+
+class Parameter():
+    def __init__(self):
+        self._parser = argparse.ArgumentParser()
+    
+    def parse(self):
+        parser = argparse.ArgumentParser()
+        self._parser.add_argument('-l', '--length', type=int, help='promoter length before TSS')
+        self._parser.add_argument('-u', '--utr_head', type=int, help='length after TSS')
+        self._parser.add_argument('-f', '--genome', type=str, help='genome fasta')
+        self._parser.add_argument('-g', '--gff', type=str, help='genome annotation file')
+        self._parser.add_argument('-o', '--output', type=str, help = 'output csv file path')
+        self._parser.add_argument('-v', '--version', help = 'epip version information', action = "store_true")
+
+        args = self._parser.parse_args()
+
+        if args.version:
+            print("promorerExtract version 0.8")
+            exit(1)
+
+        self.length = args.length
+        self.utr_head = args.utr_head
+        self.genome = args.genome
+        self.gff = args.gff
+        self.output = args.output
+        
+        if self.length is None:
+            raise  Exception("Error: promoter length is required")
+        elif self.utr_head is None:
+            raise Exception("Error: utr length is required")
+        elif self.genome is None:
+            raise Exception("Error: genome fasta is required")
+        elif self.gff is None:
+            raise Exception("Error: gff is required")
+        elif self.output is None:
+            raise Exception("Error: output is required")
+
+        return self
