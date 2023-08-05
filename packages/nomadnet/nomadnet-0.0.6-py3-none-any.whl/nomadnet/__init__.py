1f04cada1e46442ca1d32d4ae74749e9378c28a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:55 +0000

    Add package nomadnet/0.0.6/nomadnet-0.0.6-py3-none-any.whl

---
+++ packages/nomadnet/nomadnet-0.0.6-py3-none-any.whl/nomadnet/__init__.py
@@ -0,0 +1,13 @@
+import os
+import glob
+
+from .NomadNetworkApp import NomadNetworkApp
+from .Conversation import Conversation
+from .ui import *
+
+
+modules = glob.glob(os.path.dirname(__file__)+"/*.py")
+__all__ = [ os.path.basename(f)[:-3] for f in modules if not f.endswith('__init__.py')]
+
+def panic():
+    os._exit(255)
