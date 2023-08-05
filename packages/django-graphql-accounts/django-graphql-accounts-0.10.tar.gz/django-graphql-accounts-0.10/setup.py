1b37504f1a17ed832ac3d254f1fde4bd3702a58f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:12 +0000

    Add package django-graphql-accounts/0.10/django-graphql-accounts-0.10.tar.gz

---
+++ packages/django-graphql-accounts/django-graphql-accounts-0.10.tar.gz/django-graphql-accounts-0.10/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='django-graphql-accounts',
+    version='0.10',
+    author='Celia Oakley',
+    author_email='celia.oakley@alumni.stanford.edu',
+    description='A RESTful API for user signup and authentication using email addresses',
+    keywords=[
+        'django', 'python', 'rest', 'rest-framework', 'api',
+        'auth', 'authentication', 'email', 'user', 'username',
+        'registration', 'signup', 'login', 'logout', 'password',
+        'django-rest-framework', 'djangorestframework', 'django-registration',
+        'django-email-as-username'
+    ],
+    url='http://github.com/celiao/django-rest-authemail',
+    download_url='https://github.com/celiao/django-rest-authemail/tarball/2.0.6',
+    # packages=['django-graphql-accounts'],
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'Django>=2.2.8,<=3.1,<=3.2',
+        'graphene-django>=2.15' ,
+        'celery>=5.1',
+        'django-graphql-jwt>=0.3',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+)
