218e33a44c086686c4375af0f1c1bf2764f10974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:34 +0000

    Add package python-facebook-api/0.10.0/python-facebook-api-0.10.0.tar.gz

---
+++ packages/python-facebook-api/python-facebook-api-0.10.0.tar.gz/python-facebook-api-0.10.0/pyfacebook/__init__.py
@@ -0,0 +1,7 @@
+from pyfacebook.models import *
+from pyfacebook.ratelimit import RateLimitHeader, RateLimit, PercentSecond
+from pyfacebook.exceptions import PyFacebookException, FacebookError, LibraryError
+from pyfacebook.api import GraphAPI
+from pyfacebook.api.facebook.client import FacebookApi
+
+__version__ = "0.10.0"
---
+++ packages/python-facebook-api/python-facebook-api-0.10.0.tar.gz/python-facebook-api-0.10.0/pyfacebook/utils/constant.py
@@ -0,0 +1,42 @@
+"""
+    This is default fields for to retrieve data from Facebook graph API.
+"""
+
+# =============================================
+# Follows is Facebook graph fields to get data
+# =============================================
+
+USER_PUBLIC_FIELDS = [
+    "id",
+    "first_name",
+    "last_name",
+    "middle_name",
+    "name",
+    "name_format",
+    "picture",
+    "short_name",
+]
+
+
+PAGE_PUBLIC_FIELDS = [
+    "id",
+    "about",
+    "category",
+    "category_list",
+    "cover",
+    "description",
+    "description_html",
+    "display_subtext",
+    "emails",
+    "engagement",
+    "fan_count",
+    "global_brand_page_name",
+    "global_brand_root_id",
+    "link",
+    "name",
+    "phone",
+    "picture",
+    "username",
+    "verification_status",
+    "website",
+]
