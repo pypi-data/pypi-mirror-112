1e9a12629c94f4886121cb5d599a33729322a1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:46 +0000

    Add package profile-watcher/1.0.0/profile_watcher-1.0.0-py3-none-any.whl

---
+++ packages/profile-watcher/profile_watcher-1.0.0-py3-none-any.whl/watcher/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# A python library that provides update when
+# something changes in social profiles.
+# Copyright (C) 2021 Shubhendra Kushwaha
+# @TheShubhendra shubhendrakushwaha94@gmail.com
+# This file is a part of profile-watcher <https://github.com/TheShubhendra/profile-watcher>.
+#
+# profile-watcher is a free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# profile-watcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with profile-watcher.  If not, see <http://www.gnu.org/licenses/>.
+from .watcher import Watcher
+
+
+__all__ = [Watcher]
