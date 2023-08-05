cde11ae47315abe04168377c20d3130681afac0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:51 +0000

    Add package pySESL/0.2/pySESL-0.2.tar.gz

---
+++ packages/pySESL/pySESL-0.2.tar.gz/pySESL-0.2/pySESL/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+# type: ignore
+
+import pkg_resources
+
+from .historical import calc_T0, calc_temp, calc_sl, resize_T
+from .io import load_data_SESL, load_param_file
+
+__version__ = pkg_resources.get_distribution("rhg_compute_tools").version
