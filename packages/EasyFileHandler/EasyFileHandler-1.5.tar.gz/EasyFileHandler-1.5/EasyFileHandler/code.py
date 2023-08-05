41352bb4befcf24819a22f5167ce5481f78d3d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:17:03 +0000

    Add package EasyFileHandler/1.5/EasyFileHandler-1.5.tar.gz

---
+++ packages/EasyFileHandler/EasyFileHandler-1.5.tar.gz/EasyFileHandler-1.5/EasyFileHandler/code.py
@@ -0,0 +1,29 @@
+
+class FilesOperater():
+    def read(file:str, seperator=None):
+        try:
+            f = open(file,"r")
+            rd = f.read()
+            f.close()
+            return rd.split(seperator)
+        except Exception as e:
+            return f"Exception Occured: {e}"
+
+    
+    def write(file:str, content):
+        try:
+            f = open(file,"w")
+            f.write(f"{content}\n")
+            f.close()
+            return f"File \"{file}\" has been formatted successfully."
+        except Exception as e:
+            return f"Exception Occured: {e}"
+
+    def edit(file:str, content):
+        try:
+            f = open(file,"a")
+            f.write(f"{content}\n")
+            f.close()
+            return f"File \"{file}\" has been Edited Successfully."
+        except Exception as e:
+            return f"Exception Occured: {e}"
