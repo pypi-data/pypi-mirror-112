f9e47aa38d22a3d7da9773b5f139577eee93e065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:22 +0000

    Add package pacc/0.0.32/pacc-0.0.32-py3-none-any.whl

---
+++ packages/pacc/pacc-0.0.32-py3-none-any.whl/pacc/adb/adb.py
@@ -0,0 +1,225 @@
+from os import popen, system, remove
+from os.path import exists
+from ..tools import findAllWithRe, sleep, EMail, createDir
+from random import randint
+from ..mysql import Retrieve, Update
+from datetime import datetime
+from xml.dom import minidom
+from html import unescape
+
+
+def getOnlineDevices():
+    res = popen('adb devices').read()
+    res = findAllWithRe(res, r'(.+)\tdevice')
+    for i in range(len(res)):
+        res[i] = res[i].replace(':5555', '')
+    return res
+
+
+def reconnectOffline():
+    """
+    reset offline/unauthorized devices to force reconnect
+    :return:
+    """
+    system('adb reconnect offline')
+
+
+class ADB:
+    rebootPerHourRecord = [-1]
+
+    def __init__(self, deviceSN, offlineCnt=1):
+        """
+        :param deviceSN:
+        """
+        reconnectOffline()
+        self.device = Retrieve(deviceSN)
+        if self.device.ID not in getOnlineDevices():
+            if not offlineCnt % 20:
+                EMail(self.device.SN).sendOfflineError()
+            sleep(30)
+            self.__init__(deviceSN, offlineCnt+1)
+        self.cmd = 'adb -s %s ' % self.device.ID
+        if not self.getIPv4Address() == self.device.IP:
+            Update(deviceSN).updateIP(self.getIPv4Address())
+            self.device = Retrieve(deviceSN)
+        self.tcpip()
+        self.reconnect()
+        self.cmd = 'adb -s %s ' % self.device.IP
+        if not self.getModel() == self.device.Model:
+            Update(deviceSN).updateModel(self.getModel())
+            self.device = Retrieve(deviceSN)
+        if 'com.android.settings' in self.getCurrentFocus():
+            if self.device.Model == 'M2007J22C':
+                self.pressBackKey()
+
+    def getModel(self):
+        return popen(self.cmd + 'shell getprop ro.product.model').read()[:-1]
+
+    def getCurrentUIHierarchy(self):
+        system(self.cmd + 'shell uiautomator dump')
+        currentUIHierarchyDirName = 'CurrentUIHierarchy'
+        createDir(currentUIHierarchyDirName, removeOldDir=False)
+        currentUIHierarchyFileName = '%s/%s.xml' % (currentUIHierarchyDirName, self.device.SN)
+        if exists(currentUIHierarchyFileName):
+            remove(currentUIHierarchyFileName)
+        system('%s pull /sdcard/window_dump.xml %s' % (
+            self.cmd, currentUIHierarchyFileName))
+        xml = open(currentUIHierarchyFileName, 'r', encoding='utf-8').read()
+        xml = minidom.parseString(xml)
+        xml = xml.toprettyxml()
+        xml = unescape(xml)
+        with open(currentUIHierarchyFileName, 'w', encoding='utf-8') as f:
+            f.writelines(xml)
+        print(xml)
+
+    def getCurrentFocus(self):
+        r = popen(self.cmd + 'shell dumpsys window | findstr mCurrentFocus').read()
+        r = r.replace("  mCurrentFocus=Window{", '')[:-2]
+        print(r)
+        return r
+
+    def pressKey(self, keycode):
+        print('正在让%s按下%s键' % (self.device.SN, keycode))
+        system(self.cmd + 'shell input keyevent ' + keycode)
+        sleep(1)
+
+    def pressHomeKey(self):
+        self.pressKey('KEYCODE_HOME')
+
+    def pressMenuKey(self):
+        self.pressKey('KEYCODE_MENU')
+
+    def pressBackKey(self):
+        self.pressKey('KEYCODE_BACK')
+
+    def usb(self, timeout=2):
+        """
+        restart adbd listening on USB
+        :return:
+        """
+        cmd = self.cmd + 'usb'
+        system(cmd)
+        print(cmd)
+        sleep(timeout)
+
+    def tcpip(self):
+        """
+        restart adbd listening on TCP on PORT
+        :return:
+        """
+        system(self.cmd + 'tcpip 5555')
+        sleep(1)
+
+    def connect(self, timeout=1):
+        """
+        connect to a device via TCP/IP [default port=5555]
+        :return:
+        """
+        system('adb connect %s' % self.device.IP)
+        sleep(timeout)
+        if self.device.IP not in getOnlineDevices():
+            self.connect(timeout + 1)
+
+    def disconnect(self):
+        """
+        disconnect from given TCP/IP device [default port=5555], or all
+        :return:
+        """
+        system('adb disconnect %s' % self.device.IP)
+        sleep(3)
+
+    def reconnect(self):
+        self.disconnect()
+        self.connect()
+
+    def tap(self, x, y, interval=1):
+        print('正在让%s点击(%d,%d)' % (self.device.SN, x, y))
+        system(self.cmd + 'shell input tap %d %d' % (x, y))
+        sleep(interval)
+
+    def taps(self, instructions):
+        for x, y, interval, tip in instructions:
+            print(tip)
+            self.tap(x, y, interval)
+
+    def start(self, Activity, wait=True):
+        cmd = 'shell am start '
+        if wait:
+            cmd += '-W '
+        cmd = self.cmd + cmd + Activity
+        system(cmd)
+        print(cmd)
+
+    def swipe(self, x1, y1, x2, y2, duration=-1):
+        """
+        :param x1:
+        :param y1:
+        :param x2:
+        :param y2:
+        :param duration: the default duration is a random integer from 300 to 500
+        :return:
+        """
+        if duration == -1:
+            duration = randint(300, 500)
+        cmd = self.cmd + 'shell input swipe %d %d %d %d %d' % (x1, y1, x2, y2, duration)
+        system(cmd)
+        print(cmd)
+
+    def longPress(self, x, y, duration=-1):
+        """
+        :param x:
+        :param y:
+        :param duration: the default duration is a random integer from 1000 to 1500
+        :return:
+        """
+        if duration == -1:
+            duration = randint(1000, 1500)
+        self.swipe(x, y, x, y, duration)
+
+    def reboot(self):
+        self.rebootByIP()
+
+    def rebootByCMD(self, cmd):
+        popen(cmd)
+        print('已向设备%s下达重启指令' % self.device.SN)
+        sleep(96)
+        self.__init__(self.device.SN)
+
+    def rebootByIP(self):
+        if self.device.IP not in getOnlineDevices():
+            self.__init__(self.device.SN)
+        self.rebootByCMD('adb -s ' + self.device.IP + ' reboot')
+
+    def rebootPerHour(self, tip='小时'):
+        if not datetime.now().hour == self.rebootPerHourRecord[0]:
+            self.rebootPerHourRecord = [datetime.now().hour]
+        if self.device.SN not in self.rebootPerHourRecord:
+            print('按每' + tip + '重启一次的计划重启' + self.device.SN)
+            self.reboot()
+            self.rebootPerHourRecord.append(self.device.SN)
+            return True
+        return False
+
+    def rebootPerDay(self, hours=[0]):
+        if datetime.now().hour in hours:
+            self.rebootPerHour(tip='天')
+            return True
+        return False
+
+    def getIPv4Address(self):
+        rd = popen(self.cmd + 'shell ifconfig wlan0').read()
+        IPv4Address = findAllWithRe(rd, r'inet addr:(\d+.\d+.\d+.\d+)  Bcast:.+')
+        if len(IPv4Address) == 1:
+            IPv4Address = IPv4Address[0]
+        return IPv4Address
+
+    def getIPv6Address(self):
+        rd = popen(self.cmd + 'shell ifconfig wlan0').read()
+        IPv6Address = findAllWithRe(rd, r'inet6 addr: (.+:.+:.+)/64 Scope: Global')
+        if 0 < len(IPv6Address) <= 2:
+            IPv6Address = IPv6Address[0]
+            print('设备%s的公网IPv6地址为：%s' % (self.device, IPv6Address))
+        else:
+            print('%s的公网IPv6地址数大于2或小于0，正在尝试重新获取')
+            self.reboot()
+            self.getIPv6Address()
