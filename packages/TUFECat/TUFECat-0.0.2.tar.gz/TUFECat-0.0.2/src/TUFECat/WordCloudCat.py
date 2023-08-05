6d8ff1fc4c1bc705066ce24d5d9fb3431f11299d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:40 +0000

    Add package TUFECat/0.0.2/TUFECat-0.0.2.tar.gz

---
+++ packages/TUFECat/TUFECat-0.0.2.tar.gz/TUFECat-0.0.2/src/TUFECat/WordCloudCat.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import jieba
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+def draw_word_cloud(word):
+    words = jieba.cut(word)
+    wordstr = " ".join(words)
+    sw = set(STOPWORDS)
+    wc = WordCloud(
+        font_path='C:/Windows/Fonts/simhei.ttf',  # 设置字体格式
+        max_words=300,
+        max_font_size=70,
+        stopwords=sw,
+        background_color='white',
+        scale=20,
+    ).generate(wordstr)
+    # 显示词云图 
+    plt.imshow(wc.recolor())
+    plt.axis("off")
+    plt.show()
+   # 保存词云图
+    wc.to_file('result_1.jpg')
+ 
+if __name__ == "__main__":
+    with open("E://Download//7.txt", "rb") as f:#文本内容
+        word = f.read()
+    draw_word_cloud(word)
