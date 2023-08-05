13668e9e7402d77841a4419d0cd3478cca04c558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:50 +0000

    Add package calculatorpython/0.0.4b1/calculatorpython-0.0.4b1-py3-none-any.whl

---
+++ packages/calculatorpython/calculatorpython-0.0.4b1-py3-none-any.whl/calculatorpython/__init__.py
@@ -0,0 +1,5 @@
+from calculatorpython import add
+from calculatopython import subtract
+from calculatorpython import multiply
+from calculatopython import divide
+from calculatorpython import exponent
