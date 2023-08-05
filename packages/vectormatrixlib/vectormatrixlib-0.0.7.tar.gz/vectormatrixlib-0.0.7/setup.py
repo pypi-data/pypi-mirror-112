1d95becf67df4856deec363f4610a66ef276e3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:23 +0000

    Add package vectormatrixlib/0.0.7/vectormatrixlib-0.0.7.tar.gz

---
+++ packages/vectormatrixlib/vectormatrixlib-0.0.7.tar.gz/vectormatrixlib-0.0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='vectormatrixlib',
+	version='0.0.7',
+	description='Linear Algebra Matrix Tool',
+	author='Andrew King',
+	author_email='andrewking1597@gmail.com',
+	url='https://github.com/andrewking1597/vector-matrix',
+	packages=['vectormatrixlib']
+)
