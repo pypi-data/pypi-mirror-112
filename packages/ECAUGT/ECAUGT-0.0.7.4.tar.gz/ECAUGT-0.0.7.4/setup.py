02413aa725290a7a8dafb91bba912494d7d2bf9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:47 +0000

    Add package ECAUGT/0.0.7.4/ECAUGT-0.0.7.4.tar.gz

---
+++ packages/ECAUGT/ECAUGT-0.0.7.4.tar.gz/ECAUGT-0.0.7.4/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="ECAUGT",
+    version="0.0.7.4",
+    author="Yixin Chen & XiaoXiao Nong & Haiyang Bian",
+    author_email="chenyx19@mails.tsinghua.edu.cn",
+    maintainer='Minsheng Hao',
+    maintainer_email="hmsh653@gmail.com",
+    description="ECA Client",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/kenblikylee/imgkernel",
+    packages=setuptools.find_packages(),
+    package_data={
+        # 引入任何包下面的 *.txt、*.rst 文件
+        "": ["*.csv"],
+    },
+    install_requires=[
+        'tablestore>=5.2.1',
+        'numpy',
+        'pandas'
+        ],
+    liciense='GPL',
+    keywords=["Client", "ECA"],
+    python_requires='>=3',
+)
