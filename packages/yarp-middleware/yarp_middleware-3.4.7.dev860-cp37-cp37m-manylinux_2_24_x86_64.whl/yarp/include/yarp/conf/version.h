92562fbdff70766b343c7ce7ac395e7220a8ae4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:12 +0000

    Add package yarp-middleware/3.4.7.dev860/yarp_middleware-3.4.7.dev860-cp37-cp37m-manylinux_2_24_x86_64.whl

---
+++ packages/yarp-middleware/yarp_middleware-3.4.7.dev860-cp37-cp37m-manylinux_2_24_x86_64.whl/yarp/include/yarp/conf/version.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * Copyright (C) 2006-2010 RobotCub Consortium
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef YARP_CONFIG_VERSION_H
+#define YARP_CONFIG_VERSION_H
+
+#define YARP_VERSION_MAJOR 3
+#define YARP_VERSION_MINOR 4
+#define YARP_VERSION_PATCH 101
+#define YARP_VERSION_TWEAK 
+#define YARP_VERSION "3.4.101+20210707.1+gitefc07a766"
+#define YARP_VERSION_SHORT "3.4.101"
+
+#endif
