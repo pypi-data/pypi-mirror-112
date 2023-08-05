2349bc110afe2c7ed0182a7b99dea8179a450c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:33 +0000

    Add package nonebot-plugin-statistical/0.1/nonebot-plugin-statistical-0.1.tar.gz

---
+++ packages/nonebot-plugin-statistical/nonebot-plugin-statistical-0.1.tar.gz/nonebot-plugin-statistical-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='nonebot-plugin-statistical',
+      version='0.1',
+      description='Nonebot2插件',
+      url='https://github.com/HibiKier/nonebot_plugin_statistical',
+      author='hibikier',
+      author_email='775757368@qq.com',
+      license='MIT',
+      packages=['nonebot_plugin_statistical'])
