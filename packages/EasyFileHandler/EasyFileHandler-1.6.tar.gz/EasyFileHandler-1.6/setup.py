736593f7038e16ef5ec495dba7fb87c91132ee76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:05 +0000

    Add package EasyFileHandler/1.6/EasyFileHandler-1.6.tar.gz

---
+++ packages/EasyFileHandler/EasyFileHandler-1.6.tar.gz/EasyFileHandler-1.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools import find_packages
+
+version = "1.6"
+
+# This call to setup() does all the work
+setup(
+    name="EasyFileHandler",
+    version=version,
+    description="File Operations' Controller. Has Edit, Write(Format) And Read Features",
+    url="https://github.com/v1s1t0r999/PyPi_Uploads/tree/EasyFileHandler",
+    author="v1s1t0r999",
+    author_email="aditya.funs.11@gmail.com",
+    license="MIT",
+    python_requires=">=3.6",
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
