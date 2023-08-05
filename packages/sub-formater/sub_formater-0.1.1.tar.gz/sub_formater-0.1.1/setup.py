d46f448102256d163f1fd677a3df508db3232717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:27 +0000

    Add package sub-formater/0.1.1/sub_formater-0.1.1.tar.gz

---
+++ packages/sub-formater/sub_formater-0.1.1.tar.gz/sub_formater-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sub_formater']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['subformater = sub_formater:main']}
+
+setup_kwargs = {
+    'name': 'sub-formater',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Will Max',
+    'author_email': 'maxwillx@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
