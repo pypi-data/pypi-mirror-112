20a26361e8654e5a06fe9b3d1d49aa399c6b5a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:01 +0000

    Add package hoopR-py/0.0.10/hoopR-py-0.0.10.tar.gz

---
+++ packages/hoopR-py/hoopR-py-0.0.10.tar.gz/hoopR-py-0.0.10/hoopR/nba/__init__.py
@@ -0,0 +1,163 @@
+import pyarrow.parquet as pq
+import pandas as pd
+import json
+from typing import List, Callable, Iterator, Union, Optional
+from hoopR.config import NBA_BASE_URL, NBA_TEAM_BOX_URL, NBA_PLAYER_BOX_URL, NBA_TEAM_SCHEDULE_URL
+from hoopR.errors import SeasonNotFoundError
+from hoopR.dl_utils import download
+
+def load_nba_pbp(seasons: List[int]) -> pd.DataFrame:
+    """Load NBA play by play data going back to 2002
+
+    Example:
+        `nba_df = hoopR.nba.load_nba_pbp(seasons=range(2002,2022))`
+
+    Args:
+        seasons (list): Used to define different seasons. 2002 is the earliest available season.
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing the
+        play-by-plays available for the requested seasons.
+
+    Raises:
+        ValueError: If `season` is less than 2002.
+    """
+    data = pd.DataFrame()
+    if type(seasons) is int:
+        seasons = [seasons]
+    for i in seasons:
+        if int(i) < 2002:
+            raise SeasonNotFoundError("season cannot be less than 2002")
+        i_data = pd.read_parquet(NBA_BASE_URL.format(season=i), engine='auto', columns=None)
+        data = data.append(i_data)
+    #Give each row a unique index
+    data.reset_index(drop=True, inplace=True)
+    return data
+
+def load_nba_team_boxscore(seasons: List[int]) -> pd.DataFrame:
+    """Load NBA team boxscore data
+
+    Example:
+        `nba_df = hoopR.nba.load_nba_team_boxscore(seasons=range(2002,2022))`
+
+    Args:
+        seasons (list): Used to define different seasons. 2002 is the earliest available season.
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing the
+        team boxscores available for the requested seasons.
+
+    Raises:
+        ValueError: If `season` is less than 2002.
+    """
+    data = pd.DataFrame()
+    if type(seasons) is int:
+        seasons = [seasons]
+    for i in seasons:
+        if int(i) < 2002:
+            raise SeasonNotFoundError("season cannot be less than 2002")
+        i_data = pd.read_parquet(NBA_TEAM_BOX_URL.format(season = i), engine='auto', columns=None)
+        data = data.append(i_data)
+    #Give each row a unique index
+    data.reset_index(drop=True, inplace=True)
+
+    return data
+
+def load_nba_player_boxscore(seasons: List[int]) -> pd.DataFrame:
+    """Load NBA player boxscore data
+
+    Example:
+        `nba_df = hoopR.nba.load_nba_player_boxscore(seasons=range(2002,2022))`
+
+    Args:
+        seasons (list): Used to define different seasons. 2002 is the earliest available season.
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing the
+        player boxscores available for the requested seasons.
+
+    Raises:
+        ValueError: If `season` is less than 2002.
+    """
+    data = pd.DataFrame()
+    if type(seasons) is int:
+        seasons = [seasons]
+    for i in seasons:
+        if int(i) < 2002:
+            raise SeasonNotFoundError("season cannot be less than 2002")
+        i_data = pd.read_parquet(NBA_PLAYER_BOX_URL.format(season = i), engine='auto', columns=None)
+        data = data.append(i_data)
+    #Give each row a unique index
+    data.reset_index(drop=True, inplace=True)
+
+    return data
+
+def load_nba_schedule(seasons: List[int]) -> pd.DataFrame:
+    """Load NBA schedule data
+
+    Example:
+        `nba_df = hoopR.nba.load_nba_schedule(seasons=range(2002,2022))`
+
+    Args:
+        seasons (list): Used to define different seasons. 2002 is the earliest available season.
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing the
+        schedule for  the requested seasons.
+
+    Raises:
+        ValueError: If `season` is less than 2002.
+    """
+    data = pd.DataFrame()
+    if type(seasons) is int:
+        seasons = [seasons]
+    for i in seasons:
+        if int(i) < 2002:
+            raise SeasonNotFoundError("season cannot be less than 2002")
+        i_data = pd.read_parquet(NBA_TEAM_SCHEDULE_URL.format(season = i), engine='auto', columns=None)
+        data = data.append(i_data)
+    #Give each row a unique index
+    data.reset_index(drop=True, inplace=True)
+
+    return data
+
+def nba_calendar(season: int) -> pd.DataFrame:
+    """nba_calendar - look up the NBA calendar for a given season
+
+    Args:
+        season (int): Used to define different seasons. 2002 is the earliest available season.
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing
+        calendar dates for the requested season.
+
+    Raises:
+        ValueError: If `season` is less than 2002.
+    """
+    
+    if int(season) < 2002:
+        raise SeasonNotFoundError("season cannot be less than 2002")
+
+    url = "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard?dates={}".format(season)
+    resp = download(url=url)
+    txt = json.loads(resp)['leagues'][0]['calendar']
+    datenum = list(map(lambda x: x[:10].replace("-",""),txt))
+    date = list(map(lambda x: x[:10],txt))
+
+    year = list(map(lambda x: x[:4],txt))
+    month = list(map(lambda x: x[5:7],txt))
+    day = list(map(lambda x: x[8:10],txt))
+
+    data = {"season": season,
+            "datetime" : txt,
+            "date" : date,
+            "year": year,
+            "month": month,
+            "day": day,
+            "dateURL": datenum
+    }
+    df = pd.DataFrame(data)
+    df['url']="http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard?dates="
+    df['url']= df['url'] + df['dateURL']
+    return df
+
