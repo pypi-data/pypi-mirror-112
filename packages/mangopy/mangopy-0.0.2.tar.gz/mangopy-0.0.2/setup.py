312134b17d5bc19db32c7dff3a25eb819f14b90a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:49 +0000

    Add package mangopy/0.0.2/mangopy-0.0.2.tar.gz

---
+++ packages/mangopy/mangopy-0.0.2.tar.gz/mangopy-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os
+
+REQUIREMENTS = [
+    'scipy >= 1.2',
+    'h5py >= 2.9',
+    'matplotlib >= 2.2.4'
+]
+
+setup(name='mangopy',
+      version='0.0.2',
+      description='Software for MANGO ASI network',
+      author = 'P. Venkatraman, L. Lamarche, A. Bhatt',
+      license='GPLv3',
+      packages=['mangopy'],
+      install_requires=REQUIREMENTS,
+      package_data={'mangopy': ['SiteInformation.csv']},
+      zip_safe=False)
