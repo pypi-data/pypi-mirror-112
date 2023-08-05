4f49ce12f98341322c57da9cb0ccd1ca9e0669cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:21 +0000

    Add package helloworld-syed/0.0.1/helloworld-syed-0.0.1.tar.gz

---
+++ packages/helloworld-syed/helloworld-syed-0.0.1.tar.gz/helloworld-syed-0.0.1/src/helloworld.py
@@ -0,0 +1,10 @@
+from blessings import Terminal
+
+t = Terminal()
+
+
+def say_hello(name=None):
+    if name is None:
+        return "Hello, World!"
+    else:
+        return f"Hello, {t.red}{name}{t.normal}!"
