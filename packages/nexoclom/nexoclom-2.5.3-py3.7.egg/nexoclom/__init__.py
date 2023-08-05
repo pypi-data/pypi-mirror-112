9ccc5228f19298fb4259daa78326b79bfbd03ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:29 +0000

    Add package nexoclom/2.5.3/nexoclom-2.5.3-py3.7.egg

---
+++ packages/nexoclom/nexoclom-2.5.3-py3.7.egg/nexoclom/__init__.py
@@ -0,0 +1,18 @@
+from .Input import Input
+from .Output import Output
+from .LOSResult import LOSResult
+from .ModelImage import ModelImage
+from .configure_model import configure_model
+from .LossInfo import LossInfo
+# from .LOSResult_dask import LOSResult
+from .LOSResult import LOSResult
+from .IDLout import IDLout
+from .database_connect import database_connect, export_database, import_database
+
+
+name = 'nexoclom'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '2.5.3'
+
+configure_model()
