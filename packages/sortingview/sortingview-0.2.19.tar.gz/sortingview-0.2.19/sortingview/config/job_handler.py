f9ccb772b9b3e05e1b1d8a136f68b869d8b21fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:45 +0000

    Add package sortingview/0.2.19/sortingview-0.2.19.tar.gz

---
+++ packages/sortingview/sortingview-0.2.19.tar.gz/sortingview-0.2.19/sortingview/config/job_handler.py
@@ -0,0 +1,9 @@
+import hither2 as hi
+
+class job_handler:
+    correlograms = hi.ParallelJobHandler(num_workers=4)
+    timeseries = hi.ParallelJobHandler(num_workers=4)
+    waveforms = hi.ParallelJobHandler(num_workers=4)
+    clusters = hi.ParallelJobHandler(num_workers=4)
+    metrics = hi.ParallelJobHandler(num_workers=4)
+    misc = hi.ParallelJobHandler(num_workers=4)
