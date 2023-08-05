e9a815039ed146b8e1c07741bb5a9b22e9e0ce50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:19 +0000

    Add package air-drf-relation/0.0.19/air_drf_relation-0.0.19.tar.gz

---
+++ packages/air-drf-relation/air_drf_relation-0.0.19.tar.gz/air_drf_relation-0.0.19/school/models.py
@@ -0,0 +1,24 @@
+from uuid import uuid4
+
+from django.db import models
+
+
+class Child(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Cabinet(models.Model):
+    name = models.CharField(max_length=256)
+    code = models.IntegerField()
+    school = models.ForeignKey('School', on_delete=models.CASCADE, related_name='cabinets')
+
+
+class School(models.Model):
+    name = models.CharField(max_length=256)
+    children = models.ManyToManyField(Child, related_name='schools')
+
+
+class Parent(models.Model):
+    id = None
+    uuid = models.UUIDField(default=uuid4, editable=True, primary_key=True, unique=True)
+    name = models.CharField(max_length=128, unique=True)
