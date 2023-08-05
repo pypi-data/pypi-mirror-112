f50a85d2292bfa05f8ff3524a525e724d1aabf96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:04 +0000

    Add package rashsetup/0.1.8/RashSetup-0.1.8.tar.gz

---
+++ packages/rashsetup/RashSetup-0.1.8.tar.gz/RashSetup-0.1.8/RashSetup/RashScrappers/RashScrappers/spiders/README.py
@@ -0,0 +1,44 @@
+import scrapy
+
+
+class ReadmeSpider(scrapy.Spider):
+    name = 'README'
+    allowed_domains = ['github.com']
+
+    def __init__(
+            self, pipe, url
+    ):
+        super().__init__()
+
+        self.start_urls = [
+            url
+        ]
+        self.pipe = pipe
+
+    def parse(self, response, *args):
+        got = response.xpath("//div[@id='readme']")
+
+        result = got.extract()
+
+        self.pipe.put(
+            bool(result)
+        )
+
+        self.pipe.put(
+            result[0] if self.pipe[-1] else f"Failed to request README.md from {response.request.url}"
+        )
+
+        yield "extracted"
+
+
+if __name__ == "__main__":
+    import scrapy.crawler
+
+    process = scrapy.crawler.CrawlerProcess()
+    pipe = []
+    process.crawl(ReadmeSpider, url="https://github.com/RahulARanger/Rash/blob/master/README.md", pipe=pipe)
+    process.start()
+
+    print(pipe)
+
+
