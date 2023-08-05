df2aa477697b3ef99a378d8f002d27280beaae20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:52 +0000

    Add package inkprint/1.1/inkprint-1.1.tar.gz

---
+++ packages/inkprint/inkprint-1.1.tar.gz/inkprint-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='inkprint',
+      version='1.1',
+      description='A simple way to print texts with colors.',
+      packages=['inkprint'],
+      author='Shi Shu',
+      author_email = 'shushi20002003@gmail.com',
+      zip_safe=False)
