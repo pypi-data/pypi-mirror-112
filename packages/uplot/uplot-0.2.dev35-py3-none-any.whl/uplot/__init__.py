10f42e6c41654c99ad27c3ca40587a385d85959a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:33 +0000

    Add package uplot/0.2.dev35/uplot-0.2.dev35-py3-none-any.whl

---
+++ packages/uplot/uplot-0.2.dev35-py3-none-any.whl/uplot/__init__.py
@@ -0,0 +1,18 @@
+
+from .core import *
+from . import core
+
+from .quick import *
+from . import quick
+
+__all__ = [
+    'bar',
+    'figure',
+    'jet',
+    'line',
+    'plot',
+    'point',
+    'to_figsize'
+]
+
+
