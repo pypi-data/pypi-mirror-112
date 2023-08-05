55a15ef11cdf7c3c91411244bb105b24f4f18ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:29 +0000

    Add package distributions-up/0.1/distributions_up-0.1.tar.gz

---
+++ packages/distributions-up/distributions_up-0.1.tar.gz/distributions_up-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_up',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_up'],
+      author = 'Jose Ceron',
+      author_email = 'jceronrodriguez10@gmail.com',
+      zip_safe=False)
