9a4f68f300bd5f012afc62916f2be6b358da623d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:04 +0000

    Add package django-render-static/1.1.1/django-render-static-1.1.1.tar.gz

---
+++ packages/django-render-static/django-render-static-1.1.1.tar.gz/django-render-static-1.1.1/render_static/__init__.py
@@ -0,0 +1,22 @@
+r"""
+    ____                 __             _____ __        __  _
+   / __ \___  ____  ____/ /__  _____   / ___// /_____ _/ /_(_)____
+  / /_/ / _ \/ __ \/ __  / _ \/ ___/   \__ \/ __/ __ `/ __/ / ___/
+ / _, _/  __/ / / / /_/ /  __/ /      ___/ / /_/ /_/ / /_/ / /__
+/_/ |_|\___/_/ /_/\__,_/\___/_/      /____/\__/\__,_/\__/_/\___/
+
+"""
+from .context import resolve_context
+from .resource import resource
+from .url_tree import ClassURLWriter, SimpleURLWriter, URLTreeVisitor
+
+VERSION = (1, 1, 1)
+
+__title__ = 'Django Render Static'
+__version__ = '.'.join(str(i) for i in VERSION)
+__author__ = 'Brian Kohan'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020-2021 Brian Kohan'
+
+
+default_app_config = 'render_static.apps.RenderStaticConfig'  # pylint: disable=C0103
