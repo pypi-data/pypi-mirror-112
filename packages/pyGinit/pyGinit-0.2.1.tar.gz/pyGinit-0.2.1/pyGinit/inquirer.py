8c189161710792abbd58ab219245648b42c6e0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:17 +0000

    Add package pyGinit/0.2.1/pyGinit-0.2.1.tar.gz

---
+++ packages/pyGinit/pyGinit-0.2.1.tar.gz/pyGinit-0.2.1/pyGinit/inquirer.py
@@ -0,0 +1,39 @@
+from prompt_toolkit.validation import Validator, ValidationError
+from .gitignoreList import gitignore
+import os
+import sys
+
+questions = [
+    {
+        "type": "input",
+        "name": "repo_name",
+        "message": "Enter the name of your repo : ",
+    },
+    {
+        "type": "input",
+        "name": "description",
+        "message": "Enter description of your repo (optional) : ",
+    },
+    {
+        "type": "list",
+        "name": "repo_type",
+        "message": "select your github repo type",
+        "choices": ["public", "private"],
+    },
+    {
+        "type": "list",
+        "name": "gitginore_template",
+        "message": "Select gitginore template for your repo",
+        "choices": gitignore,
+    },
+    {
+        "type": "confirm",
+        "name": "readme_confirm",
+        "message": "Do you want to create readme for your repo ? ",
+    },
+    {
+        "type": "input",
+        "name": "remote_name",
+        "message": "input your remote name name(default:origin)",
+    }
+]
