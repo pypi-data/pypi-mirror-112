5162fb56fcdb1253c96138d82ff8ce22d202ccda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:48 +0000

    Add package trapeza/0.0.1/trapeza-0.0.1.tar.gz

---
+++ packages/trapeza/trapeza-0.0.1.tar.gz/trapeza-0.0.1/trapeza/__init__.py
@@ -0,0 +1,7 @@
+# This prevents a 'very funny' bug... without this line, joblib.Parallel throws an exception saying it cannot pickle
+# PyCapsule object at FXEngine.run. No idea why it's working when importing joblib
+# here (or at least before importing FXEngine)
+import joblib
+from ._version import __version__ as version_str
+
+__version__ = version_str
