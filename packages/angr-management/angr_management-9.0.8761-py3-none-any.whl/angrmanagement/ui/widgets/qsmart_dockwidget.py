3317c9006ce6322e213a850801ec524958345747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:53 +0000

    Add package angr-management/9.0.8761/angr_management-9.0.8761-py3-none-any.whl

---
+++ packages/angr-management/angr_management-9.0.8761-py3-none-any.whl/angrmanagement/ui/widgets/qsmart_dockwidget.py
@@ -0,0 +1,26 @@
+from PySide2.QtWidgets import QDockWidget
+
+
+class QSmartDockWidget(QDockWidget):
+    def __init__(self, caption, parent=None, on_close=None):
+        super(QSmartDockWidget, self).__init__(caption, parent)
+
+        self.old_size = None
+        self.original_min = None
+        self.original_max = None
+        self._on_close = on_close
+
+    def restore_original_size(self):
+
+        if self.original_min is None or self.original_max is None:
+            return
+
+        self.setMinimumWidth(self.original_min.width())
+        self.setMinimumHeight(self.original_min.height())
+        self.setMaximumWidth(self.original_max.width())
+        self.setMaximumHeight(self.original_max.height())
+
+    def closeEvent(self, event):
+        if self._on_close is not None:
+            self._on_close()
+        return super().closeEvent(event)
