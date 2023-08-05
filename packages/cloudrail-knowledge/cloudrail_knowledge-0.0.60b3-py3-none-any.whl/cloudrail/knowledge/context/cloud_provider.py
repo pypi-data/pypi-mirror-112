b3a5dc430fecc2f8d1ca0c2014f302bcd19887a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:19 +0000

    Add package cloudrail-knowledge/0.0.60b3/cloudrail_knowledge-0.0.60b3-py3-none-any.whl

---
+++ packages/cloudrail-knowledge/cloudrail_knowledge-0.0.60b3-py3-none-any.whl/cloudrail/knowledge/context/cloud_provider.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class CloudProvider(str, Enum):
+    AMAZON_WEB_SERVICES = 'amazon_web_services'
+    AZURE = 'azure'
+    GCP = 'google_cloud_provider'
+
+    @staticmethod
+    def from_string(string):
+        if string.lower() == 'aws':
+            return CloudProvider.AMAZON_WEB_SERVICES
+        try:
+            return CloudProvider(string.lower())
+        except Exception:
+            return CloudProvider[string.upper()]
