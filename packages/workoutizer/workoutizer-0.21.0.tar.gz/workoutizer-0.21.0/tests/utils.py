2a109f42282e0967f19ebc18e30466b7ea674cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:33 +0000

    Add package workoutizer/0.21.0/workoutizer-0.21.0.tar.gz

---
+++ packages/workoutizer/workoutizer-0.21.0.tar.gz/workoutizer-0.21.0/tests/utils.py
@@ -0,0 +1,13 @@
+from tenacity import retry, stop_after_attempt, wait_exponential
+
+
+@retry(
+    reraise=True,
+    wait=wait_exponential(multiplier=3, min=1, max=5),
+    stop=stop_after_attempt(3),
+)
+def delayed_assertion(func, operator, right):
+    """
+    Helper function to check if a function evaluates to given value with a fixed number of retries.
+    """
+    assert operator(func(), right)
