4d3770b05f114edf715b26db7c936b102ba37e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package easy-i18n/1.1.1/easy_i18n-1.1.1.tar.gz

---
+++ packages/easy-i18n/easy_i18n-1.1.1.tar.gz/easy_i18n-1.1.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easy_i18n",
+    version="1.1.1",
+    author="aex",
+    author_email="aex.chen@qq.com",
+    description="an easy way come true i18n for python3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ALawating-Rex/easy_i18n",
+    packages=["easy_i18n"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
