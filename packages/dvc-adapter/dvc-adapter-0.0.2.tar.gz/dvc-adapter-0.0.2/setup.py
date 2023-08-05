15b90e008358f697516dead6c5a224ce1baedf98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:43 +0000

    Add package dvc-adapter/0.0.2/dvc-adapter-0.0.2.tar.gz

---
+++ packages/dvc-adapter/dvc-adapter-0.0.2.tar.gz/dvc-adapter-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+long_description = "OpenDataDiscovery DVC Adapter"
+
+setup(
+    name='dvc-adapter',
+    version='0.0.2',
+    author='Provectus team',
+    url='https://github.com/opendatadiscovery/odd-dvc-adapter',
+    description='OpenDataDiscovery DVC Adapter.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'dvc-adapter = adapter:main'
+        ]
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=requirements,
+    zip_safe=False
+)
