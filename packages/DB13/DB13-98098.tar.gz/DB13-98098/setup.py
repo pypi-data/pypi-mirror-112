73a6aa7f0cd359156555b08f57eefc8d12e3b712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:19 +0000

    Add package DB13/98098/DB13-98098.tar.gz

---
+++ packages/DB13/DB13-98098.tar.gz/DB13-98098/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DB13",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=2.7",
+    entry_points={
+    'console_scripts': [
+        'db12 = DIRACbenchmark.dirac_benchmark',
+    ],
+},
+)
