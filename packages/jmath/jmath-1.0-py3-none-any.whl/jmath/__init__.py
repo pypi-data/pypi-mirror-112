3c5e39a57e9556c751b5f0037f59d0f41e8b4a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:33 +0000

    Add package jmath/1.0/jmath-1.0-py3-none-any.whl

---
+++ packages/jmath/jmath-1.0-py3-none-any.whl/jmath/__init__.py
@@ -0,0 +1,14 @@
+'''
+    components/__init__.py
+
+    Author: Jordan Hay
+    Date: 2021-06-17
+
+    Initialises components sub-module
+'''
+
+# Import modules
+from discrete import *
+from linearalgebra import *
+from uncertainties import *
+from physics import *
