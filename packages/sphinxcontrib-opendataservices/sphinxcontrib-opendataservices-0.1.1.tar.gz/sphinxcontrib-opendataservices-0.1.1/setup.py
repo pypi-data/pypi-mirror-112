bfa6c23c2c1f8e874666564644bea63eadd494cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:36 +0000

    Add package sphinxcontrib-opendataservices/0.1.1/sphinxcontrib-opendataservices-0.1.1.tar.gz

---
+++ packages/sphinxcontrib-opendataservices/sphinxcontrib-opendataservices-0.1.1.tar.gz/sphinxcontrib-opendataservices-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='sphinxcontrib-opendataservices',
+    version='0.1.1',
+    author='Open Data Services',
+    author_email='code@opendataservices.coop',
+    packages=['sphinxcontrib'],
+    url='https://github.com/OpenDataServices/sphinxcontrib-opendataservices',
+    install_requires=[
+        'docutils',
+        'jsonpointer',
+        'myst-parser',
+        'recommonmark',
+        'sphinx',
+        'sphinxcontrib-opendataservices-jsonschema',
+    ],
+    extras_require={
+        'test': [
+            'coveralls',
+            'flake8',
+            'isort',
+            'lxml',
+            'pytest',
+            'pytest-cov',
+        ],
+    },
+    namespace_packages=['sphinxcontrib'],
+)
