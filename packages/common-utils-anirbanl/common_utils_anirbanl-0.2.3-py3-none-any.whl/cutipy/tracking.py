7815990a2f024e62bfb386d264816e2f2aabbd9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:19 +0000

    Add package common-utils-anirbanl/0.2.3/common_utils_anirbanl-0.2.3-py3-none-any.whl

---
+++ packages/common-utils-anirbanl/common_utils_anirbanl-0.2.3-py3-none-any.whl/cutipy/tracking.py
@@ -0,0 +1,46 @@
+import time, functools
+from collections import OrderedDict
+from cutipy.designpatterns import singleton
+
+@singleton
+class Timer:
+    def __init__(self):
+        self.dts = OrderedDict()
+
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, *exc):
+        end_time = time.time()
+        self.dts[self.name] = end_time - self.start_time
+        del self.start_time
+        del self.name
+
+    def register(self, name):
+        self.name = name
+
+    def __repr__(self):
+        s = ''
+        for k,v in self.dts.items():
+            s = s + f"{k} : {v} secs\n"
+        return s
+
+    def purge(self):
+        self.dts = OrderedDict()
+
+def timerdecorator(_func=None, *, timer=Timer()):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper_timer(*args, **kwargs):
+            with timer:
+                timer.register(f"Function {func.__name__!r} Call at {time.time()}")
+                value = func(*args, **kwargs)
+            return value
+
+        return wrapper_timer
+
+    if _func is None:
+        return decorator
+    else:
+        return decorator(_func)
