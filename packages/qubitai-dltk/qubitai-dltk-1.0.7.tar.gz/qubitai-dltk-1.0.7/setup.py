e39ad7d45fc4de35cf2e6c8110471a2e0e02f4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package qubitai-dltk/1.0.7/qubitai-dltk-1.0.7.tar.gz

---
+++ packages/qubitai-dltk/qubitai-dltk-1.0.7.tar.gz/qubitai-dltk-1.0.7/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qubitai-dltk",
+    version="1.0.7",
+    author="DLTK",
+    install_requires=[
+        "pandas==1.2.3",
+        "xlrd==2.0.1",
+        "numpy==1.20.2",
+        "requests==2.25.1",
+        "openpyxl==3.0.7",
+        "certifi==2020.12.5",
+        "pandas-profiling==2.11.0",
+        "scikit-learn==0.24.1",
+        "autoviz==0.0.81",
+        "dtale==1.41.1",
+        "umap-learn==0.5.1"
+        ],
+    author_email="connect@qubitai.tech",
+    description="Python Client for DLTK.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dltk-ai/qubitai-dltk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
