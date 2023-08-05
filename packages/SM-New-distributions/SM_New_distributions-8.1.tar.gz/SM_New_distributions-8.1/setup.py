fa3328ade056046cdcdfd0f18dc4eca3aeb42496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:20 +0000

    Add package SM-New-distributions/8.1/SM_New_distributions-8.1.tar.gz

---
+++ packages/SM-New-distributions/SM_New_distributions-8.1.tar.gz/SM_New_distributions-8.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='SM_New_distributions',
+      version='8.1',
+      description='Gaussian and Binomial distributions',
+      packages=['SM_New_distributions'],
+      zip_safe=False)
