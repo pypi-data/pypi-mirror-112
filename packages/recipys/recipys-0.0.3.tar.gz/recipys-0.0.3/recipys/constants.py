d37cb4c8bb9376fd0d13f383a69b4c14224b4f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:19 +0000

    Add package recipys/0.0.3/recipys-0.0.3.tar.gz

---
+++ packages/recipys/recipys-0.0.3.tar.gz/recipys-0.0.3/recipys/constants.py
@@ -0,0 +1,10 @@
+"""
+Global constants of application
+"""
+
+# Real time delay between two executions of application (ethical scraping)
+MIN_SECONDS_REQUEST: float = 10.0
+
+
+# Seconds to wait between requests to same endpoint (ethical scraping)
+WAIT_BETWEEN_PINGS: int = 1
