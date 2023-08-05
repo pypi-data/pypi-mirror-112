b93b884621da2e9005fd328f192743b929dc423a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:49 +0000

    Add package says-hello/0.0.15/says-hello-0.0.15.tar.gz

---
+++ packages/says-hello/says-hello-0.0.15.tar.gz/says-hello-0.0.15/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="says-hello", version="0.0.15", description="Returns 'Hello world'", author="Me", packages=find_packages(),
+      keywords=["hello", "python", "say"], long_description="Returns 'Hello world'", classifiers=[
+            "Development Status :: 1 - Planning",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Unix",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+    ]
+)
