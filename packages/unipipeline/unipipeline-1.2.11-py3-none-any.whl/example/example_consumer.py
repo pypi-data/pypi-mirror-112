bf3a5d8e51d2300dcfadbbb5ae1c9f02445a13b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:07 +0000

    Add package unipipeline/1.2.11/unipipeline-1.2.11-py3-none-any.whl

---
+++ packages/unipipeline/unipipeline-1.2.11-py3-none-any.whl/example/example_consumer.py
@@ -0,0 +1,32 @@
+import os.path
+
+from unipipeline import Uni
+
+
+# import logging
+# logging.basicConfig(
+#     level=os.environ.get('LOGLEVEL', logging.DEBUG),
+#     format='%(asctime)s | %(name)s | %(levelname)s | %(message)s'
+# )
+
+CWD = os.path.dirname(os.path.abspath(__file__))
+
+u = Uni(f"{CWD}/dag.yml")
+
+u.scaffold()
+
+u.check()
+
+u.init_producer_worker('input_worker')
+
+u.init_consumer_worker("input_worker")
+
+u.init_consumer_worker("my_super_cron_worker")
+
+u.initialize()
+
+u.send_to("input_worker", dict())
+
+u.start_consuming()
+
+u.start_cron()
---
+++ packages/unipipeline/unipipeline-1.2.11-py3-none-any.whl/unipipeline/modules/uni_message_meta.py
@@ -0,0 +1,67 @@
+import uuid
+from datetime import datetime
+from enum import Enum
+from typing import Dict, Any, Optional
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class UniMessageMetaErrTopic(Enum):
+    SYSTEM_ERR = 'error_system'
+    MESSAGE_PAYLOAD_ERR = 'error_message_payload'
+    HANDLE_MESSAGE_ERR = 'error_message_handling'
+    ERROR_HANDLING_ERR = 'error_handling'
+
+
+class UniMessageMetaErr(BaseModel):
+    error_topic: UniMessageMetaErrTopic
+    error_type: str
+    error_message: str
+    retry_times: int
+
+
+class UniMessageMeta(BaseModel):
+    id: UUID
+    date_created: datetime
+    payload: Dict[str, Any]
+
+    parent: Optional[Dict[str, Any]]
+    error: Optional[UniMessageMetaErr]
+
+    @property
+    def has_error(self) -> bool:
+        return self.error is not None
+
+    @staticmethod
+    def create_new(data: Dict[str, Any], error: Optional[UniMessageMetaErr] = None) -> 'UniMessageMeta':
+        return UniMessageMeta(
+            id=uuid.uuid4(),
+            date_created=datetime.now(),
+            payload=data,
+            parent=None,
+            error=error,
+        )
+
+    def create_child(self, payload: Dict[str, Any]) -> 'UniMessageMeta':
+        return UniMessageMeta(
+            id=uuid.uuid4(),
+            date_created=datetime.now(),
+            payload=payload,
+            parent=self.dict(),
+            error=None,
+        )
+
+    def create_error_child(self, error_topic: UniMessageMetaErrTopic, error: Exception) -> 'UniMessageMeta':
+        return UniMessageMeta(
+            id=self.id,
+            date_created=self.date_created,
+            payload=self.payload,
+            parent=self.dict(),
+            error=UniMessageMetaErr(
+                error_topic=error_topic,
+                error_type=type(error).__name__,
+                error_message=str(error),
+                retry_times=self.error.retry_times + 1 if self.error is not None else 0
+            ),
+        )
