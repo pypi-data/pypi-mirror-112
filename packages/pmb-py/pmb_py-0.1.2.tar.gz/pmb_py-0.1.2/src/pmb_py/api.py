d2e0ffb7cbc175a5d8d0fb55e24c9f946b6b7a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:13 +0000

    Add package pmb-py/0.1.2/pmb_py-0.1.2.tar.gz

---
+++ packages/pmb-py/pmb_py-0.1.2.tar.gz/pmb_py-0.1.2/src/pmb_py/api.py
@@ -0,0 +1,150 @@
+import pmb_py.core as core
+from pmb_py import PMB_MEMBER_STATUS, PmbError
+from pmb_py.adapter import pmb_convert
+from pmb_py.domain.model import QueryResult
+
+
+class pmb_mixin:
+    creator = None
+
+    @classmethod
+    def list(cls, *args, **kwargs) -> QueryResult:
+        query_re = cls.get_query(*args, **kwargs)
+        if not query_re:
+            return None
+        if type(query_re) == list:
+            items = query_re
+            _count = len(items) if items and len(items)<=1000 else 0
+            query_re = QueryResult(1000, '', '', list(), 1, _count)
+        else:
+            items = query_re.results
+        objects = [cls.creator(item) for item in items]
+        query_re.results = {object.id: object for object in objects}.values()
+        return query_re
+
+
+    @classmethod
+    def get_query(cls, *args, **kwargs):
+        pass
+
+    @classmethod
+    def get(cls, **kwargs):
+        pass
+        # raise PmbError("no support for these keyword")
+
+    @classmethod
+    def create_one(cls, *args, **kwargs):
+        return dict()
+
+    @classmethod
+    def create(cls, *args, **kwargs):
+        item = cls.create_one(*args, **kwargs)
+        if cls.creator:
+            obj = cls.creator(item)
+            return obj
+        else:
+            return item
+
+    @classmethod
+    def update_one(cls, obj_id, **kwargs):
+        return dict()
+
+    @classmethod
+    def update(cls, obj_id, **kwargs):
+        try:
+            item = cls.update_one(obj_id, **kwargs)
+        except Exception as e:
+            print(str(e))
+            return False
+        if cls.creator:
+            obj = cls.creator(item)
+            return obj
+        else:
+            return item
+
+
+class ProjectStatus(pmb_mixin):
+    creator = pmb_convert.to_project_status
+
+    @classmethod
+    def get_query(cls):
+        api = "web_project_status_and_types"
+        re = core._session.post(api)
+        return re["data"]["projectStatus"]
+
+
+class ProjectType(pmb_mixin):
+    creator = pmb_convert.to_project_type
+
+    @classmethod
+    def get_query(cls):
+        api = "web_project_status_and_types"
+        re = core._session.post(api)
+        return re["data"]["projectTypes"]
+
+
+class MemberStatus(pmb_mixin):
+    creator = pmb_convert.to_member_status
+
+    @classmethod
+    def get_query(cls):
+        status_list = list()
+        for k, v in PMB_MEMBER_STATUS.items():
+            status_list.append({"id": v[0], "name": v[1], "codeName": k})
+        return status_list
+
+
+class Projects(pmb_mixin):
+    creator = pmb_convert.to_project
+
+    @classmethod
+    def get_query(cls, *args, **kwargs):
+        api = 'projects'
+        re = core._session.get(api, params=kwargs)
+        return re
+
+
+class Tasks(pmb_mixin):
+    creator = pmb_convert.to_pmb_task
+
+    @classmethod
+    def get_query(cls, *args, **kwargs):
+        api = "tasks"
+        re = core._session.get(api, params=kwargs)
+        return re
+
+
+class Blocks(pmb_mixin):
+    creator = pmb_convert.to_pmb_block
+
+    @classmethod
+    def get_query(cls, *args, **kwargs):
+        api = "blocks"
+        re = core._session.get(api, params=kwargs)
+        return re
+
+
+class GanttItems(pmb_mixin):
+    creator = pmb_convert.to_pmb_gantt_item
+
+    @classmethod
+    def get_query(cls, *args, **kwargs):
+        api = 'gantt_items'
+        re = core._session.get(api, params=kwargs)
+        return re
+
+    @classmethod
+    def create_one(cls, *args, **kwargs):
+        api = 'gantt_items'
+        re = core._session.post(api, data=kwargs)
+        return re
+
+
+class TaskGroup(pmb_mixin):
+    creator = pmb_convert.to_pmb_task_group
+
+    @classmethod
+    def get_query(cls, *args, **kwargs):
+        api = 'task_groups'
+        re = core._session.get(api, params=kwargs)
+        return re
