c04e6a09a6c29b561b7db67722dcc9627536faa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:52 +0000

    Add package saurabhrajratan-distributions/0.1/saurabhrajratan_distributions-0.1.tar.gz

---
+++ packages/saurabhrajratan-distributions/saurabhrajratan_distributions-0.1.tar.gz/saurabhrajratan_distributions-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='saurabhrajratan_distributions',
+      version='0.1',
+      description='Gaussian Distributions, Binomial Distributions, Mean, Variance, and Standard Deviation of Statistics',
+      packages=['saurabhrajratan_distributions'],
+      author = 'Saurabh Raj Ratan',
+      author_email = 'saurabhrajratan@gmail.com',
+      zip_safe=False)
+
+
