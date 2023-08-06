c9056fcfc5ef5d79a265bd94609bf9d76aa9e682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:03 +0000

    Add package IVF-extremes/0.0.8/IVF_extremes-0.0.8.tar.gz

---
+++ packages/IVF-extremes/IVF_extremes-0.0.8.tar.gz/IVF_extremes-0.0.8/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(name='IVF_extremes',
+      version='0.0.8',
+      packages=['IVF_extremes','IVF_extremes/test'],
+      author='Matteo Figliuzzi',
+      author_email='matteo.figliuzzi@igenomix.com',
+      install_requires=required,
+      entry_points={
+        "console_scripts": [
+            "IVF_extremes_batch=IVF_extremes.batch_check:CLI_batch",
+            "IVF_extremes=IVF_extremes.single_check:CLI_single_check",
+        ]},
+      data_files=['data/Sample_IVF_data.csv','requirements.txt','data/baseline_rates.csv']
+      )
