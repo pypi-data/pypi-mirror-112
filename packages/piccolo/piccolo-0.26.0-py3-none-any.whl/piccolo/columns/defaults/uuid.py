71cfef9f4e7360364f143120af1d85f420c8f551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:41 +0000

    Add package piccolo/0.26.0/piccolo-0.26.0-py3-none-any.whl

---
+++ packages/piccolo/piccolo-0.26.0-py3-none-any.whl/piccolo/columns/defaults/uuid.py
@@ -0,0 +1,24 @@
+from enum import Enum
+import typing as t
+import uuid
+
+from .base import Default
+
+
+class UUID4(Default):
+    @property
+    def postgres(self):
+        return "uuid_generate_v4()"
+
+    @property
+    def sqlite(self):
+        return "''"
+
+    def python(self):
+        return uuid.uuid4()
+
+
+UUIDArg = t.Union[UUID4, uuid.UUID, str, Enum, None]
+
+
+__all__ = ["UUIDArg", "UUID4"]
