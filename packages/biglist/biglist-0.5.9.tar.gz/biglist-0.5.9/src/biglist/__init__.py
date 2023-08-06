b72a1d9048f103b1c6536ae7875da1048b444e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:07 +0000

    Add package biglist/0.5.9/biglist-0.5.9.tar.gz

---
+++ packages/biglist/biglist-0.5.9.tar.gz/biglist-0.5.9/src/biglist/__init__.py
@@ -0,0 +1,12 @@
+from ._biglist import Biglist, ListView, FileView, FileIterStat
+from ._serializer import Serializer
+
+
+__version__ = '0.5.9'
+
+
+__all__ = [
+    'Biglist', 'ListView', 'FileView',
+    'Serializer',
+    'FileIterStat',
+]
