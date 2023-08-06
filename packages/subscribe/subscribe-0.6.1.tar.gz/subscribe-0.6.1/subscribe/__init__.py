cc8f231cae71ae75e046cea2d6b03dfbf6506397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:16 +0000

    Add package subscribe/0.6.1/subscribe-0.6.1.tar.gz

---
+++ packages/subscribe/subscribe-0.6.1.tar.gz/subscribe-0.6.1/subscribe/__init__.py
@@ -0,0 +1,7 @@
+from subscribe.subscribers import Command, Event  # noqa
+from subscribe.subscriptions import (  # noqa
+    ClassSubscriptionList,
+    Subscription,
+    SubscriptionList,
+    SuperClassSubscriptionList,
+)
