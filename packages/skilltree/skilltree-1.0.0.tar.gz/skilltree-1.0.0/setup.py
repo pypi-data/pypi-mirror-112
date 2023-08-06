3d6fee3816be885262f94c54bf60ebb05a892b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:32 +0000

    Add package skilltree/1.0.0/skilltree-1.0.0.tar.gz

---
+++ packages/skilltree/skilltree-1.0.0.tar.gz/skilltree-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.rst", "r") as f:
+  long_description = f.read()
+
+setup(name='skilltree',  # 包名
+      version='1.0.0',  # 版本号
+      description='skills for material physics',
+      long_description=long_description,
+      author='zhexian zhang',
+      author_email='brienzhang@sina.com',
+      url='',
+      install_requires=['pygame','re'],
+      license='MIT License',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
