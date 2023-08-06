7d2f15a6ac6e58affcca5b8522db7286889aef18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:18 +0000

    Add package scratchclient/0.4.8/scratchclient-0.4.8.tar.gz

---
+++ packages/scratchclient/scratchclient-0.4.8.tar.gz/scratchclient-0.4.8/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+from distutils.core import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'scratchclient',
+  packages = ['scratchclient'],
+  version = '0.4.8',
+  license='MIT',
+  description = 'A scratch API wrapper for Python.',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'CubeyTheCube',
+  author_email = 'turtles142857@gmail.com',
+  url = 'https://github.com/CubeyTheCube/scratchclient',
+  download_url = 'https://github.com/CubeyTheCube/scratchclient/archive/v_04.8.tar.gz',
+  keywords = ['scratch', 'api'],
+  install_requires=[
+          'requests',
+          'websocket-client',
+          'numpy',
+          'wsaccel',
+          'pymitter'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
