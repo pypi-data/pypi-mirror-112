b1b5799ff59e3796120d809681d7090dd49dd287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:52 +0000

    Add package PySDM-examples/1.5/PySDM-examples-1.5.tar.gz

---
+++ packages/PySDM-examples/PySDM-examples-1.5.tar.gz/PySDM-examples-1.5/README.md
@@ -0,0 +1,2 @@
+This repository stores example files for PySDM.
+See [PySDM README.md](https://github.com/atmos-cloud-sim-uj/PySDM/blob/master/README.md) file for details.
---
+++ packages/PySDM-examples/PySDM-examples-1.5.tar.gz/PySDM-examples-1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PySDM-examples',
+    version='1.5',
+    setup_requires=['setuptools_scm'],
+    install_requires=['PySDM',
+                      'PyMPDATA',
+                      'pystrict',
+                      'matplotlib',
+                      'ipywidgets',
+                      'ghapi'],  # note: test-time-requirement?
+    author='https://github.com/orgs/atmos-cloud-sim-uj/people',
+    license="GPL-3.0",
+    packages=find_packages(include=['PySDM_examples', 'PySDM_examples.*'])
+)
