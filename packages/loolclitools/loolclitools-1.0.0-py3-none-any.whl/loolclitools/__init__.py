aaa09b275b9eeae911ee0b0fd9d1705782571e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:03 +0000

    Add package loolclitools/1.0.0/loolclitools-1.0.0-py3-none-any.whl

---
+++ packages/loolclitools/loolclitools-1.0.0-py3-none-any.whl/loolclitools/__init__.py
@@ -0,0 +1,27 @@
+# lool CLI Tools #
+
+'''
+    This module brings some tools for building CLIs.
+'''
+
+__version__ = '1.0.0'  # last edit: 10.07.2021
+
+__all__ = ['enable_ANSI_esc_seq', 'getch', 'CursorPosition',
+           'get_cursor_position', 'out', 'flush', 'yesno',
+           'param', 'vline', 'Selector', 'askinput', 'askpath',
+           'console_input', 'notepad_input', 'getpass', 'pause',
+           'Timer', 'InteractiveConsole']
+
+from ._main import (
+    enable_ANSI_esc_seq, getch, CursorPosition,
+    get_cursor_position, out, flush, yesno,
+    param, vline, pause, Timer
+)
+
+from ._selector import Selector
+
+from ._interactive_console import InteractiveConsole
+
+from ._input import askinput, askpath, console_input, notepad_input
+
+from ._getpass import getpass
