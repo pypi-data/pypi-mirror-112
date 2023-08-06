89c82599801fbecf13eb7d28847aea9e42fc450d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:16 +0000

    Add package mayarepacker/0.0.6/mayarepacker-0.0.6-py2-none-any.whl

---
+++ packages/mayarepacker/mayarepacker-0.0.6-py2-none-any.whl/mayarepacker/app.py
@@ -0,0 +1,31 @@
+import logging
+import sys
+
+import maya
+
+from functools import partial
+
+
+def remove_include_module(name):
+    logger = logging.getLogger("mayarepacker")
+
+    module_list = [_ for _ in sys.modules]
+    module_list.sort()
+
+    logger.info("================================================")
+
+    first_hit_parent_module = None
+    for k in module_list:
+        if k.startswith(name):
+            if first_hit_parent_module is None:
+                first_hit_parent_module = k
+
+            del sys.modules[k]
+            # send_message_list.append("remove module : {}".format(k))
+            logger.info("remove module : {}".format(k))
+
+    if first_hit_parent_module:
+        exec("import {}".format(first_hit_parent_module))
+        logger.info("reloaded : {}".format(first_hit_parent_module))
+
+    logger.info("================================================")
