8ef1da940c279925a64bfaa7a9c952d228689c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:56 +0000

    Add package MangasOrigines/0.0.1/MangasOrigines-0.0.1.tar.gz

---
+++ packages/MangasOrigines/MangasOrigines-0.0.1.tar.gz/MangasOrigines-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8", errors="ignore") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="MangasOrigines",
+    version="0.0.1",
+    author="Asthowen",
+    description="A script for download scans on https://mangas-origines.fr/ written in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Asthowen/MangasOriginesDownloader",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": ['mangas_origines = mangas_origines.mangas_origines:start']
+    },
+    python_requires='>= 3.6',
+    include_package_data=True,
+    install_requires=['aiohttp', 'aiofiles', 'bs4']
+)
