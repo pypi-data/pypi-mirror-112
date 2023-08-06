d902d2fb71bb43333c6dcab2e71eb752abbfc2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:53 +0000

    Add package yarp-middleware/3.4.7.dev907/yarp_middleware-3.4.7.dev907.tar.gz

---
+++ packages/yarp-middleware/yarp_middleware-3.4.7.dev907.tar.gz/yarp_middleware-3.4.7.dev907/example/ros/listener_v2.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#include <yarp/os/Bottle.h>
+#include <yarp/os/LogComponent.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Node.h>
+#include <yarp/os/Port.h>
+
+using yarp::os::Bottle;
+using yarp::os::Network;
+using yarp::os::Node;
+using yarp::os::Port;
+
+namespace {
+YARP_LOG_COMPONENT(LISTENER_V2, "yarp.example.ros.listener_v2")
+}
+
+int main(int argc, char* argv[])
+{
+    YARP_UNUSED(argc);
+    YARP_UNUSED(argv);
+
+    Network yarp;
+    Node node("/yarp/listener");
+    Port port;
+    port.setReadOnly();
+    if (!port.open("chatter")) {
+        yCError(LISTENER_V2, "Failed to open port");
+        return 1;
+    }
+
+    while (true) {
+        Bottle msg;
+        if (!port.read(msg)) {
+            yCError(LISTENER_V2, "Failed to read msg");
+            continue;
+        }
+        yCInfo(LISTENER_V2, "Got [%s]", msg.get(0).asString().c_str());
+    }
+
+    return 0;
+}
---
+++ packages/yarp-middleware/yarp_middleware-3.4.7.dev907.tar.gz/yarp_middleware-3.4.7.dev907/src/libYARP_dev/src/yarp/dev/IFrameGrabber.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#define YARP_INCLUDING_DEPRECATED_HEADER_YARP_DEV_IFRAMEGRABBER_H_ON_PURPOSE
+#include <yarp/dev/IFrameGrabber.h>
+#undef YARP_INCLUDING_DEPRECATED_HEADER_YARP_DEV_IFRAMEGRABBER_H_ON_PURPOSE
+
+yarp::dev::IFrameGrabber::~IFrameGrabber() = default;
