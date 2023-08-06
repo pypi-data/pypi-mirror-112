29741059605a8a3c03f32c7d5c52fbd9e8d80c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:51 +0000

    Add package yachalk/0.0.1/yachalk-0.0.1.tar.gz

---
+++ packages/yachalk/yachalk-0.0.1.tar.gz/yachalk-0.0.1/yachalk/__init__.py
@@ -0,0 +1,10 @@
+from .chalk_instance import chalk, create_chalk
+from .chalk_factory import ChalkFactory
+
+VERSION = "0.0.1"
+
+__all__ = [
+    "chalk",
+    "create_chalk",
+    "ChalkFactory",
+]
