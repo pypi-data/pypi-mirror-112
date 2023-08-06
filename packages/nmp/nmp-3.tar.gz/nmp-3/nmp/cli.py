2b9ed69ea8bda06b09eadde8ac3e079a9e5f428b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:37 +0000

    Add package nmp/3/nmp-3.tar.gz

---
+++ packages/nmp/nmp-3.tar.gz/nmp-3/nmp/cli.py
@@ -0,0 +1,37 @@
+import argparse
+
+from nmp.model_logic_extractor import create_model
+from vaip import model_deployer
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="nmp",
+                                     description="Notebooks Model Publisher")
+    parser.add_argument("--tag", type=str, dest="tag", required=True,
+                                help="Docker Tag To Use With Build And/Or Deploy")
+    parser.add_argument("action", choices=["build", "deploy"],
+                                help="action")
+    builder_group = parser.add_argument_group("build")
+    builder_group.add_argument("--path", type=str, dest="path", default=".",
+                                help="path with Notebook and other files")
+    builder_group.add_argument("--notebook", type=str, dest="notebook",
+                                help="notebook name (should be in the path)",
+                                default="main.ipynb")
+    deployer_group = parser.add_argument_group("deploy")
+    deployer_group.add_argument("--project", type=str, dest="project",
+                                help="GCP Project (Vertex AI API should be enabled)")
+    deployer_group.add_argument("--location", type=str, dest="location",
+                                help="Vertex AI location")
+    deployer_group.add_argument("--name", type=str, dest="name",
+                                help="model name")
+
+    args = parser.parse_args()
+
+    if args.action == "build":
+        create_model(args.tag, args.path, args.notebook)
+    if args.action == "deploy":
+        model_deployer.deploy_model(args.project, args.location, args.name, args.tag)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/nmp/nmp-3.tar.gz/nmp-3/setup.py
@@ -0,0 +1,25 @@
+import sys
+
+from setuptools import setup, find_packages
+
+
+if sys.version_info < (3, 8):
+    sys.exit("Sorry, Python < 3.8 is not yet supported")
+
+
+setup(
+    name="nmp",
+    version="3",
+    author="Viacheslav Kovalevskyi",
+    author_email="viacheslav@kovalevskyi.com",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "nmp = nmp.cli:main"
+        ]
+    },
+    url="http://pypi.python.org/pypi/nmp/",
+    license="LICENSE",
+    description="Notebook Model Packager for Vertex AI",
+    install_requires=["nbformat==5.1.*", "vaip==3.1"],
+)
