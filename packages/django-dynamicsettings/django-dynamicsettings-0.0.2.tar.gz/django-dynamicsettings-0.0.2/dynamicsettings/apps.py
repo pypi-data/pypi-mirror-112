f042a3920e10fd5daccea7df9996845b02dbd637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:22 +0000

    Add package django-dynamicsettings/0.0.2/django-dynamicsettings-0.0.2.tar.gz

---
+++ packages/django-dynamicsettings/django-dynamicsettings-0.0.2.tar.gz/django-dynamicsettings-0.0.2/dynamicsettings/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DynamicSettingsConfig(AppConfig):
+    name = 'dynamicsettings'
+    verbose_name = 'Dynamic Settings'
+    default_auto_field = 'django.db.models.AutoField'
