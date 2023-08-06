cad02bea0876ba58a3ffa6f15ca05aa2e120ad5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:24 +0000

    Add package carculator-truck/0.2.3/carculator_truck-0.2.3-py3-none-any.whl

---
+++ packages/carculator-truck/carculator_truck-0.2.3-py3-none-any.whl/carculator_truck/utils.py
@@ -0,0 +1,66 @@
+import itertools
+
+import pandas as pd
+
+from . import DATA_DIR
+
+pd.options.mode.chained_assignment = None
+
+REGION_MAPPING_FILEPATH = DATA_DIR / "regionmappingH12.csv"
+IAM_ELEC_MARKETS = DATA_DIR / "electricity_markets.csv"
+IEA_DIESEL_SHARE = DATA_DIR / "diesel_share_oecd.csv"
+
+
+def build_fleet_array(fp, scope):
+    """
+    Receives a file path that points to a CSV file that contains the fleet composition
+    Checks that the fleet composition array is valid.
+
+    Specifically:
+
+    * the years specified in the fleet must be present in scope["year"]
+    * the powertrains specified in the fleet must be present in scope["powertrain"]
+    * the sizes specified in the fleet must be present in scope["size"]
+    * the sum for each year-powertrain-size set must equal 1
+
+    :param fp: filepath to an array that contains fleet composition
+    :type fp: str
+    :return array: fleet composition array
+    :rtype array: xarray.DataArray
+    """
+    arr = pd.read_csv(fp, delimiter=";", header=0, index_col=[0, 1, 2])
+    arr = arr.fillna(0)
+    arr.columns = [int(c) for c in arr.columns]
+
+    new_cols = [c for c in scope["year"] if c not in arr.columns]
+    arr[new_cols] = pd.DataFrame([[0] * len(new_cols)], index=arr.index)
+
+    a = [scope["powertrain"]] + [scope["size"]] + [scope["year"]]
+
+    for row in [i for i in list(itertools.product(*a)) if i not in arr.index]:
+        arr.loc[row] = 0
+
+    array = arr.to_xarray()
+    array = array.rename(
+        {"level_0": "powertrain", "level_1": "size", "level_2": "vintage_year"}
+    )
+
+    if not set(list(array.data_vars)).issubset(scope["year"]):
+        raise ValueError("The fleet years differ from {}".format(scope["year"]))
+
+    if set(scope["year"]) != set(array.coords["vintage_year"].values.tolist()):
+        raise ValueError(
+            "The list of vintage years differ from {}.".format(scope["year"])
+        )
+
+    if not set(array.coords["powertrain"].values.tolist()).issubset(
+        scope["powertrain"]
+    ):
+        raise ValueError(
+            "The fleet powertrain list differs from {}".format(scope["powertrain"])
+        )
+
+    if not set(array.coords["size"].values.tolist()).issubset(scope["size"]):
+        raise ValueError("The fleet size list differs from {}".format(scope["size"]))
+
+    return array.to_array().fillna(0)
