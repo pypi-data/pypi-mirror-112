c52f715ba0ec518fb53fc5e7770bd3cf9c422c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:31 +0000

    Add package pun-probability/0.1/pun_probability-0.1.tar.gz

---
+++ packages/pun-probability/pun_probability-0.1.tar.gz/pun_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pun_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['pun_probability'],
+      author = 'Tawatwee Aiem',
+      author_email = 'punnyoz1103@gmail.com',
+      zip_safe=False)
