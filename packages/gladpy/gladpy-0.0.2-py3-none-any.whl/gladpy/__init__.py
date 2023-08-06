41934efc097bfaae092dbab4197e36c39751e1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:10 +0000

    Add package gladpy/0.0.2/gladpy-0.0.2-py3-none-any.whl

---
+++ packages/gladpy/gladpy-0.0.2-py3-none-any.whl/gladpy/__init__.py
@@ -0,0 +1,5 @@
+DESCRIPTION = "GladPy is a small module in Python that is used to adding extensions for Python. It is also used for HTML as it generates HTML elements."
+LONG_DESCRIPTION = """GladPy is a small module in Python that is used to adding extensions for Python. It is also used for HTML as it generates HTML elements.
+GladPy is also usable for creating variables. Instead of classes, it uses only functions. Thanks for using GladPy! Made by pypi.org user kareemPypi (v0.0.1)."""
+__version__ = 0.01
+from gladpy.gladpy import *
