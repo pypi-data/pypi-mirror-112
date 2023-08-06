1aef277a6872aeb0288a04a4547f1303f49593c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:34 +0000

    Add package more-executors/2.7.0/more_executors-2.7.0-py2.py3-none-any.whl

---
+++ packages/more-executors/more_executors-2.7.0-py2.py3-none-any.whl/more_executors/_impl/helpers.py
@@ -0,0 +1,20 @@
+import logging
+from functools import wraps
+
+from .logwrap import LogWrapper
+
+LOG = LogWrapper(logging.getLogger(__name__))
+
+
+def executor_loop(fn):
+    @wraps(fn)
+    def out(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except RuntimeError as error:
+            if "cannot schedule new futures after" in str(error):
+                LOG.debug("Ignoring error due to interpreter shutdown", exc_info=1)
+                return
+            raise
+
+    return out
