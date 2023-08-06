4db94a89676d2736781b1cff2ffb6d52f1d2704c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:46 +0000

    Add package jet-sidebar/0.1.4/jet-sidebar-0.1.4.tar.gz

---
+++ packages/jet-sidebar/jet-sidebar-0.1.4.tar.gz/jet-sidebar-0.1.4/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="jet-sidebar",
+    version="0.1.4",
+    author="Raifran Lucas",
+    author_email="contato@raifranlucas.dev",
+    description="Este pacote é uma modificação para o Django Jet",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/raifran1/jet-sidebar",
+    project_urls={
+        "Bug Tracker": "https://github.com/raifran1/jet-sidebar/issues",
+    },
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(),
+    python_requires=">=2.6",
+)
