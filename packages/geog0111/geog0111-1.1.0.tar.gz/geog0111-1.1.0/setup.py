99aad8e72fe2d26d095d841c161e325fbec7b282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:03 +0000

    Add package geog0111/1.1.0/geog0111-1.1.0.tar.gz

---
+++ packages/geog0111/geog0111-1.1.0.tar.gz/geog0111-1.1.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+'''
+setup.py
+'''
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup = {
+    'url':'https://github.com/UCL-EO/geog0111',
+    'version':'1.1.0',
+    'name':'geog0111',
+    'description':'UCL Geography MSc notes',
+    'author': 'P. Lewis and J. Gomez-Dans',
+    'author_email':'p.lewis@ucl.ac.uk',
+    'license':'MIT',
+    'keywords':'scientific computing',
+    'long_description':long_description,
+    'long_description_content_type':"text/markdown",
+    'packages':setuptools.find_packages(),
+    'classifiers':[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    'python_requires':'>=3.6',
+}
+
+setuptools.setup(**setup) 
+
