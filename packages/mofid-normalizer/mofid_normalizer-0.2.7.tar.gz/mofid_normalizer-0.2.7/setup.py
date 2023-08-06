8b4cc5c74013bc12defb4c3965a727eeea92cfa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:41 +0000

    Add package mofid-normalizer/0.2.7/mofid_normalizer-0.2.7.tar.gz

---
+++ packages/mofid-normalizer/mofid_normalizer-0.2.7.tar.gz/mofid_normalizer-0.2.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(
+    name='mofid_normalizer',
+    packages=find_packages(include=['mofid_normalizer']),
+    version='0.2.7',
+    install_requires=["nltk","num2fawords","spacy"],
+    description='first version of Mofid Normalizer',
+    author='ali96ebrahimi@gmail.com',
+    license='MIT',
+    include_package_data=True
+)
