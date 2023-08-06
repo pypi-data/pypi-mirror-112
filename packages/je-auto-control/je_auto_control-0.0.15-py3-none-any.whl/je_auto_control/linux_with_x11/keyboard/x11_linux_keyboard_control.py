3a0cd31762eda67bb0efa32db9e3d3e9d994e7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:00 +0000

    Add package je-auto-control/0.0.15/je_auto_control-0.0.15-py3-none-any.whl

---
+++ packages/je-auto-control/je_auto_control-0.0.15-py3-none-any.whl/je_auto_control/linux_with_x11/keyboard/x11_linux_keyboard_control.py
@@ -0,0 +1,13 @@
+from je_auto_control.linux_with_x11.core.utils.x11_linux_display import display
+from Xlib.ext.xtest import fake_input
+from Xlib import X
+
+
+def press_key(keycode):
+    fake_input(display, X.KeyPress, keycode)
+    display.sync()
+
+
+def release_key(keycode):
+    fake_input(display, X.KeyRelease, keycode)
+    display.sync()
