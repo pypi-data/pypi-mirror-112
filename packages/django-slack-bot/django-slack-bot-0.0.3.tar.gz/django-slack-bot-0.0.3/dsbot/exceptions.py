d1d0c3bc15efb81e6b4fdee45e9b41cd3b44048d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:37 +0000

    Add package django-slack-bot/0.0.3/django-slack-bot-0.0.3.tar.gz

---
+++ packages/django-slack-bot/django-slack-bot-0.0.3.tar.gz/django-slack-bot-0.0.3/dsbot/exceptions.py
@@ -0,0 +1,32 @@
+from slack.errors import SlackApiError
+
+
+class BaseException(Exception):
+    pass
+
+
+class CommandError(BaseException):
+    pass
+
+
+class ArgumentError(CommandError):
+    pass
+
+
+class ArchiveException(SlackApiError):
+    pass
+
+
+class ChannelNotFound(SlackApiError):
+    pass
+
+
+class SlackException(BaseException):
+    def __init__(self, response):
+        self.data = response
+
+    def __str__(self):
+        return self.data["error"]
+
+    def __repr__(self):
+        return "SlackException(%s)" % self
