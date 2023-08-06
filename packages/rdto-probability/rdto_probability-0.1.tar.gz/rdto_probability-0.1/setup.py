cbadc784e93752f576bd99178efcad2f2aaa2ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:43 +0000

    Add package rdto-probability/0.1/rdto_probability-0.1.tar.gz

---
+++ packages/rdto-probability/rdto_probability-0.1.tar.gz/rdto_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rdto_probability',
+      version='0.1',
+      description='Gaussian rdto_probability',
+      packages=['rdto_probability'],
+      author='Reuven Duer',
+      author_email='ruvduer@gmail.com',
+      zip_safe=False)
