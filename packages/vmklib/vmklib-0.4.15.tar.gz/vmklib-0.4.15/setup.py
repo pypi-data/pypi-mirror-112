4554689bd3971a392895a2448ce6b637b4e2841f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:40 +0000

    Add package vmklib/0.4.15/vmklib-0.4.15.tar.gz

---
+++ packages/vmklib/vmklib-0.4.15.tar.gz/vmklib-0.4.15/setup.py
@@ -0,0 +1,38 @@
+# =====================================
+# generator=datazen
+# version=1.7.8
+# hash=aaa4b7bc6bba37cfb6ffa976520c717f
+# =====================================
+
+"""
+vmklib - Package definition for distribution.
+"""
+
+# third-party
+from vmklib.setup import setup
+
+# internal
+from vmklib import PKG_NAME, VERSION, DESCRIPTION
+
+
+author_info = {
+    "name": "Vaughn Kottler",
+    "email": "vaughnkottler@gmail.com",
+    "username": "vkottler",
+}
+pkg_info = {
+    "name": PKG_NAME,
+    "version": VERSION,
+    "description": DESCRIPTION,
+    "versions": [
+        "3.6",
+        "3.7",
+        "3.8",
+        "3.9",
+    ],
+}
+setup(
+    pkg_info,
+    author_info,
+    entry_override="mk",
+)
---
+++ packages/vmklib/vmklib-0.4.15.tar.gz/vmklib-0.4.15/vmklib/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=1.7.8
+# hash=fc1de2dbbbb9c3ce958049a374d994f7
+# =====================================
+
+"""
+vmklib - Useful defaults and other package metadata.
+"""
+
+PKG_NAME = "vmklib"
+VERSION = "0.4.15"
+DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
