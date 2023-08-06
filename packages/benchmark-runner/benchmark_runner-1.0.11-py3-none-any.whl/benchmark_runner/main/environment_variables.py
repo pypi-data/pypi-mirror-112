f53d8be3392b349005a4ad4e49583ee78b2875b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:36 +0000

    Add package benchmark-runner/1.0.11/benchmark_runner-1.0.11-py3-none-any.whl

---
+++ packages/benchmark-runner/benchmark_runner-1.0.11-py3-none-any.whl/benchmark_runner/main/environment_variables.py
@@ -0,0 +1,33 @@
+
+import os
+
+# when adding new environment variables update environment_variables_dict
+environment_variables_dict = {}
+
+workloads_list = ['hammerdb_pod_mariadb', 'hammerdb_pod_mssql', 'hammerdb_pod_postgres',
+                  'hammerdb_vm_mariadb', 'hammerdb_vm_mssql', 'hammerdb_vm_postgres',
+                  'stressng_pod', 'stressng_vm',
+                  'uperf_pod', 'uperf_vm']
+
+# os.environ['KUBECONFIG'] = ''
+environment_variables_dict['KUBECONFIG'] = os.environ.get('KUBECONFIG', '')
+
+# os.environ['KUBEADMIN_PASSWORD'] = ''
+environment_variables_dict['KUBEADMIN_PASSWORD'] = os.environ.get('KUBEADMIN_PASSWORD', '')
+
+# os.environ['workload'] = ''
+environment_variables_dict['workload'] = os.environ.get('workload', '')
+
+# os.environ['elasticsearch'] = ''
+environment_variables_dict['elasticsearch'] = os.environ.get('elasticsearch', '')
+
+# os.environ['elasticsearch_port'] = ''
+environment_variables_dict['elasticsearch_port'] = os.environ.get('elasticsearch_port', '')
+
+# node selectors
+# os.environ['pin_node1'] = ''
+environment_variables_dict['pin_node1'] = os.environ.get('pin_node1', '')
+
+# i.e. uperf server node
+# os.environ['pin_node2'] = ''
+environment_variables_dict['pin_node2'] = os.environ.get('pin_node2', '')
