f56f2eb38879959091d08562023620850f5f86aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:43 +0000

    Add package rer.ufficiostampa/1.0.1/rer.ufficiostampa-1.0.1.tar.gz

---
+++ packages/rer.ufficiostampa/rer.ufficiostampa-1.0.1.tar.gz/rer.ufficiostampa-1.0.1/CHANGES.rst
@@ -0,0 +1,16 @@
+Changelog
+=========
+
+
+1.0.1 (2021-07-09)
+------------------
+
+- Fix import for python2 and sort indexes.
+  [cekk]
+
+
+1.0.0 (2021-05-26)
+------------------
+
+- Initial release.
+  [cekk]
---
+++ packages/rer.ufficiostampa/rer.ufficiostampa-1.0.1.tar.gz/rer.ufficiostampa-1.0.1/src/rer/ufficiostampa/restapi/services/send_history/get.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from rer.ufficiostampa.interfaces import ISendHistoryStore
+from rer.ufficiostampa.restapi.services.common import DataGet
+from rer.ufficiostampa.restapi.services.common import DataCSVGet
+from rer.ufficiostampa.restapi.services.common import DataClear
+
+
+class SendHistoryGet(DataGet):
+    store = ISendHistoryStore
+
+
+class SendHistoryCSVGet(DataCSVGet):
+    store = ISendHistoryStore
+    type = "history"
+    columns = [
+        "status",
+        "type",
+        "date",
+        "completed_date",
+        "recipients",
+        "channels",
+        "subject",
+    ]
+
+
+class SendHistoryClearGet(DataClear):
+    store = ISendHistoryStore
