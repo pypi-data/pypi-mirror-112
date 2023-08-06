b1deebe31a89a991b1bfc83ed9298d45728f3028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:45 +0000

    Add package berry-video/0.0.3/berry_video-0.0.3.tar.gz

---
+++ packages/berry-video/berry_video-0.0.3.tar.gz/berry_video-0.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(
+    name='berry_video',
+    version='0.0.3',
+    scripts=['script/berry'],
+    author='Mr Menezes',
+    author_email='sr.tama@outlook.com',
+    license='@copyright',
+    description='Tools of berry video',
+    packages=['berry_video'],
+    install_requires=[
+        'setuptools',
+        'google-api-python-client==2.12.0',
+        'oauth2client==4.1.3',
+        'python_crontab==2.5.1',
+    ],
+    python_requires='>=3.5'
+)
