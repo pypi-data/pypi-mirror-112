46305690d85e3129d20572daf60290636242a90b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:40 +0000

    Add package dinh-distributions/0.1/dinh_distributions-0.1.tar.gz

---
+++ packages/dinh-distributions/dinh_distributions-0.1.tar.gz/dinh_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dinh_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dinh_distributions'],
+      zip_safe=False)
