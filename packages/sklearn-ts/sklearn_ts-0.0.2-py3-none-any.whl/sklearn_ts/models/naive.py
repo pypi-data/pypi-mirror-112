806598035e845d24bad0c62fa1829d007e06701d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:24 +0000

    Add package sklearn-ts/0.0.2/sklearn_ts-0.0.2-py3-none-any.whl

---
+++ packages/sklearn-ts/sklearn_ts-0.0.2-py3-none-any.whl/sklearn_ts/models/naive.py
@@ -0,0 +1,35 @@
+from sklearn.base import BaseEstimator, RegressorMixin
+from sktime.forecasting.naive import NaiveForecaster
+import numpy as np
+
+
+class NaiveRegressor(BaseEstimator, RegressorMixin):
+
+    def __init__(self, strategy='last', sp=7):
+        self.strategy = strategy
+        self.sp = sp
+
+        self.model = None
+        self.y_ = None
+
+    def fit(self, X, y):
+        forecaster = NaiveForecaster(strategy=self.strategy, sp=self.sp)
+
+        # TODO inference frequency
+        y = y.asfreq('D')
+        self.y_ = y
+        forecaster.fit(y)
+        self.model = forecaster
+        return self
+
+    def predict(self, X):
+        # TODO not valid for fitting to train
+        return self.model.predict(np.arange(X.shape[0]) + 1)
+
+    def get_params(self, deep=True):
+        return {'strategy': self.strategy, 'sp': self.sp}
+
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
+        return self
