ed81e27e65cfa81f629540656e4d8cf058672d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:10 +0000

    Add package Py-Trading/0.3.21/Py_Trading-0.3.21-py3-none-any.whl

---
+++ packages/Py-Trading/Py_Trading-0.3.21-py3-none-any.whl/py_trading/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+            # for article in googlenews.result():
+            #     sector_news.append(article.get_texts(), article.get_links())
+            # sector_news.append([ i for i in zip(googlenews.get_texts(), googlenews.get_links())])
+            
