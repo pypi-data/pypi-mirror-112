5dc5fa57c93fa3dc2a2c2c7d024df952b3a95965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:44 +0000

    Add package constant-properties-protector/0.1.1/constant_properties_protector-0.1.1-py3-none-any.whl

---
+++ packages/constant-properties-protector/constant_properties_protector-0.1.1-py3-none-any.whl/constant_properties_protector/constant_properties_protector.py
@@ -0,0 +1,19 @@
+class CPP:
+    def __init__(self, protecteds):
+        self.__protecteds = protecteds
+
+    def protect_properties(self, properties):
+        self.__protecteds += properties
+        
+    def __getattribute__(self, attribute):
+        if attribute in ['__getattribute__','_CPP__protecteds','__dict__']:
+            return object.__getattribute__(self, attribute)
+        if attribute in self.__protecteds:
+            return object.__getattribute__(self, '_'+attribute)
+        return object.__getattribute__(self, attribute)
+        
+    def __setattr__(self, attribute, value):
+        if '_CPP__protecteds' in self.__dict__:
+            if attribute in self.__protecteds:
+                raise Exception(f'Can not modify constant property: {attribute}')
+        return object.__setattr__(self, attribute, value)
