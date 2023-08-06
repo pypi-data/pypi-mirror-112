2d651bb0381aacc26e2df74eea4f7f65e4f0fbf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:24 +0000

    Add package fuc/0.17.0/fuc-0.17.0.tar.gz

---
+++ packages/fuc/fuc-0.17.0.tar.gz/fuc-0.17.0/fuc/cli/fq_count.py
@@ -0,0 +1,37 @@
+from .. import api
+import sys
+
+description = f"""
+This command will count sequence reads in FASTQ files (both zipped and unzipped).
+
+It will look for stdin if there are no arguments.
+
+Usage examples:
+  $ fuc {api.common._script_name()} in.fastq
+  $ cat fastq.list | fuc {api.common._script_name()}
+"""
+
+def create_parser(subparsers):
+    parser = api.common._add_parser(
+        subparsers,
+        api.common._script_name(),
+        help='[FASTQ] Count sequence reads in FASTQ files.',
+        description=description,
+    )
+    parser.add_argument(
+        'fastq',
+        nargs='*',
+        help='FASTQ files (default: stdin).'
+    )
+    return parser
+
+def main(args):
+    if args.fastq:
+        paths = args.fastq
+    elif not sys.stdin.isatty():
+        paths = sys.stdin.read().rstrip('\n').split('\n')
+    else:
+        raise ValueError('no input files detected')
+    for path in paths:
+        qf = api.pyfq.FqFrame.from_file(path)
+        print(qf.df.shape[0])
