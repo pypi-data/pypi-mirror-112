47cce7e872a99985389c3bbdba60a6aa72897f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:41 +0000

    Add package configmapper/0.0.0/configmapper-0.0.0.tar.gz

---
+++ packages/configmapper/configmapper-0.0.0.tar.gz/configmapper-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='configmapper',
+    packages=["configmapper"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
