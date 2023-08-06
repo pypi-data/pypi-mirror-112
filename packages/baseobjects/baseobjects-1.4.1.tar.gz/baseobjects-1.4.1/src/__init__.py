ca334c2547dad6e38826e39be94cf6666d405d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:27 +0000

    Add package baseobjects/1.4.1/baseobjects-1.4.1.tar.gz

---
+++ packages/baseobjects/baseobjects-1.4.1.tar.gz/baseobjects-1.4.1/src/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" __init__.py
+"""
+__author__ = "Anthony Fong"
+__copyright__ = "Copyright 2021, Anthony Fong"
+__credits__ = ["Anthony Fong"]
+__license__ = ""
+__version__ = "1.1.0"
+__maintainer__ = "Anthony Fong"
+__email__ = ""
+__status__ = "Production/Stable"
+
+# Default Libraries #
+
+# Downloaded Libraries #
+
+# Local Libraries #
