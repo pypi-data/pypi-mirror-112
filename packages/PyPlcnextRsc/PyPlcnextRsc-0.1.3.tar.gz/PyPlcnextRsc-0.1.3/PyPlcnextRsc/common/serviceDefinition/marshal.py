e9b28f8afde1963e6eed7ce2a8d791b8eed1ca2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:01 +0000

    Add package PyPlcnextRsc/0.1.3/PyPlcnextRsc-0.1.3.tar.gz

---
+++ packages/PyPlcnextRsc/PyPlcnextRsc-0.1.3.tar.gz/PyPlcnextRsc-0.1.3/PyPlcnextRsc/common/serviceDefinition/marshal.py
@@ -0,0 +1,44 @@
+import functools
+
+from PyPlcnextRsc.common.internalEnums.string_encoding import RscStringEncoding
+from PyPlcnextRsc.common.tag_type import RscType
+
+__all__ = [
+    "Marshal",
+    "MarshalAs",
+    "_MarshalData",
+]
+
+
+@functools.lru_cache()
+def Marshal(
+        rscStringEncoding: RscStringEncoding = RscStringEncoding.Null,
+        maxStringSize: int = -1,
+        rscType: RscType = RscType.Null, ):
+    if rscStringEncoding != RscStringEncoding.Null and rscStringEncoding is not None and rscType != RscType.SecureString:
+        if rscStringEncoding == RscStringEncoding.Utf8:
+            rscType = RscType.Utf8String
+        elif rscStringEncoding == RscStringEncoding.Ansi:
+            rscType = RscType.AnsiString
+        elif rscStringEncoding == RscStringEncoding.Utf16:
+            rscType = RscType.Utf16String
+    return _MarshalData(rscStringEncoding=rscStringEncoding, maxStringSize=maxStringSize, rscType=rscType)
+
+
+class _MarshalData:
+    def __init__(self,
+                 rscStringEncoding: RscStringEncoding = RscStringEncoding.Null,
+                 maxStringSize: int = -1,
+                 rscType: RscType = RscType.Null,
+                 ):
+        self.rscStringEncoding = rscStringEncoding
+        self.maxStringSize = maxStringSize
+        self.rscType = rscType
+
+
+def MarshalAs(*arg, **kwargs):
+    def decorator(cls):
+        cls._marshal = Marshal(*arg, **kwargs)
+        return cls
+
+    return decorator
