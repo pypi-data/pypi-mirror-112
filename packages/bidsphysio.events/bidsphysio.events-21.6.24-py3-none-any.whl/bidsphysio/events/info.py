4f12b4e3f360db527262948b16d464cb35043e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:08 +0000

    Add package bidsphysio.events/21.6.24/bidsphysio.events-21.6.24-py3-none-any.whl

---
+++ packages/bidsphysio.events/bidsphysio.events-21.6.24-py3-none-any.whl/bidsphysio/events/info.py
@@ -0,0 +1,36 @@
+__version__ ="21.06.24"
+__author__ = "Chrysa Papadaniil, Pablo Velasco"
+__author_email__ = "chrysa@nyu.edu"
+__url__ = "https://github.com/cbinyu/bidsphysio"
+__packagename__ = 'bidsphysio.events'
+__description__ = "BIDS Events classes"
+__license__ = "MIT"
+__longdesc__ = """Base classes to handle BIDS event data."""
+
+CLASSIFIERS = [
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Topic :: Scientific/Engineering'
+]
+
+PYTHON_REQUIRES = ">=3.6"
+
+REQUIRES = [
+    'numpy >= 1.20.1',
+    'pandas>=1.1.0',
+]
+
+TESTS_REQUIRES = [
+    'pytest'
+]
+
+EXTRA_REQUIRES = {
+    'tests': TESTS_REQUIRES,
+}
+
+# Flatten the lists
+EXTRA_REQUIRES['all'] = sum(EXTRA_REQUIRES.values(), [])
