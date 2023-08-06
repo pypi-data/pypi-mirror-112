1852ed34a696c2201385ee7e1b42a17787c33383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:53 +0000

    Add package sk21pdf/1.1/sk21pdf-1.1-py3-none-any.whl

---
+++ packages/sk21pdf/sk21pdf-1.1-py3-none-any.whl/sk21pdf/pdf2text.py
@@ -0,0 +1,13 @@
+""" This module provides functions to convert PDF to text strings."""
+
+def convert(path):
+    """
+    Convert the given PDF to text.
+    
+    Parameters:
+    path(str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as text.
+    """
+    print("As mentioned just learning to build and test package")
