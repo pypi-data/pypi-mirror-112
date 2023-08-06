0885280af40d3cda6529abc3edf2c59dfcebe0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:49 +0000

    Add package anthemav-hyralex/1.0.1/anthemav-hyralex-1.0.1.tar.gz

---
+++ packages/anthemav-hyralex/anthemav-hyralex-1.0.1.tar.gz/anthemav-hyralex-1.0.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""Setup for anthemav module."""
+from setuptools import setup
+
+
+def readme():
+    """Return README file as a string."""
+    with open("README.rst", "r") as f:
+        return f.read()
+
+
+setup(
+    name="anthemav-hyralex",
+    version="1.0.1",
+    author="David McNett",
+    author_email="nugget@macnugget.org",
+    url="https://github.com/nugget/python-anthemav",
+    license="LICENSE",
+    packages=["anthemav-hyralex"],
+    scripts=[],
+    description="Python API for controlling Anthem Receivers",
+    long_description=readme(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={
+        "console_scripts": [
+            "anthemav_monitor = anthemav.tools:monitor",
+        ]
+    },
+)
