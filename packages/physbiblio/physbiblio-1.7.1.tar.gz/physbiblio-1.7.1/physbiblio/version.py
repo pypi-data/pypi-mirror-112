af8a5d868b81be251b9818769d1f772b4b4da754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:24 +0000

    Add package physbiblio/1.7.1/physbiblio-1.7.1.tar.gz

---
+++ packages/physbiblio/physbiblio-1.7.1.tar.gz/physbiblio-1.7.1/physbiblio/version.py
@@ -0,0 +1,7 @@
+__version__ = "1.7.1"
+__version_date__ = "11/07/2021"
+
+__recent_changes__ = """<br>* database can now store citation counts from inspire<br>
+* improvements to arxiv module<br>
+* small improvements to some error messages<br>
+"""
