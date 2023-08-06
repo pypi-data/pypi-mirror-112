79b2d16762c1bfc51fcb64c02cd9ae686013cb69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:54 +0000

    Add package cloud-mappings/0.9.3/cloud_mappings-0.9.3-py3-none-any.whl

---
+++ packages/cloud-mappings/cloud_mappings-0.9.3-py3-none-any.whl/cloudmappings/__init__.py
@@ -0,0 +1,8 @@
+from .wrappers import AzureBlobMapping, AzureTableMapping, GoogleCloudStorageMapping, AWSS3Mapping
+
+__all__ = [
+    "AzureBlobMapping",
+    "AzureTableMapping",
+    "GoogleCloudStorageMapping",
+    "AWSS3Mapping",
+]
