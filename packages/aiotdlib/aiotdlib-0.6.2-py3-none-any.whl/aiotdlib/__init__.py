e003ff0619d88e0961e87fdf3cd80c42ff334876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:04 +0000

    Add package aiotdlib/0.6.2/aiotdlib-0.6.2-py3-none-any.whl

---
+++ packages/aiotdlib/aiotdlib-0.6.2-py3-none-any.whl/aiotdlib/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.6.2"
+
+from .client import Client, ClientProxySettings, ClientProxyType
+from .filters import FilterCallable
+from .handlers import Handler, HandlerCallable
+from .middlewares import MiddlewareCallable
+from .tdjson import TDJson, TDLibLogVerbosity
+from .utils import ainput, PendingRequest, str_to_base64
