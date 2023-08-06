aec5887da43bcf74e176285cecee8d6db9b82929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:24 +0000

    Add package temp-babelfish/0.5.6.dev0/temp-babelfish-0.5.6.dev0.tar.gz

---
+++ packages/temp-babelfish/temp-babelfish-0.5.6.dev0.tar.gz/temp-babelfish-0.5.6.dev0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013 the BabelFish authors. All rights reserved.
+# Use of this source code is governed by the 3-clause BSD license
+# that can be found in the LICENSE file.
+#
+from setuptools import setup, find_packages
+
+
+setup(name='temp-babelfish',
+    version='0.5.6-dev',
+    license='BSD',
+    description='A module to work with countries and languages',
+    long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
+    long_description_content_type='text/x-rst',
+    keywords='babelfish language country locale',
+    url='https://github.com/Diaoul/babelfish',
+    author='Dustyn Gibson',
+    author_email='miigotu@gmail.com',
+    packages=find_packages(),
+    package_data={'babelfish': ['data/iso-639-3.tab', 'data/iso-3166-1.txt', 'data/iso15924-utf8-20131012.txt',
+                                'data/opensubtitles_languages.txt']},
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    classifiers=['Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    test_suite='babelfish.tests.suite')
