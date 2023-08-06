a1e560150c60278adc614919e49735b975de25ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:47 +0000

    Add package hellomk/0.0.1/hellomk-0.0.1.tar.gz

---
+++ packages/hellomk/hellomk-0.0.1.tar.gz/hellomk-0.0.1/libname/hello.py
@@ -0,0 +1,6 @@
+def sayhello():
+    print('helloworld')
+
+# https://stackoverflow.com/questions/28705029/pycharm-error-no-module-when-trying-to-import-own-module-python-script
+# to use this file in the python console we paste this into the python console
+# sys.path.extend([r'C:\Users\Almighty\PycharmProjects\pythonProject\base-folder\libname'])
---
+++ packages/hellomk/hellomk-0.0.1.tar.gz/hellomk-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hellomk',
+    version='0.0.1',
+    packages=['libname'], #find_packages(),
+    url='https://github.com/MWFK/HelloWorldAPITest',
+    license='apache licence 2.0',
+    author='MXK',
+    author_email='themwfk@gmail.com',
+    description='this is a demo how how to create a pypi package'
+)
