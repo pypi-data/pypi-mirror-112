b4bae4136056ff16698d1ce143183bef0f0e768e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:58 +0000

    Add package kojis/1.1/kojis-1.1.tar.gz

---
+++ packages/kojis/kojis-1.1.tar.gz/kojis-1.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+import pathlib
+
+setup(
+    name='kojis',
+    version='1.1',
+    url= 'https://alantimes2020.blogspot.com/',
+    author='Pylan',
+    description='Koji allows you to incorparate emojis easily into your python code!'
+)
