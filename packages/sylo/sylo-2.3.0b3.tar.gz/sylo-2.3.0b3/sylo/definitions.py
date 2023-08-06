c1bfcec2e1d051f08dec1e20a2afb388f67234f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:30 +0000

    Add package sylo/2.3.0b3/sylo-2.3.0b3.tar.gz

---
+++ packages/sylo/sylo-2.3.0b3.tar.gz/sylo-2.3.0b3/sylo/definitions.py
@@ -0,0 +1,46 @@
+import os
+import datetime
+from pathlib import Path
+
+
+HOME_DIR = home = str(Path.home())
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+TODAY = datetime.datetime.now().date()
+YESTERDAY = (datetime.datetime.now() - datetime.timedelta(days=1)).date()
+
+TODAY_STR = TODAY.strftime("%Y-%m-%d")
+YESTERDAY_STR = YESTERDAY.strftime("%Y-%m-%d")
+
+FORTNIGHT_AGO = (datetime.datetime.now() - datetime.timedelta(days=14)).date()
+ONE_YEAR_AGO = (datetime.datetime.now() - datetime.timedelta(days=364)).date()
+
+CONFIG_FILE_LOCATION = f"{HOME_DIR}/.sylo/sylo.cfg"
+HOME_DIR_CREATE = f"mkdir {HOME_DIR}/.sylo"
+SYLO_HOME = f"{HOME_DIR}/.sylo"
+
+
+WELCOME_CHOICES = ("", "h", "q", "s", "t", "y", "a")  # "i",
+
+TIMER_DEFAULTS = {
+    "work": {
+        "mins": 25,
+        "secs": 1500,
+    },
+    "rest": {
+        "mins": 5,
+        "secs": 300,
+    },
+}
+
+COUNTDOWN_INCREMENTS = 1
+
+METRICS_HEAT = 'termgraph --calendar --color magenta --start-dt "%s" %s 2>/dev/null'
+
+METRICS_BAR = 'termgraph %s --color {red,green} --start-dt "%s" 2>/dev/null'
+
+POMS_METRICS_BAR = 'termgraph %s --color magenta --start-dt "%s" 2>/dev/null'
+
+SQLITE_DB = f"{HOME_DIR}/.sylo/_sylo.db"
+
+BOOTSTRAP_SQL = f"{ROOT_DIR}/database_interactions/bootstrap.sql"
