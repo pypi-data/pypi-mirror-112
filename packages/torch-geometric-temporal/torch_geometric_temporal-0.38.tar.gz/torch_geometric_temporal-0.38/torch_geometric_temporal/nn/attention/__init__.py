ea1b2f32743156bd2b1abbf84d36c554c874aad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:25 +0000

    Add package torch-geometric-temporal/0.38/torch_geometric_temporal-0.38.tar.gz

---
+++ packages/torch-geometric-temporal/torch_geometric_temporal-0.38.tar.gz/torch_geometric_temporal-0.38/torch_geometric_temporal/nn/attention/__init__.py
@@ -0,0 +1,6 @@
+from .stgcn import STConv, TemporalConv
+from .astgcn import ASTGCN, ChebConvAttention
+from .mstgcn import MSTGCN
+from .gman import GMAN, SpatioTemporalEmbedding, SpatioTemporalAttention
+from .mtgnn import MTGNN, MixProp, GraphConstructor
+from .tsagcn import GraphAAGCN, AAGCN
