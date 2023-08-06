ab28d037e1cc9a9f677e8b3c2ecc034b5da610b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:46 +0000

    Add package comlibpy/0.1.3/comlibpy-0.1.3.tar.gz

---
+++ packages/comlibpy/comlibpy-0.1.3.tar.gz/comlibpy-0.1.3/comlibpy/__init__.py
@@ -0,0 +1,8 @@
+
+from comlibpy.math import xand, xor, xxor, xadd, xsub, xmul, xdiv, xceil, xfloor, xmod, xpow
+from comlibpy.math import xmin, xmax, xgcd
+
+from comlibpy.iter import range, repeat, map, filter, accumulate, reduce
+
+from comlibpy.func import bypass, partial, vparam
+
