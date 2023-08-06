10e34807d45f0207790be99d11172f8e51db3ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:34 +0000

    Add package imbored/0.0.0/imbored-0.0.0.tar.gz

---
+++ packages/imbored/imbored-0.0.0.tar.gz/imbored-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='imbored',
+    packages=["imbored"],
+    version='0.0.0',
+    author='Perzan',
+    description="Image Board",
+    author_email='PerzanDevelopment@gmail.com'
+)
