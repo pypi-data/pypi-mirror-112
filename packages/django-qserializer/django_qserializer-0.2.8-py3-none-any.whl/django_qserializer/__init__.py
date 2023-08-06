d2853c5805985fecc001f96b076311517fe12701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:40 +0000

    Add package django-qserializer/0.2.8/django_qserializer-0.2.8-py3-none-any.whl

---
+++ packages/django-qserializer/django_qserializer-0.2.8-py3-none-any.whl/django_qserializer/__init__.py
@@ -0,0 +1,11 @@
+from .serialization import (
+    BaseSerializer,
+    SerializableManager,
+    SerializableQuerySet,
+    serialize,
+)
+
+BaseSerializer = BaseSerializer
+SerializableManager = SerializableManager
+SerializableQuerySet = SerializableQuerySet
+serialize = serialize
