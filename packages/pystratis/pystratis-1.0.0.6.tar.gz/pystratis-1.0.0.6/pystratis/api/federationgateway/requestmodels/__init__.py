b6e17f1574390a39c63600f2269dc3941911908c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:59 +0000

    Add package pystratis/1.0.0.6/pystratis-1.0.0.6.tar.gz

---
+++ packages/pystratis/pystratis-1.0.0.6.tar.gz/pystratis-1.0.0.6/pystratis/api/federationgateway/requestmodels/__init__.py
@@ -0,0 +1,12 @@
+from .depositsrequest import DepositsRequest
+from .fullysignedtransferrequest import FullySignedTransferRequest
+from .memberipaddrequest import MemberIPAddRequest
+from .memberipremoverequest import MemberIPRemoveRequest
+from .memberipreplacerequest import MemberIPReplaceRequest
+from .pendingtransferrequest import PendingTransferRequest
+from .verifytransferrequest import VerifyTransferRequest
+
+__all__ = [
+    'DepositsRequest', 'FullySignedTransferRequest', 'MemberIPAddRequest', 'MemberIPRemoveRequest', 'MemberIPReplaceRequest',
+    'PendingTransferRequest', 'VerifyTransferRequest'
+]
