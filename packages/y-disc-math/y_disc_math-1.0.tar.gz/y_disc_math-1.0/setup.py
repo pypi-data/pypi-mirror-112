35939abc1f2b510f9b73e49c0503d6529ae7ec16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:32 +0000

    Add package y-disc-math/1.0/y_disc_math-1.0.tar.gz

---
+++ packages/y-disc-math/y_disc_math-1.0.tar.gz/y_disc_math-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='y_disc_math',
+    version='1.0',
+    description='Discrethe mathematics functions',
+    packages=['y_disc_math'],
+    author= 'Denis Yamunaque',
+    author_email = 'dy.xcvi@gmail.com',
+    zip_safe=False
+)
---
+++ packages/y-disc-math/y_disc_math-1.0.tar.gz/y_disc_math-1.0/y_disc_math/test_or.py
@@ -0,0 +1,21 @@
+from first_order_logic import logic_or
+
+def test_0_0():
+    p1 = 0
+    p2 = 0
+    assert(logic_or(p1, p2) == 0)
+
+def test_0_1():
+    p1 = 0
+    p2 = 1
+    assert(logic_or(p1, p2) == 1)
+
+def test_1_0():
+    p1 = 1
+    p2 = 0
+    assert(logic_or(p1, p2) == 1)
+
+def test_1_1():
+    p1 = 1
+    p2 = 1
+    assert(logic_or(p1, p2) == 1)
