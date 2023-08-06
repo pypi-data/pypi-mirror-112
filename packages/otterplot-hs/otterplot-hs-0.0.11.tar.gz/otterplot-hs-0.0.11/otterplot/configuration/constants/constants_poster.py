1829eeeaf83da604fcefda61c3d57c380511023d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:57 +0000

    Add package otterplot-hs/0.0.11/otterplot-hs-0.0.11.tar.gz

---
+++ packages/otterplot-hs/otterplot-hs-0.0.11.tar.gz/otterplot-hs-0.0.11/otterplot/configuration/constants/constants_poster.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+r"""
+This module contains the constants for the fontsizes prescribed by the physical review b format.
+"""
+# ======= options for figure ========
+FIGUREWIDTH_NARROW = 8.02775
+FIGUREWIDTH_BROAD = 8.02775 * 2
+# ======= options for ticks =========
+FONTSIZEMAJORTICKS = 8
+TICKWIDTH_MAJOR = 1.5
+TICKWIDTH_MINOR = 1.0
+TICKLENGTH_MAJOR = 5.0
+TICKLENGTH_MINOR = 3.0
+TICKDIRECTION = 'in'
+# ======= options fontsizes =======
+FONTSIZEXYLABEL = 8
