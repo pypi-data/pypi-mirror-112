7ec4d6f5e3ea981eb8db0c03770cdbf5f05260d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:38 +0000

    Add package iota-dust-manager/0.1.3/iota-dust-manager-0.1.3.tar.gz

---
+++ packages/iota-dust-manager/iota-dust-manager-0.1.3.tar.gz/iota-dust-manager-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='iota-dust-manager',
+    version='0.1.3',    
+    description='A python package that manages your receiving dust addresses',
+    url='https://github.com/F-Node-Karlsruhe/iota-dust-manager',
+    author='F-Node-Karlsruhe',
+    author_email='contact@f-node.de',
+    license='MIT License',
+    packages=['iota_dust_manager'],
+    install_requires=['iota-client>=0.2.0a8',                    
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Programming Language :: Python :: 3.9',
+    ],
+)
