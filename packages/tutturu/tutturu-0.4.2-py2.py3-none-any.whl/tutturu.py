657b86dfabbd2574b797f9fc76954be4c0b236f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:03 +0000

    Add package tutturu/0.4.2/tutturu-0.4.2-py2.py3-none-any.whl

---
+++ packages/tutturu/tutturu-0.4.2-py2.py3-none-any.whl/tutturu.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Simple test. Tut-turu))
+'''
+
+__version__ = "0.4.2"
+
+def main():
+    print("Tut-Turu!")
+
+if __name__ == "__main__":
+    main()
