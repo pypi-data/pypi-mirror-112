3d90ba3464362e14722e00be5f7aa42c342be54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:39 +0000

    Add package baseobjects/1.4.0/baseobjects-1.4.0-py2.py3-none-any.whl

---
+++ packages/baseobjects/baseobjects-1.4.0-py2.py3-none-any.whl/baseobjects/objects/initmeta.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" initmeta.py
+InitMeta is an abstract metaclass that implements an init class method which allows some setup after a class is created.
+"""
+__author__ = "Anthony Fong"
+__copyright__ = "Copyright 2021, Anthony Fong"
+__credits__ = ["Anthony Fong"]
+__license__ = ""
+__version__ = "1.4.0"
+__maintainer__ = "Anthony Fong"
+__email__ = ""
+__status__ = "Production/Stable"
+
+# Default Libraries #
+
+# Downloaded Libraries #
+
+# Local Libraries #
+from ..basemeta import BaseMeta
+
+
+# Definitions #
+# Classes #
+class InitMeta(BaseMeta):
+    """An abstract metaclass that implements an init class method which allows some setup after a class is created."""
+
+    # Magic Methods
+    # Construction/Destruction
+    def __new__(mcs, name, bases, namespace, **kwargs):
+        cls = super().__new__(mcs, name, bases, namespace, **kwargs)
+        cls._init_class_()
+        return cls
---
+++ packages/baseobjects/baseobjects-1.4.0-py2.py3-none-any.whl/baseobjects/warnings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" warnings.py
+Adds additional Warnings.
+"""
+__author__ = "Anthony Fong"
+__copyright__ = "Copyright 2021, Anthony Fong"
+__credits__ = ["Anthony Fong"]
+__license__ = ""
+__version__ = "1.4.0"
+__maintainer__ = "Anthony Fong"
+__email__ = ""
+__status__ = "Production/Stable"
+
+# Default Libraries #
+
+# Downloaded Libraries #
+
+# Local Libraries #
+
+
+# Definitions #
+# Classes #
+class TimeoutWarning(Warning):
+    """A general warning for timeouts."""
+
+    # Magic Methods
+    # Construction/Destruction
+    def __init__(self, name="A function"):
+        message = f"{name} timed out"
+        super().__init__(message)
