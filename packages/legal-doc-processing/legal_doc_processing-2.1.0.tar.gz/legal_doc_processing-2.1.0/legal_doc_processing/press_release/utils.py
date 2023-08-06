9f4cd8648971f3754b9ae5ba89a72b6fa86e8618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:15 +0000

    Add package legal-doc-processing/2.1.0/legal_doc_processing-2.1.0.tar.gz

---
+++ packages/legal-doc-processing/legal_doc_processing-2.1.0.tar.gz/legal_doc_processing-2.1.0/legal_doc_processing/press_release/utils.py
@@ -0,0 +1,79 @@
+from itertools import product
+
+import pandas as pd
+
+from legal_doc_processing.utils import (
+    _if_not_spacy,
+    _if_not_pipe,
+    # get_pers,
+    # get_orgs,
+    get_label_,
+    _ask,
+)
+
+
+def product_juridiction_pairs():
+
+    cftc_cands = [
+        "cftc",
+        "Commodity Futures Trading Commission",
+        "c.f.t.c",
+        "the Commodity Futures Trading Commission",
+    ]
+    doj_cands = ["doj", "department of justice", "d.o.j.", "the department of justice"]
+    sec_cands = [
+        "sec",
+        "Securities and Exchange Commission",
+        "s.e.c.",
+        "the Securities and Exchange Commission",
+    ]
+
+    cands = dict()
+    for k, _list in [("cftc", cftc_cands), ("doj", doj_cands), ("sec", sec_cands)]:
+        dd = {i.lower().strip(): k.lower().strip() for i in _list}
+        cands.update(dd)
+
+    return cands
+
+
+def product_juridic_form():
+    """ make a list of llc, LLC, LLC. etc etc """
+
+    # cands
+    cands = ["inc", "llc", "ltd", "corp"]
+
+    # conatiner
+    llc_list = list()
+
+    # for each
+    for i in cands:
+        # various case
+        sample = [str(i), str(i).lower(), str(i).capitalize(), str(i).upper()]
+        # case * -- point ou pas -- * --  espace, ou pas --
+        sample = product(sample, [", ", " ", ""], [".", ""])
+        sample = [str(j + i + k) for i, j, k in sample]
+        # extend
+        llc_list.extend(sample)
+
+    # sorted reverse lengt
+    llc_list = [(len(i), i) for i in llc_list]
+    llc_list = sorted(llc_list, reverse=True, key=lambda i: i[0])
+    llc_list = [i[1] for i in llc_list]
+
+    return llc_list
+
+
+def get_entities_pers_orgs(struct_doc: dict, n_paragraphs: int = 2, nlpspa=None) -> list:
+    """get entities PERSON and ORG from h1 and sub_article """
+
+    nlpspa = _if_not_spacy(nlpspa)
+
+    # sub article
+    sub_article = "\n".join(struct_doc["article"].split("\n")[:n_paragraphs])
+
+    # all pers all orgs from spacy entities
+    all_pers = get_pers(struct_doc["h1"], nlpspa) + get_pers(sub_article, nlpspa)
+    all_orgs = get_orgs(struct_doc["h1"], nlpspa) + get_orgs(sub_article, nlpspa)
+    pers_org_entities_list = all_pers + all_orgs
+
+    return pers_org_entities_list
---
+++ packages/legal-doc-processing/legal_doc_processing-2.1.0.tar.gz/legal_doc_processing-2.1.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from legal_doc_processing.version import Version
+
+REQUIRED = [
+    "pandas",
+    "nltk",
+    "transformers",
+    "spacy",
+    "tensorflow",
+    "clean-text",
+    "word2vec",
+    "dateparser"
+]
+
+setuptools.setup(
+    name="legal_doc_processing",
+    version=Version("2.1.0").number,
+    description="Theolex document processing",
+    long_description=open("README.md").read().strip(),
+    long_description_content_type="text/x-rst",
+    author="Jawad Alaoui",
+    author_email="jawad@theolex.io",
+    url="http://theolex-document-processing",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=REQUIRED,
+    license="MIT License",
+    zip_safe=False,
+    keywords="theolex document processing package",
+    #  classifiers=['Packages', 'NLP']
+)
