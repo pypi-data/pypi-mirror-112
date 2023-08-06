02b12bc125c31e9943d197c5140d04de4ab86929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:00 +0000

    Add package dcactivity/1.0.0/dcactivity-1.0.0-py3-none-any.whl

---
+++ packages/dcactivity/dcactivity-1.0.0-py3-none-any.whl/dcactivity/__init__.py
@@ -0,0 +1,15 @@
+"""
+DCActivity
+-----------
+
+An unofficial module which allows you to create and use Discord's Beta 
+features such as Poker Night, Chess, YouTube and many more.
+"""
+
+
+__name__ = 'dcactivity'
+__author__ = 'AkshuAgarwal'
+__version__ = '1.0.0'
+__license__ = 'MIT'
+
+from .dcactivity import DCActivity, DCApplication
