dd0a19647ee60f00fb0d9b2e758668d5b847a471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:02 +0000

    Add package PyxDistributions/1.0.0/PyxDistributions-1.0.0.tar.gz

---
+++ packages/PyxDistributions/PyxDistributions-1.0.0.tar.gz/PyxDistributions-1.0.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='PyxDistributions',
+      version='1.0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['PyxDistributions'],
+      zip_safe=False)
