69a1978bff0f3277ed7403e3a3ceec2595e21870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:35 +0000

    Add package boolpyt/0.0.1/boolpyt-0.0.1.tar.gz

---
+++ packages/boolpyt/boolpyt-0.0.1.tar.gz/boolpyt-0.0.1/boolpyt/boolpyt.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.offline as py
+
+def bool_plot_contor(x, y, z, col_name=None, html_path=''):
+  '''
+  输入三个列表，返回一个html页面
+  '''
+  if not col_name:
+    col_name = ['转速(r/min)', '扭矩(Nm)', '比油耗(g/kWh)']
+  if not html_path:
+    filename = 'contour.html'
+    
+  df = pd.DataFrame([x, y, z]).T
+  df.columns = col_name
+  fig = px.density_contour(df, x=col_name[0], y=col_name[1], z=col_name[2], histfunc="avg")
+  fig.update_traces(contours_coloring="fill", contours_showlabels = True)
+  py.plot(
+    fig, 
+    filename=filename,
+    image='png',           
+    ) 
---
+++ packages/boolpyt/boolpyt-0.0.1.tar.gz/boolpyt-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="boolpyt",
+    version="0.0.1",
+    author="ryanren",
+    author_email="strrenyumm@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/renyumm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['pandas', 'plotly',]
+)
