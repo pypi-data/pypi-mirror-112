81f62b37b8a9d6f1b687b7637519b4cb9be0ae0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:53 +0000

    Add package malaya-speech/1.1.2/malaya_speech-1.1.2-py3-none-any.whl

---
+++ packages/malaya-speech/malaya_speech-1.1.2-py3-none-any.whl/malaya_speech/__init__.py
@@ -0,0 +1,51 @@
+# Malaya-Speech, Speech-Toolkit library for bahasa Malaysia
+#
+# Copyright (C) 2019 Malaya Project
+# Licensed under the MIT License
+# Author: huseinzol05 <husein.zol05@gmail.com>
+# URL: <https://malaya-speech.readthedocs.io/>
+# For license information, see https://github.com/huseinzol05/malaya-speech/blob/master/LICENSE
+
+version = '1.1'
+bump_version = '1.1.2'
+__version__ = bump_version
+
+import malaya_boilerplate
+
+malaya_boilerplate.__package__ = 'malaya-speech'
+malaya_boilerplate.__url__ = 'https://f000.backblazeb2.com/file/malaya-speech-model/'
+malaya_boilerplate.__package_version__ = version
+
+from malaya_boilerplate.utils import get_home
+
+__home__, _ = get_home()
+
+
+from . import augmentation
+from . import config
+from . import extra
+from . import utils
+from . import age_detection
+from . import diarization
+from . import emotion
+from . import gender
+from . import language_detection
+from . import multispeaker_separation
+from . import noise_reduction
+from . import speaker_change
+from . import speaker_overlap
+from . import speaker_vector
+from . import speech_enhancement
+from . import speechsplit_conversion
+from . import stack
+from . import stt
+from . import super_resolution
+from . import tts
+from . import vad
+from . import vocoder
+from . import voice_conversion
+from . import utils
+
+from .pipeline import Pipeline
+from .streaming import *
+from .utils import *
---
+++ packages/malaya-speech/malaya_speech-1.1.2-py3-none-any.whl/malaya_speech/utils/__init__.py
@@ -0,0 +1,40 @@
+from malaya_boilerplate.backblaze import check_file
+from malaya_boilerplate.frozen_graph import (
+    nodes_session,
+    generate_session,
+    get_device,
+    load_graph,
+)
+from malaya_boilerplate.utils import (
+    available_device,
+    available_gpu,
+    print_cache,
+    delete_cache,
+    delete_all_cache,
+    close_session,
+    describe_availability,
+)
+
+
+from . import astype
+from . import char
+from . import combine
+from . import constant
+from . import dist
+from . import featurization
+from . import generator
+from . import griffin_lim
+from . import group
+from . import metrics
+from . import outlier
+from . import padding
+from . import read
+from . import speechsplit
+from . import split
+from . import text
+from . import subword
+from . import text
+from . import tf_featurization
+from . import validator
+
+from .read import load, resample
