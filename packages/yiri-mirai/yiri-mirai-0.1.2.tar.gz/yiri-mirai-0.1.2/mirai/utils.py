0e1d1078583bb5837f7db8c7bbb078f3e7470e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:12 +0000

    Add package yiri-mirai/0.1.2/yiri-mirai-0.1.2.tar.gz

---
+++ packages/yiri-mirai/yiri-mirai-0.1.2.tar.gz/yiri-mirai-0.1.2/mirai/utils.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+此模块提供一些实用的辅助方法。
+"""
+import inspect
+from typing import Callable, List
+
+from mirai import exceptions
+
+
+async def async_(coro):
+    """将一个对象包装为 `Awaitable`。
+    """
+    if inspect.isawaitable(coro):
+        return await coro
+    else:
+        return coro
+
+
+async def async_call(func: Callable, *args, **kwargs):
+    """以异步的方式调用一个函数，此函数可以是同步或异步的。"""
+    coro = func(*args, **kwargs)
+    return await async_(coro)
+
+
+async def async_call_with_exception(func: Callable, *args, **kwargs):
+    """以异步的方式调用一个函数，此函数可以是同步或异步的，同时处理调用中发生的异常。"""
+    try:
+        return await async_call(func, *args, **kwargs)
+    except Exception as e:
+        exceptions.print_exception(e) # 打印异常信息，但不打断执行流程
+
+
+class PriorityList(list):
+    """优先级列表。
+
+    根据应用场景优化：改动较慢，读取较快。
+    """
+    def add(self, priority, value):
+        """添加元素。
+
+        `priority` 优先级，小者优先。
+
+        `value` 元素。
+        """
+        index = 0
+        for i, (prior, data) in enumerate(self):
+            if data == value:
+                raise RuntimeError("优先级列表不支持重复添加元素!")
+            if prior <= priority:
+                index = i
+        self.insert(index, (priority, value))
+
+    def remove(self, value):
+        """删除元素。
+
+        `value` 元素。
+        """
+        for i, (_, data) in enumerate(self):
+            if data == value:
+                self.pop(i)
+                return True
+        else:
+            return False
+
+
+def KMP(string, pattern, count: int = 1) -> List[int]:
+    """KMP算法。
+
+    `string` 待匹配字符串。
+
+    `pattern` 模式字符串。
+
+    `count` 至多匹配的次数。
+    """
+    if len(string) < len(pattern) or count < 1:
+        return []
+
+    # 生成下一个匹配子串的next数组。
+    next_array = [0] * len(pattern)
+    next_array[0] = 0
+    j = 0
+    for i in range(1, len(pattern)):
+        while j > 0 and pattern[j] != pattern[i]:
+            j = next_array[j - 1]
+        if pattern[j] == pattern[i]:
+            j += 1
+        next_array[i] = j
+
+    # 开始匹配。
+    matches = []
+    j = 0
+    for i in range(0, len(string)):
+        while j > 0 and pattern[j] != string[i]:
+            j = next_array[j - 1]
+        if pattern[j] == string[i]:
+            j += 1
+        if j == len(pattern):
+            matches.append(i - j + 1)
+            j = next_array[j - 1]
+        if len(matches) == count:
+            break
+    return matches
---
+++ packages/yiri-mirai/yiri-mirai-0.1.2.tar.gz/yiri-mirai-0.1.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mirai', 'mirai.adapters', 'mirai.models']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.18.2,<0.19.0',
+ 'pydantic>=1.8.2,<2.0.0',
+ 'typing-extensions>=3.10.0,<4.0.0',
+ 'websockets>=9.1,<10.0']
+
+setup_kwargs = {
+    'name': 'yiri-mirai',
+    'version': '0.1.2',
+    'description': '一个轻量级、低耦合的基于 mirai-api-http 的 Python SDK。',
+    'long_description': "# YiriMirai\n\n一个轻量级、低耦合度的基于 mirai-api-http 的 Python SDK。\n\n目前仍处于开发阶段，各种内容可能会有较大的变化。\n\n## 安装\n\n从 PyPI 安装：\n\n```shell\npip install yiri-mirai\n# 或者使用 poetry\npoetry add yiri-mirai\n```\n\n此外，你还可以克隆这个仓库到本地，然后使用 `poetry` 安装：\n\n```shell\ngit clone git@github.com:Wybxc/YiriMirai.git\ncd YiriMirai\npoetry install\n```\n\n## 使用\n\n```python\nfrom mirai import Mirai, FriendMessage, WebSocketAdapter\n\nif __name__ == '__main__':\n    bot = Mirai(12345678, adapter=WebSocketAdapter(\n        verify_key='your_verify_key', host='localhost', port=6090\n    ))\n\n    @bot.on(FriendMessage)\n    async def on_friend_message(event: FriendMessage):\n        if str(event.message_chain) == '你好':\n            return bot.send_friend_message(event.sender.id, ['Hello World!'])\n\n    bot.run()\n```\n\n更多信息参看[文档](https://yiri-mirai.vercel.app/)。\n\n## 开源协议\n\n本项目使用 AGPLv3 开源协议。\n",
+    'author': '忘忧北萱草',
+    'author_email': 'wybxc@qq.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://yiri-mirai.vercel.app',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
