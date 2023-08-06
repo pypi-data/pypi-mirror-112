74a2d5dedc173a32d740b0078689a2719c056c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:33 +0000

    Add package NGAsubGMMcoeffsKBCG20post/0.0.7/NGAsubGMMcoeffsKBCG20post-0.0.7-py3-none-any.whl

---
+++ packages/NGAsubGMMcoeffsKBCG20post/NGAsubGMMcoeffsKBCG20post-0.0.7-py3-none-any.whl/NGAsubGMMcoeffsKBCG20post.py
@@ -0,0 +1,8 @@
+
+def getposterior_coefficients_KBCG20_T_1pt00():
+    import pandas as pd
+    TableNames = ['T','mu_theta_1_if','mu_theta_1_slab']
+    TableValues = [-1.0,6.3457,9.42163]
+#     thisTablePD = pd.DataFrame(TableValues)
+#     thisTablePD.columns = TableNames
+    return thisTablePD
