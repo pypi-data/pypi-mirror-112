9c9cd7e5a456794148aba4117c26c885b8b1d318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:38 +0000

    Add package Flaskel/2.3.3/Flaskel-2.3.3-py3-none-any.whl

---
+++ packages/Flaskel/Flaskel-2.3.3-py3-none-any.whl/flaskel/http/__init__.py
@@ -0,0 +1,7 @@
+from .batch import FlaskelHTTPBatch, HTTPBatch
+from .client import (
+    all_errors, FlaskelHttp, FlaskelJsonRPC, HTTPBase, HTTPClient,
+    HTTPStatusError, HTTPTokenAuth, NetworkError
+)
+from .httpdumper import BaseHTTPDumper, FlaskelHTTPDumper, LazyHTTPDumper
+from .useragent import UserAgent
