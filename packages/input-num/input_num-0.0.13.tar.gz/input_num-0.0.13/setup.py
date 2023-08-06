75312fb50b508a0d455056816199d2f1e3134c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:42 +0000

    Add package input-num/0.0.13/input_num-0.0.13.tar.gz

---
+++ packages/input-num/input_num-0.0.13.tar.gz/input_num-0.0.13/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='input_num',
+      version='0.0.13',
+      description='Python package - input_num is like input but it only accepts numbers',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/HexagonCore/input_num',
+      author='Hexagon Core Development',
+      author_email='mp3martin.developer@gmail.com',
+      license='MIT',
+      packages=['input_num'],
+      install_requires=[
+          'markdown',
+          'requests'
+      ],
+      zip_safe=False)
