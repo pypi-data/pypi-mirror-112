b69bb2f0674e111257e3f1198df84c1ddf713bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:19 +0000

    Add package parqser/1.0.1/parqser-1.0.1.tar.gz

---
+++ packages/parqser/parqser-1.0.1.tar.gz/parqser-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='parqser',
+    version='1.0.1',
+    description='Finally, a good parser',
+    url='https://github.com/ARQtty/parqser',
+    author='Ilya Shamov',
+    author_email='ShamovIA@yandex.ru',
+    license='MIT',
+    packages=['parqser'],
+    install_requires=['requests',
+                      'lxml'],
+
+    classifiers=[],
+)
