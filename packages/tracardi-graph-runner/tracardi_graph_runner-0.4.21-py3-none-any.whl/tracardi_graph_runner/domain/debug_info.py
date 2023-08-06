61ba2977f0f81a3c1048488f80eb132aaf6ac7ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:10 +0000

    Add package tracardi-graph-runner/0.4.21/tracardi_graph_runner-0.4.21-py3-none-any.whl

---
+++ packages/tracardi-graph-runner/tracardi_graph_runner-0.4.21-py3-none-any.whl/tracardi_graph_runner/domain/debug_info.py
@@ -0,0 +1,19 @@
+from typing import List
+from pydantic import BaseModel
+from tracardi_graph_runner.domain.debug_call_info import DebugCallInfo, Profiler
+from tracardi_graph_runner.domain.entity import Entity
+from tracardi_graph_runner.domain.flow_debug_info import FlowDebugInfo
+
+
+class DebugNodeInfo(BaseModel):
+    node: Entity
+    sequenceNumber: int = 0
+    calls: List[DebugCallInfo] = []
+    profiler: Profiler
+
+
+class DebugInfo(BaseModel):
+    timestamp: float
+    event: Entity
+    flow: FlowDebugInfo
+    nodes: List[DebugNodeInfo] = []
