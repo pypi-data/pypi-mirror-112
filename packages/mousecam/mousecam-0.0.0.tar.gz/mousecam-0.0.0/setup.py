80b425022ba3b1c7662d0711619e09e3053da4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:25 +0000

    Add package mousecam/0.0.0/mousecam-0.0.0.tar.gz

---
+++ packages/mousecam/mousecam-0.0.0.tar.gz/mousecam-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='mousecam',
+    packages=["mousecam"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
