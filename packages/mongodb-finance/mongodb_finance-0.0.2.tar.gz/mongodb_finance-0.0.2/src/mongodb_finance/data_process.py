b9910fb1975a6508e17e94fb7987f60da8ac96e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:21 +0000

    Add package mongodb-finance/0.0.2/mongodb_finance-0.0.2.tar.gz

---
+++ packages/mongodb-finance/mongodb_finance-0.0.2.tar.gz/mongodb_finance-0.0.2/src/mongodb_finance/data_process.py
@@ -0,0 +1,15 @@
+import pymongo
+
+# 连接数据库
+def connect_mongodb(host='127.0.0.1', port=27017):
+    mongo_uri = f'mongodb://{host}:{port}/'
+    visitor = pymongo.MongoClient(mongo_uri)
+    return visitor
+
+# 从mongodb中读取数据库
+def query_data(client, database, factor):
+    db = client[database]
+    cursor = db[factor].find()
+    df = pd.DataFrame(list(cursor))
+    del df['_id']
+    return df
