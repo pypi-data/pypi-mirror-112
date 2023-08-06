0cda11f849bd341210a4e1486772121bd69b2af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:17 +0000

    Add package comphardware/0.1.7/comphardware-0.1.7.tar.gz

---
+++ packages/comphardware/comphardware-0.1.7.tar.gz/comphardware-0.1.7/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read() 
+
+setuptools.setup(
+    name="comphardware",
+    version="0.1.7",
+    license="Apache-2",
+    url="https://github.com/MultisampledNight/comphardware",
+
+    author="MultisampledNight",
+    author_email="contact@multisamplednight.com",
+
+    description="Library for comparing hardware",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: System :: Hardware",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "PyOpenGL",
+        "psutil",
+        "py-cpuinfo",
+        "setuptools",
+    ],
+
+    include_package_data=True,
+)
