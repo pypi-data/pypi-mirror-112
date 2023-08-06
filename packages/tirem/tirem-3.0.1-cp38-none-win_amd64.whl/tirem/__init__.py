4957a57d46c65adb7802d559c29e6a783d03ed53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:22 +0000

    Add package tirem/3.0.1/tirem-3.0.1-cp38-none-win_amd64.whl

---
+++ packages/tirem/tirem-3.0.1-cp38-none-win_amd64.whl/tirem/__init__.py
@@ -0,0 +1,9 @@
+version = (3, 0, 1)
+
+__package_name__ = "tirem"
+__version__ = '.'.join(str(v) for v in version)
+__author__ = "Idan Miara"
+__author_email__ = "idan@miara.com"
+__license__ = "MIT"
+__url__ = r"https://github.com/talos-gis/rfmodel"
+__description__ = "TIREM Python API"
