bed2ad7a5b34d25fbbc6f780c01a320003cc0fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:58 +0000

    Add package sdc-api-py/1.3.0/sdc_api_py-1.3.0.tar.gz

---
+++ packages/sdc-api-py/sdc_api_py-1.3.0.tar.gz/sdc_api_py-1.3.0/sdc_api_py/_types/User.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class SdcUserRates:
+    """
+    Тип данных хранящий обработанную информацию о голосах пользователя.
+
+    Пример:
+
+    SdcGuildRates.plus: [000000000000000000, 111111111111111111] # id гильдий
+    SdcGuildRates.plus: [222222222222222222, 333333333333333333] # id гильдий
+    SdcGuildRates.plus_count: 15                                 # количество гильдий которым пользователь поставил "+"
+    SdcGuildRates.minus_count: 15                                # количество гильдий которым пользователь поставил "-"
+    """
+
+    plus:           list = field(default_factory=[])
+    minus:          list = field(default_factory=[])
+    plus_count:     int = 0
+    minus_count:    int = 0
