dbaf577baf027c1b92aa0657aaf4245976d49777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:10 +0000

    Add package polars/0.8.10/polars-0.8.10-cp36-abi3-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/polars/polars-0.8.10-cp36-abi3-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/polars/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+from .series import Series, wrap_s
+from .frame import DataFrame, wrap_df, StringCache
+from .functions import *
+from .lazy import *
+from .datatypes import *  # type: ignore
+
+# during docs building the binary code is not yet available
+try:
+    from .frame import version
+
+    __version__ = version()
+except ImportError:
+    pass
+
+__pdoc__ = {"ffi": False}
