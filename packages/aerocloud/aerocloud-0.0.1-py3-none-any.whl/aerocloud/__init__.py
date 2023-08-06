9408c7f7051466fb14a7a88bea15e777f99d8a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:24 +0000

    Add package aerocloud/0.0.1/aerocloud-0.0.1-py3-none-any.whl

---
+++ packages/aerocloud/aerocloud-0.0.1-py3-none-any.whl/aerocloud/__init__.py
@@ -0,0 +1,2 @@
+from .environment import getInputDirectory, getInputFiles, getResourceFile, getOutputDirectory, isLocal, getLocalWorkspace, setLocalWorkspace, getWorkingDirectory
+from .packages import getPackageDirectory
