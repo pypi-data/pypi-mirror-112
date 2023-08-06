3cca5098053068eb69734b14f060af01691d1266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:09 +0000

    Add package je-auto-control/0.0.5/je_auto_control-0.0.5-py3-none-any.whl

---
+++ packages/je-auto-control/je_auto_control-0.0.5-py3-none-any.whl/je_auto_control/windows/keyboard/win32_ctype_keyboard_control.py
@@ -0,0 +1,16 @@
+from je_auto_control.windows.core.util.win32_ctype_input import Input
+from je_auto_control.windows.core.util.win32_ctype_input import Keyboard
+from je_auto_control.windows.core.util.win32_ctype_input import KeyboardInput
+from je_auto_control.windows.core.util.win32_ctype_input import SendInput
+from je_auto_control.windows.core.util.win32_ctype_input import ctypes
+from je_auto_control.windows.core.util.win32_ctype_input import EventF_KEYUP
+
+
+def press_key(keyCode):
+    x = Input(type=Keyboard, ki=KeyboardInput(wVk=keyCode))
+    SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+
+
+def release_key(keyCode):
+    x = Input(type=Keyboard, ki=KeyboardInput(wVk=keyCode, dwFlags=EventF_KEYUP))
+    SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
