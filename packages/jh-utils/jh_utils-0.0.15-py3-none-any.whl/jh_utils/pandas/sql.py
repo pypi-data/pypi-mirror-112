cebc0f93ed8c71c3e4301a96478cb19f929362c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:31 +0000

    Add package jh-utils/0.0.15/jh_utils-0.0.15-py3-none-any.whl

---
+++ packages/jh-utils/jh_utils-0.0.15-py3-none-any.whl/jh_utils/pandas/sql.py
@@ -0,0 +1,42 @@
+## importing libs
+import pandas as pd
+import json
+from sqlalchemy import create_engine
+import psycopg2
+
+def get_data(query, engine):
+    """
+    make a query in a database
+    """
+    ## open connection
+    engine.connect()
+    
+    ## make select query
+    table = pd.read_sql(query, engine)
+
+    ## close connection    
+    engine.close()
+    return table
+
+def write_table(table, table_name, schema, engine, if_exists = 'append', chunksize = 10_000, index= False, close_connection = True):
+    """
+    if_exists => ["append","replace"]
+    
+    """
+
+    ## open connection
+    engine.connect()
+
+    ## write or replace table
+    table.to_sql(name = table_name,
+                 if_exists = if_exists,
+                 con = engine,
+                 schema=schema,
+                 method='multi',
+                 chunksize = chunksize,
+                 index = index)
+
+    # ## close connection
+    if close_connection:
+        engine.close()
+
