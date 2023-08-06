e643dde495a6ab3e26d17d05338ce449fcfb2b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:22 +0000

    Add package maggie_nester/1.6.0/maggie_nester-1.6.0.tar.gz

---
+++ packages/maggie_nester/maggie_nester-1.6.0.tar.gz/maggie_nester-1.6.0/nested_list.py
@@ -0,0 +1,16 @@
+import sys
+"""This module contains a function that will print the items individually from the list irrespective of its nested depth."""
+def print_list(num_list ,indent = False, level = 0,fh=sys.stdout):
+    """start of the function,A second argument called “level" is used to insert tab-stops when a nested list is encountered."""
+    for each in num_list:
+        if isinstance(each,list):
+            print_list(each,indent,level+1,fh)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end = '',file=fh)
+            print(each,file=fh)
+
+
+
+
