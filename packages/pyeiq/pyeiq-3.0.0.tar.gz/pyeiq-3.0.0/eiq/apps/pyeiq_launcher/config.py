418c3e0d011ad027aeb4f0eaa93f3903407f6c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:00 +0000

    Add package pyeiq/3.0.0/pyeiq-3.0.0.tar.gz

---
+++ packages/pyeiq/pyeiq-3.0.0.tar.gz/pyeiq-3.0.0/eiq/apps/pyeiq_launcher/config.py
@@ -0,0 +1,17 @@
+# Copyright 2021 NXP Semiconductors
+# SPDX-License-Identifier: BSD-3-Clause
+
+from eiq.apps.switch_image.switch_image import eIQSwitchLabelImage
+from eiq.apps.switch_video.switch_video import eIQVideoSwitchCore
+from eiq.modules.classification.classification_tflite import eIQObjectClassificationTFLite
+from eiq.modules.detection.object_detection_ssd import eIQObjectDetection
+from eiq.modules.detection.covid19_detection import eIQCOVID19Detection
+
+
+APPS = {'switch_image': eIQSwitchLabelImage,
+        'switch_video': eIQVideoSwitchCore}
+
+DEMOS = {'covid19_detection': eIQCOVID19Detection,
+         'object_classification_tflite': eIQObjectClassificationTFLite,
+         'object_detection_tflite': eIQObjectDetection
+         }
