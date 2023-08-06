01eb382bb52322bb1ceacaa7cef0596b9c85dda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:33 +0000

    Add package flask-pf-common/1.0/flask-pf-common-1.0.tar.gz

---
+++ packages/flask-pf-common/flask-pf-common-1.0.tar.gz/flask-pf-common-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-pf-common',
+    version='1.0',
+    url='https://github.com/problemfighter/flask-pf-marshmallow-swagger',
+    license='Apache 2.0',
+    author='Touhid Mia',
+    author_email='hmtm.cse@gmail.com',
+    description='This is Problem Fighter Flask Common Extension',
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
