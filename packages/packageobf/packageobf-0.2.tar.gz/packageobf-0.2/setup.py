b337cfaaf7f8964e69f57429603bcdcdd636604f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:04 +0000

    Add package packageobf/0.2/packageobf-0.2.tar.gz

---
+++ packages/packageobf/packageobf-0.2.tar.gz/packageobf-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup( name='packageobf',
+       version="0.2",
+       description="This is code with package",
+       long_description="THis is a verry long descrioption.",
+       author='jitu',
+       packages=['packageobf'],
+       install_requires=[]
+       )
