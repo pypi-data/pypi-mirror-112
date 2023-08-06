fa86c377be1aed01ca9309db3ec65fa6214645c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:16 +0000

    Add package EMPeaks/1.0.1/EMPeaks-1.0.1.tar.gz

---
+++ packages/EMPeaks/EMPeaks-1.0.1.tar.gz/EMPeaks-1.0.1/EMPeaks/GaussianMixture/__init__ 2.py
@@ -0,0 +1,8 @@
+# License: BSD-3-clause
+# Copyright © 2020 National Institute of Advanced Industrial Science and Technology (AIST)
+
+from EMPeaks.GaussianMixture._spectrum_adapted_em import SpectrumAdaptedEM
+from EMPeaks.GaussianMixture._em import EM
+from EMPeaks.GaussianMixture.Tests import *
+
+__all__ = ['SpectrumAdaptedEM', 'EM', 'Tests']
