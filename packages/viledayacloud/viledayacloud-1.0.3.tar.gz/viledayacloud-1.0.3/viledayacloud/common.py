65349ff233e8391941edecbd6047d2e6eb4bc14d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:12 +0000

    Add package viledayacloud/1.0.3/viledayacloud-1.0.3.tar.gz

---
+++ packages/viledayacloud/viledayacloud-1.0.3.tar.gz/viledayacloud-1.0.3/viledayacloud/common.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+"""
+Common shared functions and classes
+"""
+from typing import Any, Optional
+
+import aiohttp
+
+
+async def getsecret(sname: str, fid: str, atoken: str) -> Optional[str]:
+    """
+    Returns the secret value from Yandex Cloud Lockbox
+
+    :param sname: name of a secret
+    :param fid: folderId of the cloud folder
+    :param atoken: access token for Lockbox API
+    :return: secret contents
+    """
+    async with aiohttp.ClientSession(raise_for_status=True) as s:
+        async with s.get(f"https://lockbox.api.cloud.yandex.net/lockbox/v1/secrets?folderId={fid}",
+                         headers={"Authorization": f"Bearer {atoken}"}) as r:
+            _list = await r.json()
+            for _s_item in _list["secrets"]:
+                if _s_item["name"] == sname:
+                    _sid = _s_item["id"]
+                    async with s.get(f"https://payload.lockbox.api.cloud.yandex.net/lockbox/v1/secrets/{_sid}/payload",
+                                     headers={"Authorization": f"Bearer {atoken}"}) as r:
+                        _secret = await r.json()
+                        return _secret["entries"][0]["textValue"]
+    return None
