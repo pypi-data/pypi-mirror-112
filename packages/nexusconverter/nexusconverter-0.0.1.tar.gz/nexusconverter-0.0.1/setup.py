2fac35139a69be2041f465809368bb5a3f1cd4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:03 +0000

    Add package nexusconverter/0.0.1/nexusconverter-0.0.1.tar.gz

---
+++ packages/nexusconverter/nexusconverter-0.0.1.tar.gz/nexusconverter-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="nexusconverter",
+    version="0.0.1",
+    author="Guo Shuai",
+    author_email="gs0801@foxmail.com",
+    py_modules=["convert"],
+    python_requires=">=3.6, <4",
+)
