cd8918ca2793e4bb4f55ea467a6d6b3d51a1e66c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:54 +0000

    Add package projpicker-gui/0.1.0.dev5/projpicker-gui-0.1.0.dev5.tar.gz

---
+++ packages/projpicker-gui/projpicker-gui-0.1.0.dev5.tar.gz/projpicker-gui-0.1.0.dev5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read().rstrip()
+
+with open("projpicker-gui/VERSION") as f:
+    version = f.read().rstrip()
+
+setuptools.setup(
+    name="projpicker-gui",
+    version=version,
+    license="GPLv3+",
+    author="Owen Smith and Huidae Cho",
+    author_email="grass4u@gmail.com",
+    description="ProjPicker GUI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HuidaeCho/projpicker-gui",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3",
+    package_data={"projpicker-gui": ["VERSION", "map.html"]},
+    entry_points={"console_scripts": ["projpicker-gui=projpicker_gui:main"]},
+)
