d7042bbed5301388871059f49d78d2fc002ed1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:02 +0000

    Add package yaml-validate/1.0.2/yaml-validate-1.0.2.tar.gz

---
+++ packages/yaml-validate/yaml-validate-1.0.2.tar.gz/yaml-validate-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+def readfile(filename):
+    with open(filename, 'r+') as f:
+        return f.read()
+
+
+setup(
+    name="yaml-validate",
+    version="1.0.2",
+    description="",
+    # long_description=readfile('README.md'),
+    long_description="",
+    author="54696d21",
+    author_email="void@example.com",
+    url="https://github.com/54696d21/yaml-validate",
+    py_modules=['yaml-validate'],
+    license=readfile('LICENSE'),
+    entry_points={'console_scripts': ['yaml-validate = main:main']},
+)
