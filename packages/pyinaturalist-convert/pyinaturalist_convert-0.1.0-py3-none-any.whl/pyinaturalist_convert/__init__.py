54ff01d753ccf8611e32120690d91207906a4740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:32 +0000

    Add package pyinaturalist-convert/0.1.0/pyinaturalist_convert-0.1.0-py3-none-any.whl

---
+++ packages/pyinaturalist-convert/pyinaturalist_convert-0.1.0-py3-none-any.whl/pyinaturalist_convert/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa: F401, F403
+# Ignore ImportErrors if invoked outside a virtualenv
+try:
+    from pyinaturalist_convert.converters import *
+    from pyinaturalist_convert.bulk_csv import read_csv_export
+    from pyinaturalist_convert.dwc import to_dwc
+except ImportError as e:
+    print(e)
+
+# Attempt to import additional modules with optional dependencies
+try:
+    from pyinaturalist_convert.gpx import observations_to_gpx
+except ImportError:
+    pass
