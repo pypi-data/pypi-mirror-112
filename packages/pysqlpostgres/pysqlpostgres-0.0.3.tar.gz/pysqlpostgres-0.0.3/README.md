4664b7fb53728e75d7f0df18beb510d9ddc9bd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:54 +0000

    Add package pysqlpostgres/0.0.3/pysqlpostgres-0.0.3.tar.gz

---
+++ packages/pysqlpostgres/pysqlpostgres-0.0.3.tar.gz/pysqlpostgres-0.0.3/README.md
@@ -0,0 +1,2 @@
+# Postgres Python library
+ A Python library for interfacing and querying Postgres databases
---
+++ packages/pysqlpostgres/pysqlpostgres-0.0.3.tar.gz/pysqlpostgres-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='pysqlpostgres',
+    version='0.0.3',
+    description='A Postgres database SQL query library',
+    long_description='',
+    py_modules=[''],
+    Licence='MIT',
+    packages=find_packages(),
+
+)
