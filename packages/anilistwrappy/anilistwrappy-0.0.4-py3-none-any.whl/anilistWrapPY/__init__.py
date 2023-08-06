b6282380ebdabbd91b107c338575715132f14ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:49 +0000

    Add package anilistwrappy/0.0.4/anilistwrappy-0.0.4-py3-none-any.whl

---
+++ packages/anilistwrappy/anilistwrappy-0.0.4-py3-none-any.whl/anilistWrapPY/__init__.py
@@ -0,0 +1,6 @@
+# The contents of this file is free and unencumbered software released into the
+# public domain. For more information, please refer to <http://unlicense.org/>
+
+from .client import aniWrapPYClient
+
+__version__ = "0.0.4"
