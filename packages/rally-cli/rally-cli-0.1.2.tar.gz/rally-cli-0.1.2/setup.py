6c3be7c0b7ec8000e0e683849a5eff1c1bbed88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:32 +0000

    Add package rally-cli/0.1.2/rally-cli-0.1.2.tar.gz

---
+++ packages/rally-cli/rally-cli-0.1.2.tar.gz/rally-cli-0.1.2/setup.py
@@ -0,0 +1,27 @@
+import site
+import sys
+from setuptools import setup
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+
+with open("./README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+with open("./requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+
+setup(
+    name='rally-cli',
+    version='0.1.2',
+    packages=['rallycli'],
+    url='',
+    license='MIT',
+    author='juguerre',
+    author_email='juguerre@gmail.com',
+    description='Rally Software API Client',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=install_requires
+)
