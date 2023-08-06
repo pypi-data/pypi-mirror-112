a78937ecf1e6bf61501195b48445e75433a43346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:26 +0000

    Add package distribution-package/1.0/distribution_package-1.0.tar.gz

---
+++ packages/distribution-package/distribution_package-1.0.tar.gz/distribution_package-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distribution_package',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distribution_package'],
+      author = 'Dung Hoang',
+      author_email = 'dung.hoang300699@gmail.com',
+      zip_safe=False)
