c1acb737d8653829d90c8873dcf637350045acde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:27 +0000

    Add package dse-data-loader/0.0.3/dse-data-loader-0.0.3.tar.gz

---
+++ packages/dse-data-loader/dse-data-loader-0.0.3.tar.gz/dse-data-loader-0.0.3/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+__author__ = "Sk Farhad"
+__copyright__ = "Copyright (c) 2021 The Python Packaging Authority"
+
+
+import dse_data_loader
+from .dse_data_loader import PriceData
+from .dse_data_loader import FundamentalData
---
+++ packages/dse-data-loader/dse-data-loader-0.0.3.tar.gz/dse-data-loader-0.0.3/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dse-data-loader",
+    version="0.0.3",
+    author="Sk Farhad",
+    author_email="sk.farhad.eee@gmail.com",
+    description="This is a tool to download stock market data of Dhaka Stock Exchange.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/skfarhad/algo-trading/tree/master/dse_data_loader_pkg",
+    project_urls={
+        "Bug Tracker": "https://github.com/skfarhad/algo-trading/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
+        'pandas',
+        'beautifulsoup4'
+    ],
+    # package_dir={"": "src"},
+    # packages=setuptools.find_packages(where="dse_data_loader"),
+    packages=['dse_data_loader'],
+    python_requires=">=3.6",
+    zip_safe=False
+)
+
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
