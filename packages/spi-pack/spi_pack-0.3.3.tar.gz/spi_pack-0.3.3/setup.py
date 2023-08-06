91b176a6a71773e2ee6c0feeb1bbe3c1a1d03c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:03 +0000

    Add package spi-pack/0.3.3/spi_pack-0.3.3.tar.gz

---
+++ packages/spi-pack/spi_pack-0.3.3.tar.gz/spi_pack-0.3.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import os
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+setup(
+   name='spi_pack',  # package name
+   version='0.3.3',
+   description='A module to run analysis',
+   url = 'https://github.com/lizahina/test',
+   license='MIT',
+   author='Elizaveta',
+   author_email='ekaduhin@mail.ru',
+   packages=find_packages(), #same as name   
+   install_requires=[
+          'numpy', 'pandas', 'datetime', 'statsmodels', 'seaborn', 'scipy', 'sklearn', 'matplotlib'
+           ],
+)
