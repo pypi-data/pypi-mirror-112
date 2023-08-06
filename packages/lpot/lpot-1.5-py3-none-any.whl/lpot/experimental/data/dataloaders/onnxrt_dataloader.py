9e122c2e19f2fb7013e44d698312775c0a87dbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:50 +0000

    Add package lpot/1.5/lpot-1.5-py3-none-any.whl

---
+++ packages/lpot/lpot-1.5-py3-none-any.whl/lpot/experimental/data/dataloaders/onnxrt_dataloader.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from lpot.utils.utility import LazyImport
+from .base_dataloader import BaseDataLoader
+from .default_dataloader import DefaultDataLoader
+import logging
+
+class ONNXRTDataLoader(BaseDataLoader):
+    def _generate_dataloader(self, dataset, batch_size, last_batch, collate_fn,
+                             sampler, batch_sampler, num_workers, pin_memory,
+                             shuffle):
+        if shuffle:
+            logging.warning('Shuffle is not supported yet in ONNXRTDataLoader, ' \
+                            'ignoring shuffle keyword.')
+
+        return DefaultDataLoader(dataset, batch_size, last_batch, collate_fn,
+                                 sampler, batch_sampler, num_workers, pin_memory,
+                                 shuffle)
