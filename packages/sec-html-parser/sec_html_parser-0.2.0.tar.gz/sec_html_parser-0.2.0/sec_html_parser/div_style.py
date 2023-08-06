68260afb42cafbd5e4e0ad91c897f4703a6bba02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:42 +0000

    Add package sec-html-parser/0.2.0/sec_html_parser-0.2.0.tar.gz

---
+++ packages/sec-html-parser/sec_html_parser-0.2.0.tar.gz/sec_html_parser-0.2.0/sec_html_parser/div_style.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from typing import Optional, Tuple, Union
+
+import regex as re
+from bs4.element import Tag
+
+from sec_html_parser.span_style import SpanStyle
+
+
+@dataclass
+class DivStyle:
+    """Parsed HTML div style element attributes"""
+
+    margin_top: Optional[float]
+
+    def __init__(self, node_or_style: Union[str, Tag]) -> None:
+        _margin_top_re = re.compile(r"margin-top:(\d+\.?\d*)pt;?")
+
+        div_style_str = SpanStyle._get_style_string(node_or_style)
+
+        size = _margin_top_re.search(div_style_str)
+        self.margin_top = None if size is None else float(size.group(1))
+
+    def to_tuple(self) -> Tuple[float]:
+        """
+        Convert DivStyle to a tuple object.
+
+        This should be used when comparing two DivStyle objects
+        in order to check which one is a 'child' of the other.
+
+        None margin-top is converted to -1.
+
+        Example:
+            ```python
+            A = DivStyle("<div margin-top:12/>")
+            B = DivStyle("<div margin-top:10/>")
+
+            assert A.to_tuple() < B.to_tuple()
+            ```
+
+        Returns (margin_top, )
+        """
+
+        return (self.margin_top or -1.0,)
---
+++ packages/sec-html-parser/sec_html_parser-0.2.0.tar.gz/sec_html_parser-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sec_html_parser']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.9.3,<5.0.0', 'click>=8.0.1,<9.0.0']
+
+setup_kwargs = {
+    'name': 'sec-html-parser',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Tom Selfin',
+    'author_email': 'selfint@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
