2fe432616780c9d6edfcc892b57699efcb0879ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:36 +0000

    Add package bbc-reader/1.0.1/bbc-reader-1.0.1.tar.gz

---
+++ packages/bbc-reader/bbc-reader-1.0.1.tar.gz/bbc-reader-1.0.1/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import find_packages, setup
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="bbc-reader",
+    version="1.0.1",
+    description="Read the latest BBC news",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Khang Nguyen",
+    author_email="khangvu.1109@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["feedparser", "html2text"],
+    entry_points={
+        "console_scripts": [
+            "realbbc=bbc.__main__:main",
+        ]
+    },
+)
