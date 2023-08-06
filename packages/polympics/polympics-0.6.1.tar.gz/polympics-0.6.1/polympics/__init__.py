b368ea54945b8642cf957baa280f133b29f8756f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:52 +0000

    Add package polympics/0.6.1/polympics-0.6.1.tar.gz

---
+++ packages/polympics/polympics-0.6.1.tar.gz/polympics-0.6.1/polympics/__init__.py
@@ -0,0 +1,5 @@
+"""A Python wrapper for the Polympics API."""
+from .clients import *       # noqa:F401,F403
+from .handlers import *      # noqa:F401,F403
+from .pagination import *    # noqa:F401,F403
+from .types import *         # noqa:F401,F403
