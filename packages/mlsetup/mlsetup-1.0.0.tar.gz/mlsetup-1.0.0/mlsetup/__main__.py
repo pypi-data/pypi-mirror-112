a340e702ba9b9586d9d36f0b78b448f073c07803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:41 +0000

    Add package mlsetup/1.0.0/mlsetup-1.0.0.tar.gz

---
+++ packages/mlsetup/mlsetup-1.0.0.tar.gz/mlsetup-1.0.0/mlsetup/__main__.py
@@ -0,0 +1,23 @@
+import click
+from aihubcli.creator import createProject
+
+
+@click.group()
+def cli():
+    print("version = 1.0.0")
+    pass
+
+@cli.command()
+@click.argument('name')
+@click.option('-t','--template', default='default', help='name of the template to use')
+def create(name, template):
+    click.echo(f'creating project with name {name} and template file {template}')
+    createProject(name, template)
+
+@cli.command()
+def list():
+    click.echo('template name: default \n\nCurrently there is only one default template available. we can register more templates in the project')
+
+
+if __name__ == '__main__':
+    cli(prog_name="mlsetup")
