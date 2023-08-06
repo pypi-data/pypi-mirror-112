61bac2f7204767f06adbad13fbd8d382da6a8ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:34 +0000

    Add package LivestockCV/0.0.0.26/LivestockCV-0.0.0.26.tar.gz

---
+++ packages/LivestockCV/LivestockCV-0.0.0.26.tar.gz/LivestockCV-0.0.0.26/src/LivestockCV/core/js_to_image.py
@@ -0,0 +1,26 @@
+# import dependencies
+from IPython.display import display, Javascript, Image
+from google.colab.output import eval_js
+from base64 import b64decode, b64encode
+import cv2
+import numpy as np
+import PIL
+import io
+import html
+import time
+
+def js_to_image(js_reply):
+  """
+  Params:
+          js_reply: JavaScript object containing image from webcam
+  Returns:
+          img: OpenCV BGR image
+  """
+  # decode base64 image
+  image_bytes = b64decode(js_reply.split(',')[1])
+  # convert bytes to numpy array
+  jpg_as_np = np.frombuffer(image_bytes, dtype=np.uint8)
+  # decode numpy array into OpenCV BGR image
+  img = cv2.imdecode(jpg_as_np, flags=1)
+
+  return img
