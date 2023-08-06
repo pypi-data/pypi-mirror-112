5d28b5d94950de4307a764e485c3847a63b7cbc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:51 +0000

    Add package Jarve/0.0.3/Jarve-0.0.3.tar.gz

---
+++ packages/Jarve/Jarve-0.0.3.tar.gz/Jarve-0.0.3/jarve/simplechat.py
@@ -0,0 +1,5 @@
+
+def simplechat_dictionary_run(input_charecter_dictionary, input_dictionary_dictionary):
+    while True:
+        input_question_dictionary = input(input_charecter_dictionary)
+        print(input_dictionary_dictionary[input_question_dictionary])
