6552bd655854cb81c3bcdbd10314c3240bd5602b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:24 +0000

    Add package py-assembly-payments/0.3.0/py-assembly-payments-0.3.0.tar.gz

---
+++ packages/py-assembly-payments/py-assembly-payments-0.3.0.tar.gz/py-assembly-payments-0.3.0/assembly_payments/services/company.py
@@ -0,0 +1,18 @@
+
+from assembly_payments.services.base import BaseService
+from assembly_payments.types import Company, CompanyRequest
+
+
+class CompanyService(BaseService):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.endpoint = "/companies"
+
+    def list(self, *args, **kwargs):
+        response = self._execute(CompanyService.GET, self.endpoint)
+        return [Company(**company) for company in response.get('companies', [])]
+
+    def create(self, **kwargs):
+        data = CompanyRequest(**kwargs)
+        response = self._execute(CompanyService.POST, self.endpoint, data=data.dict())
+        return Company(**response['companies'])
