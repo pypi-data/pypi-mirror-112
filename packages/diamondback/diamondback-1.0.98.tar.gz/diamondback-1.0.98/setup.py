1cfb93615d7670002609e2994586e15514564a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:39 +0000

    Add package diamondback/1.0.98/diamondback-1.0.98.tar.gz

---
+++ packages/diamondback/diamondback-1.0.98.tar.gz/diamondback-1.0.98/setup.py
@@ -0,0 +1,110 @@
+""" **Description**
+
+        Diamondback setup.
+
+    **License**
+
+        `BSD-3C.  <https://github.com/larryturner/diamondback/blob/master/license>`_
+
+        © 2018 - 2021 Larry Turner, Schneider Electric Industries SAS. All rights reserved.
+
+    **Author**
+
+        Larry Turner, Schneider Electric, Analytics & AI, 2018-01-23.
+
+    **Definition**
+
+"""
+
+from setuptools import find_packages, setup
+import os
+import re
+
+try :
+
+    with open( 'readme.rst', 'r' ) as fin :
+
+        description = fin.read( )
+
+except Exception :
+
+    description = ''
+
+name = 'diamondback'
+
+with open( name + os.path.sep + '__init__.py', 'r' ) as fin :
+
+    label, value = r'__version__\s{0,4}=\s{0,4}', r'\d{1,3}\.\d{1,3}\.\d{1,3}'
+
+    version = re.findall( value, re.findall( label + '\W' + value + '\W', fin.read( ) )[ 0 ] )[ 0 ]
+
+setup( name = name,
+       version = version,
+       author = 'Larry Turner',
+       author_email = 'larry.turner@se.com',
+       url = 'https://github.com/larryturner/diamondback',
+       description = 'Diamondback digital signal processing package.',
+       long_description = description,
+       classifiers = [ 'Operating System :: OS Independent',
+                       'Programming Language :: Python :: 3',
+                       'Programming Language :: Python :: 3.6',
+                       'Programming Language :: Python :: 3.7',
+                       'Programming Language :: Python :: 3.8',
+                       'Programming Language :: Python :: 3.9',
+                       'License :: OSI Approved :: BSD License' ],
+       install_requires = [ 'jsonpickle >= 2.0.0',
+                            'loguru >= 0.5.3',
+                            'numpy >= 1.21.0',
+                            'pandas >= 1.2.5',
+                            'requests >= 2.25.1',
+                            'scipy >= 1.7.0' ],
+       extras_require = { 'full' : [ 'ipython >= 7.25.0',
+                                     'ipywidgets >= 7.6.3',
+                                     'jupyter >= 1.0.0',
+                                     'matplotlib >= 3.4.2',
+                                     'nox >= 2021.6.12',
+                                     'pillow >= 8.3.0',
+                                     'pytest >= 6.2.4',
+                                     'setuptools >= 57.0.0',
+                                     'sphinx >= 4.0.2',
+                                     'sphinx-pydantic >= 0.1.1',
+                                     'sphinx-rtd-theme >= 0.5.2' ] },
+       keywords = [ 'BSON',
+                    'DSP',
+                    'FFT',
+                    'FIR',
+                    'GZIP',
+                    'IIR',
+                    'JSON',
+                    'PCA',
+                    'PID',
+                    'PSD',
+                    'REST',
+                    'adaptive',
+                    'complex',
+                    'derivative',
+                    'diamondback',
+                    'diversity',
+                    'exponential',
+                    'filter',
+                    'fourier',
+                    'frequency',
+                    'goertzel',
+                    'integral',
+                    'jsonpickle',
+                    'log',
+                    'loguru',
+                    'model',
+                    'polynomial',
+                    'polyphase',
+                    'power',
+                    'rate',
+                    'requests',
+                    'serial',
+                    'transform',
+                    'wavelet' ],
+       packages = find_packages( exclude = [ 'tests', 'tests.*' ] ),
+       python_requires = '>= 3.6',
+       license = 'BSD-3C.  © 2018 - 2021 Larry Turner, Schneider Electric Industries SAS. All rights reserved.' )
+
+
---
+++ packages/diamondback/diamondback-1.0.98.tar.gz/diamondback-1.0.98/sphinx/conf.py
@@ -0,0 +1,83 @@
+""" **Description**
+
+        Sphinx documentation configuration.
+
+    **License**
+
+        `BSD-3C. <https://github.com/larryturner/diamondback/blob/master/license>`_
+
+        © 2018 - 2021 Larry Turner, Schneider Electric Industries SAS. All rights reserved.
+
+    **Author**
+
+        Larry Turner, Schneider Electric, Analytics & AI, 2018-03-22.
+
+    **Definition**
+
+"""
+
+import os
+import sys
+
+# Path.
+
+sys.path.insert( 0, os.path.abspath( '../' ) )
+
+# Project.
+
+author = 'Larry Turner'
+
+copyright = '© 2018 - 2021 Larry Turner, Schneider Electric Industries SAS. All rights reserved.'
+
+project = os.getcwd( ).split( os.path.sep )[ -2 ]
+
+# Configuration.
+
+exclude_patterns = [ 'build', 'setup', 'tests' ]
+
+extensions = [ 'sphinx.ext.autodoc',
+               'sphinx.ext.autosummary',
+               'sphinx.ext.githubpages',
+               'sphinx.ext.intersphinx',
+               'sphinx.ext.napoleon',
+               'sphinx.ext.viewcode',
+               'sphinx-pydantic' ]
+
+templates_path = [ 'templates' ]
+
+# Extensions.
+
+autoclass_content = 'both'
+
+autodoc_default_options = { 'members' : True }
+
+autodoc_member_order = 'bysource'
+
+autosummary_generate = True
+
+intersphinx_mapping = { 'python' : ( 'https://docs.python.org/3', None ),
+                        'pandas' : ( 'https://pandas.pydata.org/docs/', None ),
+                        'numpy' : ( 'https://numpy.org/doc/stable/', None ),
+                        'plotly' : ( 'https://plotly.com/python-api-reference/', None ) }
+
+napoleon_google_docstring = True
+
+napoleon_include_init_with_doc = False
+
+napoleon_include_private_with_doc = True
+
+napoleon_include_special_with_doc = True
+
+napolean_use_admonition_for_examples = True
+
+napolean_use_admonition_for_notes = True
+
+napolean_use_rtype = False
+
+# Theme.
+
+html_theme = 'sphinx_rtd_theme'
+
+# Help.
+
+htmlhelp_basename = project + 'doc'
