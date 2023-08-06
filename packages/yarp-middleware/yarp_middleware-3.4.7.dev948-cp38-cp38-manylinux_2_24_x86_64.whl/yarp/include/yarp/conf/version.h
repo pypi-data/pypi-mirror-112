c6bbe4298a6de6122446992dc2fd1839195dc62c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:25 +0000

    Add package yarp-middleware/3.4.7.dev948/yarp_middleware-3.4.7.dev948-cp38-cp38-manylinux_2_24_x86_64.whl

---
+++ packages/yarp-middleware/yarp_middleware-3.4.7.dev948-cp38-cp38-manylinux_2_24_x86_64.whl/yarp/include/yarp/conf/version.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * SPDX-FileCopyrightText: 2006-2010 RobotCub Consortium
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef YARP_CONFIG_VERSION_H
+#define YARP_CONFIG_VERSION_H
+
+#define YARP_VERSION_MAJOR 3
+#define YARP_VERSION_MINOR 4
+#define YARP_VERSION_PATCH 101
+#define YARP_VERSION_TWEAK 
+#define YARP_VERSION "3.4.101+20210712.22+gitea6ed180d"
+#define YARP_VERSION_SHORT "3.4.101"
+
+#endif
