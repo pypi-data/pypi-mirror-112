068ef9b56401029b3e074141c3968bed7b00f39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:25 +0000

    Add package tf-keras-vis/0.7.2/tf-keras-vis-0.7.2.tar.gz

---
+++ packages/tf-keras-vis/tf-keras-vis-0.7.2.tar.gz/tf-keras-vis-0.7.2/setup.py
@@ -0,0 +1,73 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="tf-keras-vis",
+    version="0.7.2",
+    author="keisen (Yasuhiro Kubota)",
+    author_email="k.keisen@gmail.com",
+    description="Neural network visualization toolkit for tf.keras",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/keisen/tf-keras-vis",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.6, <3.10',
+    install_requires=[
+        'tensorflow',
+        'scipy',
+        'pillow',
+        'deprecated',
+        'imageio',
+        'packaging',
+    ],
+    extras_require={
+        'develop': [
+            'flake8',
+            'flake8-docstrings',
+            'isort',
+            'yapf',
+            'pytest',
+            'pytest-pycodestyle',
+            'pytest-cov',
+            'pytest-env',
+            'pytest-xdist',
+        ],
+        'docs': [
+            'sphinx',
+            'sphinx-autobuild',
+            'sphinx-rtd-theme',
+            'myst-parser',
+            'nbsphinx',
+            'pandoc',
+        ],
+        'examples': [
+            'jupyterlab~=2.0',
+            'jedi~=0.17.0',
+            'matplotlib',
+        ],
+    },
+    include_package_data=True,
+)
