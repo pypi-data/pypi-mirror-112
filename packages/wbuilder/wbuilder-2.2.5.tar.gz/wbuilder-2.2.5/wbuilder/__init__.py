49eac5ab59d462cf1a01f7d6dea2f5f2ca858579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:05 +0000

    Add package wbuilder/2.2.5/wbuilder-2.2.5.tar.gz

---
+++ packages/wbuilder/wbuilder-2.2.5.tar.gz/wbuilder-2.2.5/wbuilder/__init__.py
@@ -0,0 +1,6 @@
+""" wbuilder """
+from .wbuilder import FromJSONBuild
+from .wbuilder import WebBuilder
+from .wbuilder import Css
+from .version import version as __version__
+__all__ = ["wbuilder"]
