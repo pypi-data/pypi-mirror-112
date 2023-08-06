6ab4595c7570fb75d0e7a5e7e19444aceb11afde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:20 +0000

    Add package tgsdk/1.0.20/tgsdk-1.0.20.tar.gz

---
+++ packages/tgsdk/tgsdk-1.0.20.tar.gz/tgsdk-1.0.20/tgsdk/_version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015-2021 Evgeniy Privalov, https://linkedin.com/in/evgeniyprivalov/
+
+__version__ = "1.0.20"
+
+__tg_version__ = "5.3"
