790c8fcf70617af7abf2d58eb1c180d194f2eede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:59 +0000

    Add package hcai-models/0.0.2/hcai-models-0.0.2.tar.gz

---
+++ packages/hcai-models/hcai-models-0.0.2.tar.gz/hcai-models-0.0.2/hcai_models/core/abstract_audio_model.py
@@ -0,0 +1,8 @@
+from hcai_models.core.abstract_model import Model
+
+class AudioModel(Model):
+    """
+       Abstract base class for audio models.
+       Specifies specific functionality of audio models and ensures compatibility with the interface for external calls.
+    """
+    ...
---
+++ packages/hcai-models/hcai-models-0.0.2.tar.gz/hcai-models-0.0.2/hcai_models/models/image/hcai_efficient_net/test.py
@@ -0,0 +1,9 @@
+import hcai_models.core.registered_model
+from hcai_models.core.abstract_model import Model
+from hcai_models.core.abstract_image_model import ImageModel
+from hcai_models.models.image.hcai_efficient_net.efficient_net_b7 import EfficientNetB7
+
+
+if __name__ == '__main__':
+    model = EfficientNetB7()
+    ...
