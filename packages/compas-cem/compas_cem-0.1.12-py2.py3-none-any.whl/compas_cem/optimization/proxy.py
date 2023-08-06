ed5a4c1340e6816774cdf94e334de1eb2e0afd0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:02 +0000

    Add package compas-cem/0.1.12/compas_cem-0.1.12-py2.py3-none-any.whl

---
+++ packages/compas-cem/compas_cem-0.1.12-py2.py3-none-any.whl/compas_cem/optimization/proxy.py
@@ -0,0 +1,42 @@
+from time import time
+
+
+__all__ = ["solve_nlopt_proxy"]
+
+
+# ------------------------------------------------------------------------------
+# Optimization
+# ------------------------------------------------------------------------------
+
+def solve_nlopt_proxy(topology, constraints, parameters, algorithm, iters, eps):
+    """
+    Deprecated version of ``optimizer_solve_nlopt_proxy``.
+    """
+    from compas_cem.optimization import Optimizer
+
+    optimizer = Optimizer()
+
+    # add constraints
+    for constraint in constraints:
+        optimizer.add_constraint(constraint)
+
+    # add parameters
+    for parameter in parameters:
+        optimizer.add_parameter(parameter)
+
+    start = time()
+    form = optimizer.solve_nlopt(topology, algorithm, iters, eps)
+
+    duration = time() - start
+    penalty = optimizer.penalty
+    evals = optimizer.evals
+
+    return form, penalty, evals, duration
+
+# ------------------------------------------------------------------------------
+# Main
+# ------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    pass
