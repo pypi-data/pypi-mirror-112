ed2da15a350d136a7b02422891886dbafd429ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:30 +0000

    Add package trolldb/1.0.3/trolldb-1.0.3.tar.gz

---
+++ packages/trolldb/trolldb-1.0.3.tar.gz/trolldb-1.0.3/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="trolldb",
+    version="1.0.3",
+    description="JSON Database but just encrypted",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ProYT303/trollDB",
+    author="Walterepic",
+    author_email="social.proyt303@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["cryptography"],
+    include_package_data=True,
+)
