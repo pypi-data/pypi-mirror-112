9d1e8b0efa8de660739a5ef9833148298ab27406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:51 +0000

    Add package ble-distributions/0.1/ble_distributions-0.1.tar.gz

---
+++ packages/ble-distributions/ble_distributions-0.1.tar.gz/ble_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ble_distributions',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['ble_distributions'],
+      author = 'Blessing Okeke',
+      author_email = 'blessing.okekeou@gmail.com',
+      zip_safe=False)
