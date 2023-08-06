b4ae837f6640923ea6e18eb6cc836e26c066f8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:40 +0000

    Add package filechooser/0.1.8.dev53/filechooser-0.1.8.dev53.tar.gz

---
+++ packages/filechooser/filechooser-0.1.8.dev53.tar.gz/filechooser-0.1.8.dev53/README.rst
@@ -0,0 +1,16 @@
+.. image:: https://readthedocs.org/projects/filechooser/badge/?version=latest
+   :target: https://filechooser.readthedocs.io/en/latest/?badge=latest
+   :alt: Documentation Status
+
+.. image:: https://badge.fury.io/py/filechooser.svg
+   :target: https://badge.fury.io/py/filechooser
+
+A script that copies a random selection of files from a set of folders
+to a single destination folder.
+
+Documentation
+=============
+
+You can find a more detailed documentation of this project at
+
+https://filechooser.readthedocs.io/
---
+++ packages/filechooser/filechooser-0.1.8.dev53.tar.gz/filechooser-0.1.8.dev53/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os.path
+import setuptools
+import subprocess
+
+
+def get_latest_tag():
+    version = "unknown"
+    try:
+        with open('.version') as fd:
+            version = fd.readline()
+        return version
+    except FileNotFoundError:
+        pass
+
+    git = subprocess.Popen(['git', 'describe', '--tags'],
+                           stdout=subprocess.PIPE,
+                           universal_newlines=True)
+    git.wait()
+    if git.returncode == 0:
+        raw_version = git.stdout.readlines()[0][1:].rstrip().split('-')
+        version = raw_version[0]
+        if len(raw_version) > 1:
+            version += ".dev" + raw_version[1]
+    with open('.version', 'w') as fd:
+        fd.write('{}\n'.format(version))
+    return version
+
+
+setuptools.setup(
+    name="filechooser",
+    version=get_latest_tag(),
+    data_files=['.version'],
+    license="BSD",
+    url="https://github.com/nicolasbock/filechooser.git",
+    project_urls={
+        "Documentation": "https://setuptools.readthedocs.io/"
+    },
+    scripts=["scripts/autorotate.sh"],
+    entry_points={
+        "console_scripts": [
+            "pick-files = filechooser_legacy.main:main",
+            "pick-files-new = filechooser.main:main"
+        ]
+    },
+    packages=setuptools.find_packages(),
+    test_suite="tests"
+)
