79bdd057219057d1e4d843d3a676130af67c973f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:58 +0000

    Add package labml-app/0.0.0/labml_app-0.0.0-py3-none-any.whl

---
+++ packages/labml-app/labml_app-0.0.0-py3-none-any.whl/labml_app/settings.py
@@ -0,0 +1,16 @@
+from pathlib import PurePath
+
+SLACK_CLIENT_ID = 'XXX'
+SLACK_CLIENT_SECRET = 'XXX'
+SERVER_URL = 'http://localhost:5000'
+WEB_URL = 'http://localhost:5000'
+DATA_PATH = PurePath('./data')
+FLOAT_PROJECT_TOKEN = 'float'
+SAMPLES_PROJECT_TOKEN = 'samples'
+IS_LOCAL_SETUP = True
+IS_MIX_PANEL = False
+SENTRY_DSN = None
+LABML_VERSION = '0.4.53'
+ANALYSES_MODULE = '.app_analyses'
+AUTH0_DOMAIN = 'https://dev-gt21oy8i.us.auth0.com'
+INDICATOR_LIMIT = 100
