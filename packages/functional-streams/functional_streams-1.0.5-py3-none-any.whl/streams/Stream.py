23f968d0c8f7b5aeb61a0b89847c2030d06e4874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:38 +0000

    Add package functional-streams/1.0.5/functional_streams-1.0.5-py3-none-any.whl

---
+++ packages/functional-streams/functional_streams-1.0.5-py3-none-any.whl/streams/Stream.py
@@ -0,0 +1,83 @@
+from functools import reduce
+
+
+def generator_from_list(data):
+    for row in data:
+        yield row
+
+
+def generator_from_list_of_lists(toplist):
+    for values in toplist:
+        for value in values:
+            yield value
+
+
+class Stream():
+    def __init__(self, data):
+        self.data = data
+
+    def map(self, fn):
+        self.data = map(fn, self.data)
+        return self
+
+    def filter(self, fn):
+        self.data = filter(fn, self.data)
+        return self
+
+    def peek(self, fn):
+        def peekfunction(val):
+            fn(val)
+            return val
+
+        self.data = map(peekfunction, self.data)
+        return self
+
+    def skip(self, number):
+        for i in range(number):
+            next(self.data)
+
+        return self
+
+    def take(self, number):
+        results = self.__convert_as_list()
+        updated_value = results[:number]
+        self.data = generator_from_list(updated_value)
+        return self
+
+    def distinct(self):
+        results = self.__convert_as_list()
+        unique_items = list(set(results))
+        self.data = generator_from_list(unique_items)
+        return self
+
+    def flatmap(self, fn):
+        data = map(fn, self.data)
+        self.data = generator_from_list_of_lists(data)
+        return self
+
+    def __create_a_copy(self):
+        result = list(self.data)
+        self.data = generator_from_list(result)
+        return result
+
+    def __convert_as_list(self):
+        return list(self.data)
+
+    def stream(self):
+        result = self.__create_a_copy()
+        return Stream(generator_from_list(result))
+
+    def length(self):
+        results = self.__convert_as_list()
+        return len(results)
+
+    def asList(self):
+        result = self.__convert_as_list()
+        return result
+
+    def reduce(self, fn):
+        return reduce(fn, self.data)
+
+    @staticmethod
+    def create(data):
+        return Stream(generator_from_list(data))
