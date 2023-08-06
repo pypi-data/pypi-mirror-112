dd3a7b860fa669f1f10652476e12844acf0d05fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:32 +0000

    Add package pytov/0.3.5/pytov-0.3.5.tar.gz

---
+++ packages/pytov/pytov-0.3.5.tar.gz/pytov-0.3.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="pytov",
+    version="0.3.5",
+    license='GNU',
+    author="Yuval Rosen",
+    author_email="yuv.rosen@gmail.com",
+    description="Python with a larger syntax support",
+    long_description=''.join(open('README.md', encoding='utf-8').readlines()),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Yuvix25/pytov",
+    packages=setuptools.find_packages(),
+    package_data = {'pytov' : ['*.tokens', '*.interp', '*.g4', '*.iml', '*.txt']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["antlr4-python3-runtime==4.9"],
+
+    entry_points={
+        'console_scripts': [
+            'pytov=pytov.pytov:run',
+        ],
+    },
+)
