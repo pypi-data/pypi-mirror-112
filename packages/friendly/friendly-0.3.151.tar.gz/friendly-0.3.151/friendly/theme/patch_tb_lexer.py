3b10140b2200b45168147328eb693a85744bf9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:58 +0000

    Add package friendly/0.3.151/friendly-0.3.151.tar.gz

---
+++ packages/friendly/friendly-0.3.151.tar.gz/friendly-0.3.151/friendly/theme/patch_tb_lexer.py
@@ -0,0 +1,20 @@
+# Monkeypatching the traceback lexer so that it treats
+# "Code block" the same as "File"
+from pygments.lexers.python import PythonTracebackLexer
+from pygments.lexer import bygroups
+from pygments.token import Text, Name, Number, Operator
+
+PythonTracebackLexer.tokens["intb"].insert(
+    0,
+    (
+        r'^(  Code block )(\[)(\d+)(\])(, line )(\d+)(, in )(.+)(\n)',
+        bygroups(Text, Operator, Number, Operator, Text, Number, Text, Name, Text),
+    ),
+)
+PythonTracebackLexer.tokens["intb"].insert(
+    1,
+    (
+        r'^(  Code block )(\[)(\d+)(\])(, line )(\d+)(\n)',
+        bygroups(Text, Operator, Number, Operator, Text, Number, Text),
+    ),
+)
