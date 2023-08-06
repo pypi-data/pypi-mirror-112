8944e005e2f92bac83adb5896b2c37cac1b96b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:55 +0000

    Add package LibForShiva/1.0/LibForShiva-1.0.tar.gz

---
+++ packages/LibForShiva/LibForShiva-1.0.tar.gz/LibForShiva-1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='LibForShiva',
+  version='1.0',
+  description='Librairie de simplification pour Shiva.',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description_content_type='text/markdown',
+  url='',  
+  author='Guillaume ROCHE',
+  author_email='g.roche1712@protonmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Shiva', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
