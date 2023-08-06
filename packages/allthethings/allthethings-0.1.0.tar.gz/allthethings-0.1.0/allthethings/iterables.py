eb52197003bfa5e16c68b1400d92be6faf3facf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:56 +0000

    Add package allthethings/0.1.0/allthethings-0.1.0.tar.gz

---
+++ packages/allthethings/allthethings-0.1.0.tar.gz/allthethings-0.1.0/allthethings/iterables.py
@@ -0,0 +1,14 @@
+from typing import TypeVar, Dict, List, Iterable, Callable
+
+U = TypeVar('U')
+R = TypeVar('R')
+
+
+def groupby(f: Callable[[U], R], xs: Iterable[U]) -> Dict[R, List[U]]:
+    r = dict()
+    for x in xs:
+        k = f(x)
+        if k not in r:
+            r[k] = []
+        r[k].append(x)
+    return r
