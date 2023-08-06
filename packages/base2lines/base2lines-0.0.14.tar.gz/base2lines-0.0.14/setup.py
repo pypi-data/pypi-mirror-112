e24c52b3f8383c1f51663fed75df20151402d36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:35 +0000

    Add package base2lines/0.0.14/base2lines-0.0.14.tar.gz

---
+++ packages/base2lines/base2lines-0.0.14.tar.gz/base2lines-0.0.14/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+VERSION = '0.0.14'
+DESCRIPTION = 'a python package to benchmarks algorithms against various datasets'
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Setting up
+setup(
+    name="base2lines",
+    version=VERSION,
+    author="Vishwas",
+    author_email="vishwasgoyal47@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["seaborn","scikit-learn","xgboost"],
+    keywords=[],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    # package_dir={'':'baseline'},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
