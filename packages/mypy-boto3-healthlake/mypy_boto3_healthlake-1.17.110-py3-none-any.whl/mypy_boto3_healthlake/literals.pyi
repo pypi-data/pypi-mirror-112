ca131172748028a0df6aee5fea69ef34d4846802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:11 +0000

    Add package mypy-boto3-healthlake/1.17.110/mypy_boto3_healthlake-1.17.110-py3-none-any.whl

---
+++ packages/mypy-boto3-healthlake/mypy_boto3_healthlake-1.17.110-py3-none-any.whl/mypy_boto3_healthlake/literals.pyi
@@ -0,0 +1,26 @@
+"""
+Type annotations for healthlake service literal definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_healthlake/literals.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_healthlake.literals import DatastoreStatusType
+
+    data: DatastoreStatusType = "ACTIVE"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("DatastoreStatusType", "FHIRVersionType", "JobStatusType", "PreloadDataTypeType")
+
+DatastoreStatusType = Literal["ACTIVE", "CREATING", "DELETED", "DELETING"]
+FHIRVersionType = Literal["R4"]
+JobStatusType = Literal["COMPLETED", "FAILED", "IN_PROGRESS", "SUBMITTED"]
+PreloadDataTypeType = Literal["SYNTHEA"]
