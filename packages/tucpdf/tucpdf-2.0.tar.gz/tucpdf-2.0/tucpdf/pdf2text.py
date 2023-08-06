71321c023c9072b49973f16d2790af710550cbc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:38 +0000

    Add package tucpdf/2.0/tucpdf-2.0.tar.gz

---
+++ packages/tucpdf/tucpdf-2.0.tar.gz/tucpdf-2.0/tucpdf/pdf2text.py
@@ -0,0 +1,14 @@
+""" This module provides functions to convert pdf to text """
+
+
+def convert():
+    """
+    Convert the given pdf to text.
+
+    Parameters:
+    path (str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as text.
+    """
+    print("pdf2text")
