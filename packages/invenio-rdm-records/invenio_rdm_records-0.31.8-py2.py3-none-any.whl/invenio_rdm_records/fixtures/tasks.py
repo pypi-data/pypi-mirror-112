597eff97c51e0cf8d1a99913c61319d89e01d32f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:44 +0000

    Add package invenio-rdm-records/0.31.8/invenio_rdm_records-0.31.8-py2.py3-none-any.whl

---
+++ packages/invenio-rdm-records/invenio_rdm_records-0.31.8-py2.py3-none-any.whl/invenio_rdm_records/fixtures/tasks.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+# Copyright (C) 2021 Northwestern University.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Celery tasks for fixtures."""
+import arrow
+from celery import shared_task
+from flask import current_app
+from invenio_access.permissions import system_identity
+from invenio_vocabularies.proxies import current_service as vocabulary_service
+
+from ..proxies import current_rdm_records
+from ..services.errors import EmbargoNotLiftedError
+
+
+@shared_task
+def create_vocabulary_record(data):
+    """Create a vocabulary record."""
+    vocabulary_service.create(system_identity, data)
+
+
+@shared_task
+def create_demo_record(data):
+    """Create a demo record."""
+    service = current_rdm_records.records_service
+    draft = service.create(data=data, identity=system_identity)
+    service.publish(id_=draft.id, identity=system_identity)
