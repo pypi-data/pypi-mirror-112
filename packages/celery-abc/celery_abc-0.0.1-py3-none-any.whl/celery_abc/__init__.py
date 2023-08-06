97ebea60a072d1e30f71888b22e3b37aa0c3b291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:07 +0000

    Add package celery-abc/0.0.1/celery_abc-0.0.1-py3-none-any.whl

---
+++ packages/celery-abc/celery_abc-0.0.1-py3-none-any.whl/celery_abc/__init__.py
@@ -0,0 +1,78 @@
+"""
+Set of tools to create microservice app with `celery` as message brocker.
+
+Say, you want to write some microservices with python.And you want them to
+work seamlessly with each other - call methods of one service from another
+like it is a single app. You may use `celery` just for that. But you actually
+need to register tasks in the same app. Isn't that a little strange - to keep
+the code base of ALL the services in each and every of them? Wouldn't it be
+more convinient, to share only interfaces? That way each developer could track
+what they can use of other services, but without actully storing their code.
+
+How to use it?
+We'll need at least two services and a shared directory. In shared directory
+we'll create an abstract class `Interface`. Let's call one service `Caller`
+and the other - `Worker`. `Caller` has `Interface` imported, but knows nothing
+about the implementation. `Worker` has `Interface` implemented.
+
+1. Create common directory, share it with all your services.
+2. In the shared folder create python module. Define your
+interfaces there. For now, `celery-abc` requires for `Interface`
+to be abstract.
+>>> from abc import ABC, abstractmethod
+
+>>> class Interface(ABC):
+>>>     @abstractmethod
+>>>     def do_some_stuff(self, arg):
+>>>         pass
+>>>     @abstractmethod
+>>>     def do_more_stuff(self, arg):
+>>>         pass
+3. In your `Worker` service import `Interface` from shared module
+and implement it. Metaclass for your implementation should be
+`WorkerMetaBase` from `celery-abc`. You don't need to register
+it's methods, just instantiate your class with `celery`.
+Run your `worker` as a celery worker.
+>>> from celery import Celery
+>>> from celery_abc import WorkerMetaBase
+>>> from shared import Interface
+
+>>> class Worker(Interface, metaclass=WorkerMetaBase):
+>>>     def do_some_stuff(self, arg):
+>>>         return self.do_more_stuff(arg)
+
+>>>     def do_more_stuff(self, arg):
+>>>         return f"Doing stuff {arg}"
+
+>>> celery_app = Celery(...)
+>>> Worker(celery)
+4. In `Caller` service import `CallerMetaBase`, create a new
+class, that inherits from `Interface` and have `CallerMetaBase`
+as a metaclass. Instantiate it with your `celery` app.
+Unlike `worker`, `caller` may run just as regular python app.
+>>> class Caller(Interface, metaclass=CallerMetaBase):
+>>>     pass
+
+>>> celery_app = Celery(...)
+>>> caller = Caller(celery)
+>>> result = caller.do_some_stuff('Hello!')
+Now, you can call methods, that are implemented in the `Worker` =)
+
+Here you go, now you can seamlessly call methods of `Worker` from
+`Caller`. As you can see, you can call `Worker`'s methods from each
+other. But you can not keep any instnce variables in `Worker`'s `self`.
+In the end, microservices are better when they are stateless.
+
+Resrictions and limitations:
+If you want to create statefull microservices - you still can use `celery-abc`,
+if only for comunication. But there may be other solutions.
+Method signature can't have `*args` and `**kwargs` in it.
+It is totally fine to pass argumetns this way,
+but capturing them is not supported yet.
+
+If you want to create complex system of classes. OOP is cool,
+but multiple and chaind inheritance are not supported at the moment.
+"""
+
+from .caller import CallerMetaBase
+from .worker import WorkerMetaBase
---
+++ packages/celery-abc/celery_abc-0.0.1-py3-none-any.whl/celery_abc/caller.py
@@ -0,0 +1,52 @@
+"""Replace abstract interface methods with calls of `celery` tasks."""
+
+from abc import ABCMeta
+from inspect import FullArgSpec, getfullargspec
+from types import MethodType
+
+
+class CallerMetaBase(ABCMeta):
+    """
+    Metabase class for your interface on caller's side.
+
+    It replaces abstract methods with `celery` calls with names like
+    `Interface.method`.
+    """
+
+    def _init_overriden(self, celery):
+        self._celery = celery
+
+    class _CalledTask:
+        def __init__(self, name, args_info: FullArgSpec):
+            self._name = name
+            self._arg_names = [arg for arg in args_info.args if arg != 'self']
+            self._defaults = args_info.defaults
+
+        def __call__(self, obj, *args, **kargs):
+            all_args = {name: val for name, val in zip(self._arg_names, args)}
+            all_args.update(kargs)
+            return obj._celery.send_task(self._name, kwargs=all_args).get()
+
+        def __get__(self, obj, objtype=None):
+            if obj is None:
+                return self
+            return MethodType(self, obj)
+
+    def __new__(cls, name, bases, dct):
+        """Create new class with all public methods replaced with rpc calls."""
+        if len(bases) != 1:
+            raise Exception(
+                "Can't create caller class with more than one parent, yet"
+            )
+        dct['__init__'] = CallerMetaBase._init_overriden
+        base_name = bases[0].__name__
+        for attr_name in dir(bases[0]):
+            if not attr_name.startswith('_'):
+                args_info = getfullargspec(getattr(bases[0], attr_name))
+                task_name = '.'.join((base_name, attr_name))
+                dct[attr_name] = CallerMetaBase._CalledTask(
+                    task_name,
+                    args_info
+                )
+
+        return ABCMeta.__new__(cls, name, bases, dct)
