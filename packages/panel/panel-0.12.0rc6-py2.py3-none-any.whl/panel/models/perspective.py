4726506795ca5a2a4e80826f1ec9418b347a3c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:06 +0000

    Add package panel/0.12.0rc6/panel-0.12.0rc6-py2.py3-none-any.whl

---
+++ packages/panel/panel-0.12.0rc6-py2.py3-none-any.whl/panel/models/perspective.py
@@ -0,0 +1,80 @@
+from bokeh.core.properties import (
+    Any, Bool, Dict, Either, Instance, List, Null, Nullable, String
+)
+from bokeh.models import ColumnDataSource, HTMLBox
+
+from ..io.resources import bundled_files
+from ..util import classproperty
+
+
+class Perspective(HTMLBox):
+
+    aggregates = Either(Dict(String, Any), Null())
+
+    column_pivots = Either(List(String), Null())
+
+    columns = Either(List(String), Null)
+
+    computed_columns = Either(List(String), Null())
+
+    editable = Nullable(Bool())
+
+    filters = Either(List(Any), Null())
+
+    plugin = String()
+
+    plugin_config = Either(Dict(String, Any), Null)
+
+    row_pivots = Either(List(String), Null())
+
+    selectable = Nullable(Bool())
+
+    schema = Dict(String, String)
+
+    sort = Either(List(List(String)), Null())
+
+    source = Instance(ColumnDataSource)
+
+    toggle_config = Bool(True)
+
+    theme = String()
+
+    # pylint: disable=line-too-long
+    __javascript_raw__ = [
+        "https://unpkg.com/@finos/perspective@0.5.2/dist/umd/perspective.js",
+        "https://unpkg.com/@finos/perspective-viewer@0.5.2/dist/umd/perspective-viewer.js",
+        "https://unpkg.com/@finos/perspective-viewer-datagrid@0.5.2/dist/umd/perspective-viewer-datagrid.js",
+        "https://unpkg.com/@finos/perspective-viewer-hypergrid@0.5.2/dist/umd/perspective-viewer-hypergrid.js",
+        "https://unpkg.com/@finos/perspective-viewer-d3fc@0.5.2/dist/umd/perspective-viewer-d3fc.js",
+    ]
+
+    @classproperty
+    def __javascript__(cls):
+        return bundled_files(cls)
+
+    __js_skip__ = {
+        "perspective": __javascript__,
+    }
+
+    __js_require__ = {
+        "paths": {
+            "perspective": "https://unpkg.com/@finos/perspective@0.5.2/dist/umd/perspective",
+            "perspective-viewer": "https://unpkg.com/@finos/perspective-viewer@0.5.2/dist/umd/perspective-viewer",
+            "perspective-viewer-datagrid": "https://unpkg.com/@finos/perspective-viewer-datagrid@0.5.2/dist/umd/perspective-viewer-datagrid",
+            "perspective-viewer-hypergrid": "https://unpkg.com/@finos/perspective-viewer-hypergrid@0.5.2/dist/umd/perspective-viewer-hypergrid",
+            "perspective-viewer-d3fc": "https://unpkg.com/@finos/perspective-viewer-d3fc@0.5.2/dist/umd/perspective-viewer-d3fc",
+        },
+        "exports": {
+            "perspective": "Perspective",
+            "perspective-viewer": "PerspectiveViewer",
+            "perspective-viewer-datagrid": "PerspectiveViewerDatagrid",
+            "perspective-viewer-hypergrid": "PerspectiveViewerHypergrid",
+            "perspective-viewer-d3fc": "PerspectiveViewerD3fc",
+        },
+    }
+
+    __css_raw__ = ["https://unpkg.com/@finos/perspective-viewer@0.5.2/dist/umd/all-themes.css"]
+
+    @classproperty
+    def __css__(cls):
+        return bundled_files(cls, 'css')
