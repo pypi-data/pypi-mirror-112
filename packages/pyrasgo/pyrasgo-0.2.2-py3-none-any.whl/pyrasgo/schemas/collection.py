95b0aa96bbb8ec0289a1f3a0b6802bd367dadb4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:41 +0000

    Add package pyrasgo/0.2.2/pyrasgo-0.2.2-py3-none-any.whl

---
+++ packages/pyrasgo/pyrasgo-0.2.2-py3-none-any.whl/pyrasgo/schemas/collection.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+from pyrasgo.schemas.enums import ModelTypes
+from pyrasgo.schemas.feature import Feature
+
+
+class CollectionBase(BaseModel):
+    id: int
+
+class Collection(CollectionBase):
+    name: Optional[str]
+    authorId: Optional[int]
+    dataTableName: Optional[str]
+    type: Optional[ModelTypes] = None
+    organizationId: Optional[int]
+    features: Optional[List[Feature]]
+    description: Optional[str]
+    isShared: Optional[bool]
+
+class CollectionUpdate(BaseModel):
+    id: int
+    name: Optional[str]
