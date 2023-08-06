a20f129f3cc456566627474099c50446ea06472c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:30 +0000

    Add package aerocloud/0.0.3/aerocloud-0.0.3.tar.gz

---
+++ packages/aerocloud/aerocloud-0.0.3.tar.gz/aerocloud-0.0.3/src/aerocloud/__init__.py
@@ -0,0 +1,14 @@
+from .environment import (
+    getInputDirectory,
+    getInputFiles,
+    getAllInputDirectories,
+    getAllInputFiles,
+    getResourceFile,
+    getOutputDirectory,
+    isLocal,
+    getLocalWorkspace,
+    setLocalWorkspace,
+    getWorkingDirectory
+)
+
+from .packages import getPackageDirectory
