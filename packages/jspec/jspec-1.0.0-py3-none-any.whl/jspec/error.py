bedd7177c7f0880efc8031f435177b257d54a142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:22 +0000

    Add package jspec/1.0.0/jspec-1.0.0-py3-none-any.whl

---
+++ packages/jspec/jspec-1.0.0-py3-none-any.whl/jspec/error.py
@@ -0,0 +1,10 @@
+"""Module for defining JSPEC errors."""
+
+
+class JSpecError(Exception):
+    """Base class for Jspec Errors"""
+    pass
+
+class JSpecLoadError(JSpecError):
+    """Class for Jspec Loading Errors"""
+    pass
