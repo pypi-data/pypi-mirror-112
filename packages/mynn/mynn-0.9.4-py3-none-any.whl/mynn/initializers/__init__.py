5a2846a42041fb66c1c76e1372135fb5749dbaa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:22 +0000

    Add package mynn/0.9.4/mynn-0.9.4-py3-none-any.whl

---
+++ packages/mynn/mynn-0.9.4-py3-none-any.whl/mynn/initializers/__init__.py
@@ -0,0 +1,23 @@
+from mygrad.nnet.initializers import (
+    constant,
+    dirac,
+    glorot_normal,
+    glorot_uniform,
+    he_normal,
+    he_uniform,
+    identity,
+    normal,
+    uniform,
+)
+
+__all__ = (
+    "constant",
+    "dirac",
+    "glorot_normal",
+    "glorot_uniform",
+    "he_normal",
+    "he_uniform",
+    "identity",
+    "normal",
+    "uniform",
+)
