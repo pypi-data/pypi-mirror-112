58875e9acd13a96ddc72c2c9dca6cb1954bc1e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:59 +0000

    Add package benchmark-runner/1.0.0/benchmark-runner-1.0.0.tar.gz

---
+++ packages/benchmark-runner/benchmark-runner-1.0.0.tar.gz/benchmark-runner-1.0.0/setup.py
@@ -0,0 +1,54 @@
+from codecs import open
+from os import path
+from setuptools import setup, find_packages
+
+
+__version__ = '1.0.0'
+
+here = path.abspath(path.dirname(__file__))
+
+
+if path.isfile(path.join(here, 'README.md')):
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+else:
+    long_description = ""
+
+setup(
+    name='benchmark-runner',
+    version=__version__,
+    description='Benchmark Runner Tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Red Hat',
+    author_email='ebattat@redhat.com',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+
+    zip_safe=False,
+
+    # Find all packages (__init__.py)
+    packages=find_packages(include=['benchmark_runner', 'benchmark_runner.*']),
+
+    install_requires=[
+        'typing==3.7.4.3',
+        'typeguard==2.12.0',
+        'elasticsearch==7.13.2',
+        'elasticsearch_dsl==7.3.0',  # for deep search
+        'pandas==1.2.5',
+        'jinja2==3.0.1',
+        'typeguard==2.12.1',
+        'PyYAML==5.4.1'
+    ],
+
+    setup_requires=['pytest', 'pytest-runner', 'wheel', 'coverage'],
+
+    include_package_data=True,
+
+    # dependency_links=[]
+)
