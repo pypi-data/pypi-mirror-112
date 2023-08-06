43883f02e1290d686dad9fb7c2196466a9554220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:33 +0000

    Add package youtube-stream/0.0.14/youtube-stream-0.0.14.tar.gz

---
+++ packages/youtube-stream/youtube-stream-0.0.14.tar.gz/youtube-stream-0.0.14/youtube_stream/playmedia.py
@@ -0,0 +1,18 @@
+import subprocess, os
+from configparser import ConfigParser
+from .imports import config_file
+def get_path():
+    cfg = ConfigParser()
+    try:
+        cfg.read_string(config_file)
+    except:
+        return r'{} '.format("vlc")
+    if os.name=='nt':
+        path = cfg.get("windows", "vlc")
+    else:
+        path = cfg.get("linux", "vlc")
+    return r'{} '.format(path)
+
+def play(videos):
+    if os.name!='nt': subprocess.Popen(get_path()+ videos +" 2> /dev/null", shell=True)
+    else: subprocess.Popen(get_path()+ videos, shell=True)
