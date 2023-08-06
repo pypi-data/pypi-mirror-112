4f3e9dc22ebe0fcdb7b5df1097d326a7a76995c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:23 +0000

    Add package thesis-api/0.2.13/thesis_api-0.2.13.tar.gz

---
+++ packages/thesis-api/thesis_api-0.2.13.tar.gz/thesis_api-0.2.13/thesis_api/__init__.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+__doc__ = """The API interface to the LaTeX project.
+
+Note that this interface uses the Numpy style python docstrings.
+
+"""
+
+import logging
+import sys
+
+__version__ = "0.2.11"
+
+
+##################
+
+PT_IN_CM: float = 0.03514  # https://tex.stackexchange.com/questions/8260/what-are-the-various-units-ex-em-in-pt-bp-dd-pc-expressed-in-mm
+
+LATEX_CONFIG_DIC = {
+    "arraystretch": 1.8,  # scaling of tables
+    "si_round_precision": 3,  # round precision of siunitx
+    "encoding": "utf-8",  # the encoding of the LaTeX files
+    "tex_file": ".tex",  # the file ending of LaTeX files
+    "scrbook_width": 14.89787,  # the width of the scrbook class in [cm]
+    # This is the line width of the LaTeX class scrbook in [cm], which my be useful for tables, etc.
+    # The line length can be obtained by including the package printlen in praeambel.tex and typing
+    # \uselengthunit{cm}\printlength{\textwidth} in a file.
+    "article_width": 441.01773 * PT_IN_CM,
+}
+"""dict[str, Union[float, int, str]]: The configurations in the LaTeX file.
+"""
+
+
+class CustomFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors."""
+
+    grey = "\x1b[38;21m"
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+def get_logger(name: str, stream: bool = False) -> logging.Logger:
+    """Get the logger.
+
+    Parameters
+    ----------
+    name : str
+        The name of the module to be logged.
+    stream : bool
+        Determine whether to log to console or to a file,
+        by default False.
+
+    Returns
+    -------
+    logging.Logger
+        The logger object.
+
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    if stream:
+        stream_handler = logging.StreamHandler(stream=sys.stdout)
+        stream_handler.setLevel(logging.DEBUG)
+        stream_handler.setFormatter(CustomFormatter())
+        # add the stream handler
+        logger.addHandler(stream_handler)
+        # coloredlogs.install(level="DEBUG", logger=logger)
+    else:
+        file_handler = logging.FileHandler(
+            name + ".log", mode="w", encoding=LATEX_CONFIG_DIC["encoding"]
+        )
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(CustomFormatter())
+        # add the file handler
+        logger.addHandler(file_handler)
+    return logger
---
+++ packages/thesis-api/thesis_api-0.2.13.tar.gz/thesis_api-0.2.13/thesis_api/tools/template_strings.py
@@ -0,0 +1,339 @@
+# -*- coding: utf-8 -*-
+
+__doc__ = """
+This module implements some LaTeX template strings as Python
+template strings, which makes it easy to use them and write to files.
+
+Note that the # type: ignore comment should generally not be used,
+although it seems that in this case there is no other solution
+to the linting problem.
+
+"""
+
+import string
+import pathlib
+from typing import Mapping, Optional
+
+__all__ = [
+    "ChapterTemplate",
+    "SectionTemplate",
+    "SubsectionTemplate",
+    "FigureTemplate",
+    "TableTemplate",
+    "CodeTemplate",
+    "InputTemplate",
+    "SiUnitxTemplate",
+]
+
+
+def reformat_path(path: pathlib.Path) -> str:
+    """Reformat the path structure.
+
+    The ``path`` structure is replaced by the actual path,
+    but the path starts at ``source`` and the windows backslashes \\
+    are replaced by slashes /.
+
+    Parameters
+    ----------
+    path : pathlib.Path
+        The path which should be formatted.
+
+    Returns
+    -------
+    str
+        A string representation of the reformatted path.
+
+    """
+    for i, part in enumerate(path.parts):
+        if part == "source":
+            child: str = "/".join(path.parts[i:])
+            break
+        else:
+            child: str = f"{path}"
+    return child
+
+
+class SiUnitxTemplate(string.Template):
+    def __init__(self, unit: Optional[str], kwds: dict = {}) -> None:
+        """Generate a siunitx macro.
+
+        Parameters
+        ----------
+        unit : Optional[str]
+            The unit of the quantitity.
+        kwds : dict, optional
+            Additional keyword arguments the macros of the siunitx package
+            take, by default {}.
+        
+        """
+        if kwds:
+            opt_args = "\n"
+            for k, v in kwds.items():
+                opt_args += f"{k}={v},"
+            if unit:
+                template: str = "\\qty[" + opt_args + "]{$num}{$unit}"
+
+            else:
+                template: str = "\\num[" + opt_args + "]{$num}"
+        else:
+            if unit:
+                template: str = "\\qty{$num}{$unit}"
+            else:
+                template: str = "\\num{$num}"
+        super().__init__(template)
+
+
+class InputTemplate(string.Template):
+    """A template class for ``input`` statements.
+
+    This template class provides an interface to LaTeX by
+    generating template strings for the LaTeX ``input``
+    command.
+    
+    """
+
+    def __init__(self) -> None:
+        template: str = "\n\\input{$path}\n"
+        super().__init__(template)
+
+    def substitute(
+        self, __mapping: Mapping[str, object], **kwds: object
+    ) -> str:
+        """Substitute the placeholder.
+
+        The ``path`` placeholder is replaced by the actual path,
+        but the path starts at ``chapters`` and the windows backslashes \\
+        are replaced by slashes /.
+
+        Returns
+        -------
+        str
+            The substituted template string.
+
+        """
+        path = _InputTemplate__mapping["path"]  # type: ignore
+        child = reformat_path(path)
+        _InputTemplate__mapping["path"] = child  # type: ignore
+        return super().substitute(_InputTemplate__mapping, **kwds)  # type: ignore
+
+
+class ChapterTemplate(string.Template):
+    """A template class for chapters.
+
+    This template class provides an interface to LaTeX by
+    generating use-ready LaTeX files which contain chapters.
+    
+    """
+
+    def __init__(self) -> None:
+        template: str = (
+            "% !TEX root = ../../main.tex\n\n"
+            "\\chapter{$title}\n"
+            "\\label{cha:$label}\n\n"
+            "This is a chapter.\n"
+        )
+        super().__init__(template)
+
+
+class SectionTemplate(string.Template):
+    """A template class for sections.
+
+    This template class provides an interface to LaTeX by
+    generating use-ready LaTeX files which contain sections.
+    
+    """
+
+    def __init__(self) -> None:
+        template: str = (
+            "\\section{$title}\n"
+            "\\label{sec:$label}\n\n"
+            "This is a section within a chapter.\n"
+        )
+        super().__init__(template)
+
+
+class SubsectionTemplate(string.Template):
+    """A template class for subsections.
+
+    This template class provides an interface to LaTeX by
+    generating use-ready LaTeX files which contain subsections.
+    
+    """
+
+    def __init__(self) -> None:
+        template: str = (
+            "\\subsection{$title}\n"
+            "\\label{subsec:$label}\n\n"
+            "This is a subsection within a section.\n"
+        )
+        super().__init__(template)
+
+
+class _CaptionTemplate(string.Template):
+    """Base class.
+
+    This is a base class for LaTeX templates which can provide a short
+    caption, like figures and pseudo codes.
+    
+    """
+
+    def __init__(self, template: str, short_caption: bool = False) -> None:
+        """Init the class.
+
+        Parameters
+        ----------
+        template : str
+            The template to use.
+        short_caption : bool, optional
+            Determine if a short caption should be used,
+            by default False.
+
+        """
+        self._short_caption = short_caption
+        super().__init__(template)
+
+    def substitute(
+        self, __mapping: Mapping[str, object], **kwds: object
+    ) -> str:
+        """Overwrite the substitute method.
+
+        Depending on the value of ``self._short_caption``, the template is
+        filled differently.
+
+        Returns
+        -------
+        str
+            The template with replaced values.
+
+        """
+        if self._short_caption:
+            long_caption, short_caption = _CaptionTemplate__mapping["caption"]  # type: ignore
+            _CaptionTemplate__mapping["short_caption"] = short_caption  # type: ignore
+            _CaptionTemplate__mapping["long_caption"] = long_caption  # type: ignore
+            _CaptionTemplate__mapping.pop("caption")  # type: ignore
+        return super().substitute(_CaptionTemplate__mapping, **kwds)  # type: ignore
+
+    def safe_substitute(
+        self, __mapping: Mapping[str, object], **kwds: object
+    ) -> str:
+        """Overwrite the ``safe_substitute`` method.
+
+        Depending on the value of ``self._short_caption``, the template is
+        filled differently.
+
+        Returns
+        -------
+        str
+            The template with replaced values.
+
+        """
+        if self._short_caption:
+            long_caption, short_caption = _CaptionTemplate__mapping["caption"]  # type: ignore
+            _CaptionTemplate__mapping["short_caption"] = short_caption  # type: ignore
+            _CaptionTemplate__mapping["long_caption"] = long_caption  # type: ignore
+            _CaptionTemplate__mapping.pop("caption")  # type: ignore
+        return super().safe_substitute(_CaptionTemplate__mapping, **kwds)  # type: ignore
+
+
+class FigureTemplate(_CaptionTemplate):
+    """A template class for figures.
+
+    This template class provides an interface to LaTeX by
+    generating use-ready LaTeX files which contain figures.
+
+    """
+
+    def __init__(self, short_caption: bool = False) -> None:
+        """Init the FigureTemplate.
+
+        Parameters
+        ----------
+        short_caption : bool, optional
+            Determine if a short caption should be used,
+            by default False.
+
+        """
+
+        if short_caption:
+            template: str = (
+                "\\begin{figure}[$position]\n"
+                "\t\\centering\n"
+                "\t\\includegraphics[width=$width\\textwidth]{$path}\n"
+                "\t\\caption[$short_caption]{$long_caption}\n"
+                "\t\\label{fig:$label}\n"
+                "\\end{figure}\n"
+            )
+        else:
+            template: str = (
+                "\\begin{figure}[$position]\n"
+                "\t\\centering\n"
+                "\t\\includegraphics[width=$width\\textwidth]{$path}\n"
+                "\t\\caption{$caption}\n"
+                "\t\\label{fig:$label}\n"
+                "\\end{figure}\n"
+            )
+        super().__init__(template, short_caption)
+
+
+class TableTemplate(string.Template):
+    """A template class for tables.
+
+    This template class provides an interface to LaTeX by
+    generating use-ready LaTeX files which contain tables.
+
+    The tables itself are generated using the pandas
+    ``to_latex`` method of the ``DataFrame`` class,
+    see also [1].
+
+    References
+    ----------
+    [1] https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_latex.html
+
+    """
+
+    def __init__(self) -> None:
+        template: str = (
+            "\\begingroup\n"
+            "\\renewcommand{\\arraystretch}{$arraystretch}\n"
+            "$data\n"
+            "\\endgroup\n"
+        )
+        super().__init__(template)
+
+
+class CodeTemplate(_CaptionTemplate):
+    """A template class for pseudocode.
+
+    This template class provides an interface to LaTeX by
+    generating use-ready LaTeX files which contain pseudo code.
+        
+    """
+
+    def __init__(self, short_caption: bool = False) -> None:
+        """Init the CodeTemplate.
+
+        Parameters
+        ----------
+        short_caption : bool, optional
+            Determine if a short caption should be used,
+            by default False.
+
+        """
+        if short_caption:
+            template: str = (
+                "\\begin{listing}[$position]\n"
+                "\t\\inputminted{$language}{$path}\n"
+                "\t\\caption[$short_caption]{$long_caption}\n"
+                "\t\\label{lst:$label}\n"
+                "\\end{listing}\n"
+            )
+        else:
+            template: str = (
+                "\\begin{listing}[$position]\n"
+                "\t\\inputminted{$language}{$path}\n"
+                "\t\\caption{$caption}\n"
+                "\t\\label{lst:$label}\n"
+                "\\end{listing}\n"
+            )
+        super().__init__(template, short_caption)
+
