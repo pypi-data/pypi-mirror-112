e25cf34bd02b9af55a2b94276e4bf61700bb49c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:10 +0000

    Add package cait/1.0.0/cait-1.0.0.tar.gz

---
+++ packages/cait/cait-1.0.0.tar.gz/cait-1.0.0/cait/datasets/__init__.py
@@ -0,0 +1,12 @@
+from ._pt_dataset import *
+from ._pt_datamodule import *
+from ._pt_sampler import *
+from ._pt_transforms import *
+
+__all__ = ['H5CryoData',
+           'CryoDataModule',
+           'get_random_samplers',
+           'RemoveOffset',
+           'Normalize',
+           'DownSample',
+           'ToTensor']
---
+++ packages/cait/cait-1.0.0.tar.gz/cait-1.0.0/setup.py
@@ -0,0 +1,52 @@
+from setuptools import find_packages, setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cait',
+    version='1.0.0',
+    author='Daniel Bartolot, '
+           'Jens Burkhart, '
+           'Damir Rizvanovic, '
+           'Daniel Schmiedmayer, '
+           'Felix Wagner',
+    author_email="felix.wagner@oeaw.ac.at",
+    description='Cryogenic Artificial Intelligence Tools - A Python Package for the Data Analysis of Rare Event Search Experiments with Machine Learning.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.cryocluster.org/fwagner/cait",
+    license='GPLv3',
+    packages=find_packages(include=['cait', 'cait.*']),
+    install_requires=['setuptools>=47.1.1',
+                      'h5py',
+                      'pickle-mixin',
+                      'numpy',
+                      'matplotlib',
+                      'scipy',
+                      'numba',
+                      'sklearn',
+                      'uproot',
+                      'awkward1',
+                      'torch',
+                      'torchvision',
+                      'pytorch-lightning',
+                      'ipywidgets',
+                      'tqdm',
+                      'pandas',
+                      'plotly',
+                      'dash_html_components',
+                      'dash_core_components',
+                      'dash'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
