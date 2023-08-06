ca2878ed7dbb421efa8bcae21c117eb71a535969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:58 +0000

    Add package neogiinstruments/2.1.1/NeogiInstruments-2.1.1-py3-none-any.whl

---
+++ packages/neogiinstruments/NeogiInstruments-2.1.1-py3-none-any.whl/neogiinstruments/Acquisition.py
@@ -0,0 +1,11 @@
+import thread
+
+def input_thread(a_list):
+    raw_input()
+    a_list.append(True)
+
+def do_stuff():
+    a_list = []
+    thread.start_new_thread(input_thread, (a_list,))
+    while not a_list:
+        stuff()
---
+++ packages/neogiinstruments/NeogiInstruments-2.1.1-py3-none-any.whl/neogiinstruments/test.py
@@ -0,0 +1,1 @@
+import StellarNet as SN
