9017e81ef4ba89c58dd5cd9e7205fda64d0b8357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:55 +0000

    Add package vsource/1.0.11/vsource-1.0.11.tar.gz

---
+++ packages/vsource/vsource-1.0.11.tar.gz/vsource-1.0.11/vsource/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author   : Ecohnoch(xcy)
+# @File     : __init__.py.py
+# @Function : TODO
+
+from vsource.loginInstance import login
+from vsource.algorithms.face_recognition import face_recognition
+from vsource.algorithms.face_detection import face_detection
+from vsource.algorithms.face_attribute import face_attribute
+from vsource.algorithms.speaker_recognition import speaker_recognition
+from vsource.algorithms.face_generation import face_generation
+from vsource.algorithms.random_number import random_number
+
+from vsource.algorithms.new_algorithm import new_algorithm
+from vsource.algorithms.cod_cpd import cod_cpd
+
+from vsource.scripts.algorithm_scripts.algorithms import parse_algorithm
+from vsource.scripts.enroll_service import enroll
+
+__version__ = 'v1.0.11'
+__author__  = 'Ecohnoch'
+__email__   = '542305306@qq.com'
