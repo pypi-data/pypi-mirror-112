86d277585f1aaa7033fce230b1b3f9634f18a989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:22 +0000

    Add package hexlet-code-vp/0.1.0/hexlet_code_vp-0.1.0-py3-none-any.whl

---
+++ packages/hexlet-code-vp/hexlet_code_vp-0.1.0-py3-none-any.whl/brain_games/scripts/brain_games.py
@@ -0,0 +1,12 @@
+#!/home/valentynscivp/ValentynHexletProject/python-project-lvl1/.venv/bin/python3.9
+
+def greet():
+    print('Welcome to the Brain Games!')
+
+
+def main():
+    greet()
+
+
+if __name__ == '__main__':
+    main()
