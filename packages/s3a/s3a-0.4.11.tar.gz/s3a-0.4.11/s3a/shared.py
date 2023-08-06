324fee545c290fe69a1d7eda89b3addbf198501f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:04 +0000

    Add package s3a/0.4.11/s3a-0.4.11.tar.gz

---
+++ packages/s3a/s3a-0.4.11.tar.gz/s3a-0.4.11/s3a/shared.py
@@ -0,0 +1,34 @@
+import logging
+
+from pyqtgraph.Qt import QtCore
+
+from s3a.constants import GEN_PROPS_DIR, SCHEMES_DIR, SHORTCUTS_DIR, IMG_PROC_DIR, \
+  MULT_PRED_DIR, CFG_DIR
+from s3a.parameditors.algcollection import AlgCollection
+from s3a.parameditors.quickloader import QuickLoaderEditor
+from s3a.parameditors.table import TableData
+from s3a.plugins.misc import SettingsPlugin, ShortcutsPlugin
+from s3a.processing import ImgProcWrapper, ImageProcess
+from utilitys import ActionStack, ParamEditor, ShortcutParameter, dockPluginFactory
+from utilitys.fns import AppLogger
+import logging
+
+class SharedAppSettings(QtCore.QObject):
+  def __init__(self, parent=None):
+    super().__init__(parent)
+    self.actionStack = ActionStack()
+
+    self.tableData = TableData()
+    self.filter = self.tableData.filter
+
+    self.imgProcClctn = AlgCollection(ImgProcWrapper, ImageProcess, saveDir=IMG_PROC_DIR,
+                                      template=CFG_DIR/'imageproc.yml')
+    self.multiPredClctn = AlgCollection(saveDir=MULT_PRED_DIR, template=CFG_DIR/'multipred.yml')
+
+    self.settingsPlg = SettingsPlugin()
+    self.colorScheme = self.settingsPlg.colorScheme
+    self.generalProps = self.settingsPlg.generalProps
+
+    self.shortcutsPlg = ShortcutsPlugin()
+    self.shortcuts = self.shortcutsPlg.shortcuts
+    self.quickLoader = self.shortcutsPlg.quickLoader
