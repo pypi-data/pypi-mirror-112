4b73335b12db93c39e062529e4f1709befc1bae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:21 +0000

    Add package gogettr/0.4.0/gogettr-0.4.0-py3-none-any.whl

---
+++ packages/gogettr/gogettr-0.4.0-py3-none-any.whl/gogettr/utils.py
@@ -0,0 +1,29 @@
+"""Defines small utility functions to be used internally in GoGettr."""
+
+
+def merge(*dicts):
+    """Merges the given dictionaries into a single dictionary, ignoring overlapping keys."""
+
+    out = dict()
+    for dictionary in dicts:
+        for (key, val) in dictionary.items():
+            out[key] = val
+    return out
+
+
+# Following two functions adapted from
+# https://stackoverflow.com/questions/1181919/python-base-36-encoding
+def b36encode(number: int) -> str:
+    """Convert the number to base36."""
+    alphabet, base36 = ["0123456789abcdefghijklmnopqrstuvwxyz", ""]
+
+    while number:
+        number, i = divmod(number, 36)
+        base36 = alphabet[i] + base36
+
+    return base36 or alphabet[0]
+
+
+def b36decode(number: str) -> int:
+    """Convert the base36 number to an integer."""
+    return int(number, 36)
