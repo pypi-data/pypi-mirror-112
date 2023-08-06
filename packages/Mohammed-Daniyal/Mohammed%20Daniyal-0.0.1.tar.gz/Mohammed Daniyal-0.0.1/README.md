17785081479fe7045c49d310c8e69c45a3360731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:22 +0000

    Add package Mohammed-Daniyal/0.0.1/Mohammed%20Daniyal-0.0.1.tar.gz

---
+++ packages/Mohammed-Daniyal/Mohammed%20Daniyal-0.0.1.tar.gz/Mohammed Daniyal-0.0.1/README.md
@@ -0,0 +1,8 @@
+# 0.0.1
+We have added "jase_run" which simply takes 3 values 
+
+1. Input value
+2. Questions list
+3. Answers List
+
+then it combines them in to a dictionary and then asks the user for an input and if that input is in the dictionary then it prints the response(from the Answers list)
---
+++ packages/Mohammed-Daniyal/Mohammed%20Daniyal-0.0.1.tar.gz/Mohammed Daniyal-0.0.1/__init__.py
@@ -0,0 +1,12 @@
+question_answer = {}
+
+
+def jase_run(input_character, questions_user, answers_user):
+    for question_ in questions_user : 
+        for answer_ in answers_user: 
+            question_answer[question_] = answer_ 
+            answers_user.remove(answer_) 
+            break
+        while True:
+            qs = input(input_character)
+            print(question_answer[qs])
