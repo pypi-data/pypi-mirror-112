07d79bccbdc2d26d80b4c3ca22c3bcaa0d1131e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:45 +0000

    Add package mditre/0.0.1/mditre-0.0.1.tar.gz

---
+++ packages/mditre/mditre-0.0.1.tar.gz/mditre-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='mditre',
+    version='0.0.1',    
+    description='Microbial Differentibale Temporal Rule Engine',
+    url='https://github.com/gerberlab/mditre',
+    author='Venkata Suhas Maringanti',
+    author_email='vsuhas.m@gmail.com',
+    license='GPLv3',
+    packages=['mditre', 'mditre.datasets'],
+    install_requires=[
+                      'numpy',
+                      'scikit-learn',
+                      'PyQt5==5.11.3',
+                      'ete3',
+                      'matplotlib',
+                      'seaborn',
+                      'pandas',
+                      'scipy',
+                      'dendropy',
+                      'jupyterlab',                    
+                      ],
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    python_requires=">=3.6",
+)
