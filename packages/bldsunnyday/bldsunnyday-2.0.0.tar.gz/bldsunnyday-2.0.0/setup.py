9ae04a0d2b4924f4760ead662d7e397a9e4b5d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:01 +0000

    Add package bldsunnyday/2.0.0/bldsunnyday-2.0.0.tar.gz

---
+++ packages/bldsunnyday/bldsunnyday-2.0.0.tar.gz/bldsunnyday-2.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='bldsunnyday',  # * Your package will have this name
+    packages=['sunnyday'],  # * Name the package again
+    version='2.0.0',  # * To be increased every time you change your library
+    license='MIT',  # Type of license. More here: https://help.github.com/articles/licensing-a-repository
+    description='Weather forecast data',  # Short description of your library
+    author='Ardit Sulce',  # Your name
+    author_email='your.email@example.com',  # Your email
+    # url='https//example.com',  # Homepage of your library (e.g. github or your website)
+    keywords=['weather', 'forecast', 'openweather'],  # Keywords users can search on pypi.org
+    install_requires=['requests'],  # Other 3rd-party libs that pip needs to install
+    classifiers=['Development Status :: 3 - Alpha',  # Chose either '3 - Alpha', '4 - Beta' or
+                 # '5 - Production/Stable' as the current status
+                 'Intended Audience :: Developers',  # Who is the audience for your library?
+                 'Topic :: Software Development :: Build Tools',
+                 'License :: OSI Approved :: MIT License',  # Type a license again
+                 'Programming Language :: Python :: 3.5',  # Python versions that your library supports
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
+                 ]
+)
---
+++ packages/bldsunnyday/bldsunnyday-2.0.0.tar.gz/bldsunnyday-2.0.0/sunnyday/sunnyday.py
@@ -0,0 +1,58 @@
+import requests
+
+
+class Weather:
+    """Creates a Weather object getting an apikey as input
+    and either a city name or lat and lon coordinates.
+
+    Package use example:
+
+    # Create a weather object using a city name:
+    # The api key below is not guaranteed to work.
+    # Get your own apikey from https://openweathermap.org
+    # And wait a couple of hours for the apikey to be activated
+
+    >>> weather1 = Weather(apikey='71bafeefe82e26ce9eebc1bf9ea8293b', city='Seattle')
+
+    # Using latitude and longitude coordinates
+    >>> weather2 = Weather(apikey='71bafeefe82e26ce9eebc1bf9ea8293b', lat=41.1, lon=25)
+
+    # Get complete weather data for the next 12 hours:
+    >>> weather1.next_12h()
+
+    # Simplified data for the next 12 hours:
+    >>> weather1.next_12h_simplified()
+
+    # Sample url to get sky condition icons:
+    http://openweather.org/img/wn/10d@2x.png
+    """
+
+    def __init__(self, apikey, city=None, lat=None, lon=None):
+        if city:
+            url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apikey}&units=imperial'
+            r = requests.get(url)
+            self.data = r.json()
+        elif lat and lon:
+            url = f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apikey}&units=imperial'
+            r = requests.get(url)
+            self.data = r.json()
+        else:
+            raise TypeError("provide either a city or lat and lon arguments")
+
+        if self.data['cod'] != '200':
+            raise ValueError(self.data['message'])
+
+    def next_12h(self):
+        """Returns 3-hour data for the next 12 hours as a dict."""
+        return self.data['list'][:4]
+
+    def next_12h_simplified(self):
+        """Returns date, temperature, and sky condition every 3 hours
+        for the next 12 hours as a tuple of tuples"""
+        simple_data = []
+        for i in self.data['list'][:4]:
+            simple_data.append((i['dt_txt'],
+                                i['main']['temp'],
+                                i['weather'][0]['description'],
+                                i['weather'][0]['icon']))
+        return simple_data
