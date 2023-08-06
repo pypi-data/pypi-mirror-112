9df28a4e249f732e9479b17c28236e74eaeef4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:21 +0000

    Add package pycce/0.6.5a0/pycce-0.6.5a0.tar.gz

---
+++ packages/pycce/pycce-0.6.5a0.tar.gz/pycce-0.6.5a0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='pycce',
+    version='0.6.5a',
+    url='',
+    license='',
+    author='Nikita Onizhuk',
+    author_email='onizhuk@uchicago.edu',
+    description='A package to compute spin dynamics using CCE method',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy', 'scipy', 'ase', 'pandas'
+    ],
+    include_package_data=True,
+    package_data={'': ['bath/*.txt']},
+)
