a5559151e685eff5474ea1d066e81f8c2f66b082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:56 +0000

    Add package yarp-middleware/3.4.7.dev908/yarp_middleware-3.4.7.dev908-cp37-cp37m-manylinux_2_24_x86_64.whl

---
+++ packages/yarp-middleware/yarp_middleware-3.4.7.dev908-cp37-cp37m-manylinux_2_24_x86_64.whl/yarp/include/yarp/dev/IVisualParams.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef YARP_DEV_IVISUALPARAMS_H
+#define YARP_DEV_IVISUALPARAMS_H
+
+#include <yarp/conf/system.h>
+#if !defined(YARP_INCLUDING_DEPRECATED_HEADER_YARP_DEV_IVISUALPARAMS_H_ON_PURPOSE)
+YARP_COMPILER_WARNING("<yarp/dev/IVisualParams.h> file is deprecated")
+#endif
+
+#include <yarp/dev/IRgbVisualParams.h>
+#include <yarp/dev/IDepthVisualParams.h>
+
+#endif   // YARP_DEV_IVISUALPARAMS_H
