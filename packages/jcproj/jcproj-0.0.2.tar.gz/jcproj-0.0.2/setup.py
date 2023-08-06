26f3df61865ce278f5e8afe337533af9c8f7a177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:39 +0000

    Add package jcproj/0.0.2/jcproj-0.0.2.tar.gz

---
+++ packages/jcproj/jcproj-0.0.2.tar.gz/jcproj-0.0.2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name = "jcproj",
+    version = "0.0.2",
+    author = "Li Jicher",
+    author_email='lijichen8c@163.com',
+    description = "this is a mess package to solve something",
+    packages = setuptools.find_packages(),
+)
