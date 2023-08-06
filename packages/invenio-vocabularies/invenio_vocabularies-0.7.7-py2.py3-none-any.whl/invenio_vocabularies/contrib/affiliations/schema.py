85872604e649219cad4ceb52c6490c30e4b6073e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:38 +0000

    Add package invenio-vocabularies/0.7.7/invenio_vocabularies-0.7.7-py2.py3-none-any.whl

---
+++ packages/invenio-vocabularies/invenio_vocabularies-0.7.7-py2.py3-none-any.whl/invenio_vocabularies/contrib/affiliations/schema.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2021 CERN.
+#
+# Invenio-Vocabularies is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Affiliations schema."""
+
+from marshmallow import fields
+from marshmallow_utils.fields import IdentifierSet, SanitizedUnicode
+from marshmallow_utils.schemas import IdentifierSchema
+
+from ...services.schema import BaseVocabularySchema
+
+
+class AffiliationSchema(BaseVocabularySchema):
+    """Service schema for affiliations."""
+
+    acronym = SanitizedUnicode()
+    identifiers = IdentifierSet(fields.Nested(IdentifierSchema))
+    name = SanitizedUnicode(required=True)
