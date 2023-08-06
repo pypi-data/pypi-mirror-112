d189a93d922b6f053e7dd24ea53e8a384346a45a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:26 +0000

    Add package shortestpaths/1.0.0/shortestpaths-1.0.0-py3-none-any.whl

---
+++ packages/shortestpaths/shortestpaths-1.0.0-py3-none-any.whl/shortestpaths/__init__.py
@@ -0,0 +1,21 @@
+# __init__.py is part of ShortestPaths
+#
+# ShortestPaths is free software; you may redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version. You should have received a copy of the GNU General Pu-
+# blic License along with this program. If not, see
+# <https://www.gnu.org/licenses/>.
+#
+# (C) 2020 Athanasios Mattas
+# ==========================================================================
+"""Special project variables"""
+
+__name__ = 'shortestpaths'
+__version__ = '1.0.0'
+__author__ = 'Athanasios Mattas'
+__author_email__ = 'thanasismatt@gmail.gr'
+__description__ = "Bidirectional replacement paths and k-shortest paths search with dynamic programming"
+__url__ = 'https://github.com/ThanasisMattas/shortestpaths.git'
+__license__ = 'GNU General Public License v3'
+__copyright__ = 'Copyright 2020 Athanasios Mattas'
