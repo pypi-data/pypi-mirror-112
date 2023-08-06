7cae10859f91bbafbd57d34736bbc34934a4d8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:23 +0000

    Add package nautobot-golden-config/0.9.5/nautobot_golden_config-0.9.5-py3-none-any.whl

---
+++ packages/nautobot-golden-config/nautobot_golden_config-0.9.5-py3-none-any.whl/README.md
@@ -0,0 +1,162 @@
+# nautobot-golden-config
+
+A plugin for [Nautobot](https://github.com/nautobot/nautobot) that intends to provide context around golden configuration.
+
+**This version is currently in Beta and will require a rebuild of the database for a 1.0 release.**
+
+# Overview
+
+You may see the [Navigating Overview](./docs/navigating-golden.md) documentation for an overview of navigating through the different areas of this plugin. You may also see the [FAQ](./docs/FAQ.md) for commonly asked questions.
+
+The golden configuration plugin performs four primary actions, each of which can be toggled on with a respective `enable_*` setting, covered in detail 
+later in the readme. 
+
+* Configuration Backup - Is a Nornir process to connect to devices, optionally parse out lines/secrets, backup the configuration, and save to a Git repository.
+    * see [Navigating Backup](./docs/navigating-backup.md) for more information
+* Configuration Intended - Is a Nornir process to generate configuration based on a Git repo of Jinja files and a Git repo to store the intended configuration.
+    * see [Navigating Intended](./docs/navigating-intended.md) for more information
+* Source of Truth Aggregation - Is a GraphQL query per device with that creates a data structure used in the generation of configuration.
+    * see [Navigating SoTAgg](./docs/navigating-sot-agg.md) for more information
+* Configuration Compliance - Is a Nornir process to run comparison of the actual (via backups) and intended (via Jinja file creation) CLI configurations.
+    * see [Navigating Compliance](./docs/navigating-compliance.md) for more information
+
+The operator's of their own Nautobot instance are welcome to use any combination of these features. Though the appearance may seem like they are tightly 
+coupled, this isn't actually the case. For example, one can obtain backup configurations from their current RANCID/Oxidized process and simply provide a Git Repo
+of the location of the backup configurations, and the compliance process would work the same way. Also, another user may only want to generate configurations,
+but not want to use other features, which is perfectly fine to do so.
+
+## Screenshots
+
+There are many features and capabilities the plugin provides into the Nautobot ecosystem. The following screenshots are intended to provide a quick visual overview of some of these features.
+
+The golden configuration is driven by jobs that run a series of tasks and the result is captured in this overview.
+
+![Overview](./docs/img/golden-overview.png)
+
+The compliance report provides a high-level overview on the compliance of your network.
+![Compliance Report](./docs/img/compliance-report.png)
+
+The compliance overview will provide a per device and feature overview on the compliance of your network devices.
+![Compliance Overview](./docs/img/compliance-overview.png)
+
+Drilling into a specific device and feature, you can get an immediate detailed understanding of your device.
+![Compliance Device](./docs/img/compliance-device.png)
+
+![Compliance Rule](./docs/img/compliance-rule.png)
+
+## Plugin Settings
+
+There is a setting to determine the inclusion of any of the four given components.
+
+* The `enable_backup`, `enable_compliance`, `enable_intended`, and `enable_sotagg` will toggle inclusion of the entire component.
+
+# Installation
+
+Plugins can be installed manually or use Python's `pip`. See the [nautobot documentation](https://nautobot.readthedocs.io/en/latest/plugins/#install-the-package) for more details. The pip package name for this plugin is [`nautobot-golden-config`](https://pypi.org/project/nautobot-golden-config/)
+
+> The plugin is compatible with Nautobot 1.0.0 and higher
+
+**Prerequisite:** The plugin relies on [`nautobot_plugin_nornir`](https://pypi.org/project/nautobot-plugin-nornir/) to be installed and both plugins to be enabled in your configuration settings.
+
+**Required:** The following block of code below shows the additional configuration required to be added to your `nautobot_config.py` file:
+- append `"nautobot_golden_config"` to the `PLUGINS` list
+- append the `"nautobot_golden_config"` dictionary to the `PLUGINS_CONFIG` dictionary
+
+```python
+PLUGINS = ["nautobot_plugin_nornir", "nautobot_golden_config"]
+
+PLUGINS_CONFIG = {
+    "nautobot_plugin_nornir": {
+        "nornir_settings": {
+            "credentials": "nautobot_plugin_nornir.plugins.credentials.env_vars.CredentialsEnvVars",
+            "runner": {
+                "plugin": "threaded",
+                "options": {
+                    "num_workers": 20,
+                },
+            },
+        },
+    },
+    "nautobot_golden_config": {
+        "per_feature_bar_width": 0.15,
+        "per_feature_width": 13,
+        "per_feature_height": 4,
+        "enable_backup": True,
+        "enable_compliance": True,
+        "enable_intended": True,
+        "enable_sotagg": True,
+        "sot_agg_transposer": None,
+        "platform_slug_map": None,
+    },
+}
+
+```
+
+The plugin behavior can be controlled with the following list of settings. 
+
+| Key     | Example | Default | Description                          |
+| ------- | ------ | -------- | ------------------------------------- |
+| enable_backup | True | True | A boolean to represent whether or not to run backup configurations within the plugin. |
+| enable_compliance | True | True | A boolean to represent whether or not to run the compliance process within the plugin. |
+| enable_intended | True | True | A boolean to represent whether or not to generate intended configurations within the plugin. |
+| enable_sotagg | True | True | A boolean to represent whether or not to provide a GraphQL query per device to allow the intended configuration to provide data variables to the plugin. |
+| platform_slug_map | {"cisco_wlc": "cisco_aireos"} | None | A dictionary in which the key is the platform slug and the value is what netutils uses in any "network_os" parameter. |
+| sot_agg_transposer | mypkg.transposer | - | A string representation of a function that can post-process the graphQL data. |
+| per_feature_bar_width | 0.15 | 0.15 | The width of the table bar within the overview report | 
+| per_feature_width | 13 | 13 | The width in inches that the overview table can be. | 
+| per_feature_height | 4 | 4 | The height in inches that the overview table can be. | 
+
+> Note: Over time the intention is to make the compliance report more dynamic, but for now allow users to configure the `per_*` configs in a way that fits best for them.
+
+> Note: Review [`nautobot_plugin_nornir`](https://pypi.org/project/nautobot-plugin-nornir/) for Nornir and dispatcher configuration options. 
+
+# Contributing
+
+Pull requests are welcomed and automatically built and tested against multiple versions of Python and Nautobot through TravisCI.
+
+The project is packaged with a light development environment based on `docker-compose` to help with the local development of the project and to run tests within TravisCI.
+
+The project is following Network to Code software development guidelines and are leveraging the following:
+- Black, Pylint, Bandit, flake8, and pydocstyle for Python linting and formatting.
+- Django unit test to ensure the plugin is working properly.
+
+## CLI Helper Commands
+
+The project features a CLI helper based on [invoke](http://www.pyinvoke.org/) to help setup the development environment. The commands are listed below in 3 categories:
+- `dev environment`
+- `utility`
+- `testing`. 
+
+Each command can be executed with `invoke <command>`. All commands support the arguments `--nautobot-ver` and `--python-ver` if you want to manually define the version of Python and Nautobot to use. Each command also has its own help `invoke <command> --help`
+
+### Local dev environment
+
+```
+  build            Build all docker images.
+  debug            Start Nautobot and its dependencies in debug mode.
+  destroy          Destroy all containers and volumes.
+  restart          Restart Nautobot and its dependencies in detached mode.
+  start            Start Nautobot and its dependencies in detached mode.
+  stop             Stop Nautobot and its dependencies.
+```
+
+### Utility 
+
+```
+  cli              Launch a bash shell inside the running Nautobot container.
+  create-user      Create a new user in django (default: admin), will prompt for password.
+  makemigrations   Run Make Migration in Django.
+  nbshell          Launch a nbshell session.
+```
+
+### Testing 
+
+```
+  bandit           Run bandit to validate basic static code security analysis.
+  black            Run black to check that Python files adhere to its style standards.
+  flake8           Run flake8 to check that Python files adhere to its style standards.
+  pydocstyle       Run pydocstyle to validate docstring formatting adheres to NTC defined standards.
+  pylint           Run pylint code analysis.
+  tests            Run all tests for this plugin.
+  unittest         Run Django unit tests for the plugin.
+```
---
+++ packages/nautobot-golden-config/nautobot_golden_config-0.9.5-py3-none-any.whl/nautobot_golden_config/tables.py
@@ -0,0 +1,320 @@
+"""Django Tables2 classes for golden_config plugin."""
+import copy
+
+from django.utils.html import format_html
+from django_tables2 import Column, LinkColumn, TemplateColumn
+from django_tables2.utils import A
+
+from nautobot.utilities.tables import (
+    BaseTable,
+    ToggleColumn,
+)
+from nautobot_golden_config import models
+from nautobot_golden_config.utilities.constant import ENABLE_BACKUP, ENABLE_COMPLIANCE, ENABLE_INTENDED, CONFIG_FEATURES
+
+BACKUP_SUCCESS = """
+{% if record.backup_last_success_date and record.backup_last_attempt_date == record.backup_last_success_date %}
+    <span class="text-success" id="actions">
+{% else %}
+    <span class="text-danger" id="actions">
+{% endif %}
+{% if record.backup_last_success_date %}
+        {{ record.backup_last_success_date|date:"SHORT_DATETIME_FORMAT" }}
+{% else %}
+    --
+{% endif %}
+        <span id=actiontext>{{ record.backup_last_attempt_date|date:"SHORT_DATETIME_FORMAT" }}</span>
+    </span>
+"""
+
+INTENDED_SUCCESS = """
+{% if record.intended_last_success_date and record.intended_last_attempt_date == record.intended_last_success_date %}
+    <span class="text-success" id="actions">
+{% else %}
+    <span class="text-danger" id="actions">
+{% endif %}
+{% if record.intended_last_success_date %}
+        {{ record.intended_last_success_date|date:"SHORT_DATETIME_FORMAT" }}
+{% else %}
+    --
+{% endif %}
+        <span id=actiontext>{{ record.intended_last_attempt_date|date:"SHORT_DATETIME_FORMAT" }}</span>
+    </span>
+"""
+
+
+COMPLIANCE_SUCCESS = """
+{% if record.compliance_last_success_date and record.compliance_last_attempt_date == record.compliance_last_success_date %}
+    <span class="text-success" id="actions">
+{% else %}
+    <span class="text-danger" id="actions">
+{% endif %}
+{% if record.compliance_last_success_date %}
+        {{ record.compliance_last_success_date|date:"SHORT_DATETIME_FORMAT" }}
+{% else %}
+    --
+{% endif %}
+        <span id=actiontext>{{ record.compliance_last_attempt_date|date:"SHORT_DATETIME_FORMAT" }}</span>
+    </span>
+"""
+
+ALL_ACTIONS = """
+{% if backup == True %}
+    {% if record.backup_config %}
+        <a value="{% url 'plugins:nautobot_golden_config:configcompliance_details' pk=record.device.pk config_type='backup' %}" class="openBtn" data-href="{% url 'plugins:nautobot_golden_config:configcompliance_details' pk=record.device.pk config_type='backup' %}?modal=true"> 
+            <i class="mdi mdi-file-document-outline" title="Backup Configuration"></i>
+        </a>
+    {% else %}
+        <i class="mdi mdi-circle-small"></i>
+    {% endif %}
+{% endif %}
+{% if intended == True %}
+    {% if record.intended_config %}
+        <a value="{% url 'plugins:nautobot_golden_config:configcompliance_details' pk=record.device.pk config_type='intended' %}" class="openBtn" data-href="{% url 'plugins:nautobot_golden_config:configcompliance_details' pk=record.device.pk config_type='intended' %}?modal=true"> 
+            <i class="mdi mdi-text-box-check-outline" title="Intended Configuration"></i>
+        </a>
+    {% else %}
+        <i class="mdi mdi-circle-small"></i>
+    {% endif %}
+{% endif %}
+{% if compliance == True %}
+    {% if record.compliance_config %}
+        <a value="{% url 'plugins:nautobot_golden_config:configcompliance_details' pk=record.device.pk config_type='compliance' %}" class="openBtn" data-href="{% url 'plugins:nautobot_golden_config:configcompliance_details' pk=record.device.pk config_type='compliance' %}?modal=true"> 
+            <i class="mdi mdi-file-compare" title="Compliance Details"></i>
+        </a>
+    {% else %}
+        <i class="mdi mdi-circle-small"></i>
+    {% endif %}
+{% endif %}
+{% if sotagg == True %}
+    <a value="{% url 'plugins:nautobot_golden_config:configcompliance_details' pk=record.device.pk config_type='sotagg' %}" class="openBtn" data-href="{% url 'plugins:nautobot_golden_config:configcompliance_details' pk=record.device.pk config_type='sotagg' %}?modal=true"> 
+        <i class="mdi mdi-code-json" title="SOT Aggregate Data"></i>
+    </a>
+    <a href="{% url 'extras:job' class_path='plugins/nautobot_golden_config.jobs/AllGoldenConfig' %}?device={{ record.device.pk }}"
+        <span class="text-primary">
+            <i class="mdi mdi-play-circle" title="Execute All Golden Config Jobs"></i>
+        </span>
+    </a>
+{% endif %}
+"""
+
+MATCH_CONFIG = """{{ record.match_config|linebreaksbr }}"""
+
+
+def actual_fields():
+    """Convienance function to conditionally toggle columns."""
+    active_fields = ["pk", "device__name"]
+    if ENABLE_BACKUP:
+        active_fields.append("backup_last_success_date")
+    if ENABLE_INTENDED:
+        active_fields.append("intended_last_success_date")
+    if ENABLE_COMPLIANCE:
+        active_fields.append("compliance_last_success_date")
+    active_fields.append("actions")
+    return tuple(active_fields)
+
+
+#
+# Columns
+#
+
+
+class PercentageColumn(Column):
+    """Column used to display percentage."""
+
+    def render(self, value):
+        """Render percentage value."""
+        return f"{value} %"
+
+
+class ComplianceColumn(Column):
+    """Column used to display config compliance status (True/False/None)."""
+
+    def render(self, value):
+        """Render an entry in this column."""
+        if value == 1:  # pylint: disable=no-else-return
+            return format_html('<span class="text-success"><i class="mdi mdi-check-bold"></i></span>')
+        elif value == 0:
+            return format_html('<span class="text-danger"><i class="mdi mdi-close-thick"></i></span>')
+        else:  # value is None
+            return format_html('<span class="mdi mdi-minus"></span>')
+
+
+#
+# Tables
+#
+
+# ConfigCompliance
+class ConfigComplianceTable(BaseTable):
+    """Table for rendering a listing of Device entries and their associated ConfigCompliance record status."""
+
+    pk = ToggleColumn(accessor=A("device"))
+    device = TemplateColumn(
+        template_code="""<a href="{% url 'plugins:nautobot_golden_config:configcompliance_devicedetail' pk=record.device  %}" <strong>{{ record.device__name }}</strong></a> """
+    )
+
+    def __init__(self, *args, **kwargs):
+        """Override default values to dynamically add columns."""
+        # Used ConfigCompliance.objects on purpose, vs queryset (set in args[0]), as there were issues with that as
+        # well as not as expected from user standpoint (e.g. not always the same values on columns depending on
+        # filtering)
+        features = list(
+            models.ConfigCompliance.objects.order_by("rule__feature__name")
+            .values_list("rule__feature__name", flat=True)
+            .distinct()
+        )
+        extra_columns = [(feature, ComplianceColumn(verbose_name=feature)) for feature in features]
+        kwargs["extra_columns"] = extra_columns
+        # Nautobot's BaseTable.configurable_columns() only recognizes columns in self.base_columns,
+        # so override the class's base_columns to include our additional columns as configurable.
+        self.base_columns = copy.deepcopy(self.base_columns)
+        for feature, column in extra_columns:
+            self.base_columns[feature] = column
+        super().__init__(*args, **kwargs)
+
+    class Meta(BaseTable.Meta):
+        """Metaclass attributes of ConfigComplianceTable."""
+
+        model = models.ConfigCompliance
+        fields = (
+            "pk",
+            "device",
+        )
+        # All other fields (ConfigCompliance names) are constructed dynamically at instantiation time - see views.py
+
+
+class ConfigComplianceGlobalFeatureTable(BaseTable):
+    """Table for feature compliance report."""
+
+    name = Column(accessor="rule__feature__slug", verbose_name="Feature")
+    count = Column(accessor="count", verbose_name="Total")
+    compliant = Column(accessor="compliant", verbose_name="Compliant")
+    non_compliant = Column(accessor="non_compliant", verbose_name="Non-Compliant")
+    comp_percent = PercentageColumn(accessor="comp_percent", verbose_name="Compliance (%)")
+
+    class Meta(BaseTable.Meta):
+        """Metaclass attributes of ConfigComplianceGlobalFeatureTable."""
+
+        model = models.ConfigCompliance
+        fields = ["name", "count", "compliant", "non_compliant", "comp_percent"]
+        default_columns = [
+            "name",
+            "count",
+            "compliant",
+            "non_compliant",
+            "comp_percent",
+        ]
+
+
+class ConfigComplianceDeleteTable(BaseTable):
+    """Table for device compliance report."""
+
+    feature = Column(accessor="rule__feature__name", verbose_name="Feature")
+
+    class Meta(BaseTable.Meta):
+        """Metaclass attributes of ConfigComplianceDeleteTable."""
+
+        device = Column(accessor="device__name", verbose_name="Device Name")
+        compliance = Column(accessor="compliance", verbose_name="Compliance")
+        model = models.ConfigCompliance
+        fields = ("device", "feature", "compliance")
+
+
+# GoldenConfig
+
+
+class GoldenConfigTable(BaseTable):
+    """Table to display Config Management Status."""
+
+    pk = ToggleColumn()
+    device__name = TemplateColumn(
+        template_code="""<a href="{% url 'dcim:device' pk=record.device.pk %}">{{ record.device }}</a>""",
+        verbose_name="Device",
+    )
+    if ENABLE_BACKUP:
+        backup_last_success_date = TemplateColumn(verbose_name="Backup Status", template_code=BACKUP_SUCCESS)
+    if ENABLE_INTENDED:
+        intended_last_success_date = TemplateColumn(verbose_name="Intended Status", template_code=INTENDED_SUCCESS)
+    if ENABLE_COMPLIANCE:
+        compliance_last_success_date = TemplateColumn(
+            verbose_name="Compliance Status", template_code=COMPLIANCE_SUCCESS
+        )
+
+    actions = TemplateColumn(
+        template_code=ALL_ACTIONS, verbose_name="Actions", extra_context=CONFIG_FEATURES, orderable=False
+    )
+
+    class Meta(BaseTable.Meta):
+        """Meta for class CircuitMaintenanceTable."""
+
+        model = models.GoldenConfig
+        fields = actual_fields()
+
+
+# ComplianceFeature
+
+
+class ComplianceFeatureTable(BaseTable):
+    """Table to display Compliance Features."""
+
+    pk = ToggleColumn()
+    name = LinkColumn("plugins:nautobot_golden_config:compliancefeature", args=[A("pk")])
+
+    class Meta(BaseTable.Meta):
+        """Table to display Compliance Features Meta Data."""
+
+        model = models.ComplianceFeature
+        fields = ("pk", "name", "slug", "description")
+        default_columns = ("pk", "name", "slug", "description")
+
+
+# ComplianceRule
+
+
+class ComplianceRuleTable(BaseTable):
+    """Table to display Compliance Rules."""
+
+    pk = ToggleColumn()
+    feature = LinkColumn("plugins:nautobot_golden_config:compliancerule", args=[A("pk")])
+    match_config = TemplateColumn(template_code=MATCH_CONFIG)
+
+    class Meta(BaseTable.Meta):
+        """Table to display Compliance Rules Meta Data."""
+
+        model = models.ComplianceRule
+        fields = ("pk", "feature", "platform", "description", "config_ordered", "match_config", "config_type")
+        default_columns = ("pk", "feature", "platform", "description", "config_ordered", "match_config", "config_type")
+
+
+# ConfigRemove
+
+
+class ConfigRemoveTable(BaseTable):
+    """Table to display Compliance Rules."""
+
+    pk = ToggleColumn()
+    name = LinkColumn("plugins:nautobot_golden_config:configremove", args=[A("pk")])
+
+    class Meta(BaseTable.Meta):
+        """Table to display Compliance Rules Meta Data."""
+
+        model = models.ConfigRemove
+        fields = ("pk", "name", "platform", "description", "regex")
+        default_columns = ("pk", "name", "platform", "description", "regex")
+
+
+# ConfigReplace
+
+
+class ConfigReplaceTable(BaseTable):
+    """Table to display Compliance Rules."""
+
+    pk = ToggleColumn()
+    name = LinkColumn("plugins:nautobot_golden_config:configreplace", args=[A("pk")])
+
+    class Meta(BaseTable.Meta):
+        """Table to display Compliance Rules Meta Data."""
+
+        model = models.ConfigReplace
+        fields = ("pk", "name", "platform", "description", "regex", "replace")
+        default_columns = ("pk", "name", "platform", "description", "regex", "replace")
