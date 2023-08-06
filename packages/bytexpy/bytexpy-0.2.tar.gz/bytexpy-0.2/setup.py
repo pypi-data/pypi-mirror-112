49c0f25d12e8de7bb941d41bc06cd7022b436789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:02 +0000

    Add package bytexpy/0.2/bytexpy-0.2.tar.gz

---
+++ packages/bytexpy/bytexpy-0.2.tar.gz/bytexpy-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bytexpy',
+      version='0.2',
+      description='Easy Graphics',
+      packages=['bytexpy'],
+      author_email='spgerghelp@gmail.com',
+      long_description=open('README.md').read(),
+      zip_safe=False)
