c0f65fce9c7e08c00be5bd383555ed31bf569a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:46 +0000

    Add package ping3/2.8.1/ping3-2.8.1.tar.gz

---
+++ packages/ping3/ping3-2.8.1.tar.gz/ping3-2.8.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name='ping3',
+    description='A pure python3 version of ICMP ping implementation using raw socket.',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kyan001/ping3',
+    author='Kai Yan',
+    author_email='kai@kyan001.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: System :: Networking',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='python3 ping icmp socket tool',
+    packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests']),
+    py_modules=["ping3", "command_line_ping3", "errors", "enums"],
+    python_requires='>=3',
+    install_requires=[],
+    extras_require={},
+    package_data={},
+    data_files=[],
+    entry_points={
+        'console_scripts': ['ping3=command_line_ping3:main'],
+    },
+)
