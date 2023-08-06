c24b17f9868daff185971d4e1c38438ccf6f26e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:07 +0000

    Add package thesis-api/0.2.10/thesis_api-0.2.10.tar.gz

---
+++ packages/thesis-api/thesis_api-0.2.10.tar.gz/thesis_api-0.2.10/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['thesis_api', 'thesis_api.tools']
+
+package_data = \
+{'': ['*'], 'thesis_api.tools': ['templates/*']}
+
+setup_kwargs = {
+    'name': 'thesis-api',
+    'version': '0.2.10',
+    'description': '',
+    'long_description': None,
+    'author': 'Julian Stang',
+    'author_email': 'julian.stang@tum.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
