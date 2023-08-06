43c3d9a6c667b4658d35ee0c5280b3afa30c30d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:50 +0000

    Add package probabilities-test-higor/1.2/probabilities_test_higor-1.2.tar.gz

---
+++ packages/probabilities-test-higor/probabilities_test_higor-1.2.tar.gz/probabilities_test_higor-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probabilities_test_higor',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['probabilities_test_higor'],
+      author = 'Higor de Faria',
+      author_mail = 'higorpr@gmail.com',
+      zip_safe=False)
