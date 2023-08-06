69d144f8c23d70f7d6e9d70d13c16a58c37755d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:35 +0000

    Add package aqib1lib/0.0.1/aqib1lib-0.0.1.tar.gz

---
+++ packages/aqib1lib/aqib1lib-0.0.1.tar.gz/aqib1lib-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+VERSION = '0.0.1'
+DESCRIPTION = 'A basic hello package'
+
+# Setting up
+setup(
+    name="aqib1lib",
+    version=VERSION,
+    author="Muhammad Aqib",
+    author_email="<inbox.aqib@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
