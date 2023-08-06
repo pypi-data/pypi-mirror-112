b4103cea85cbb883b08d74ad1f760e3b6252d365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:16 +0000

    Add package igraph/0.1.14/igraph-0.1.14.tar.gz

---
+++ packages/igraph/igraph-0.1.14.tar.gz/igraph-0.1.14/README.md
@@ -0,0 +1,23 @@
+Deprecation notice
+==================
+
+To avoid name collision with the igraph project, the original igraph visualization
+library has been renamed to `jgraph` in December 2015. The `igraph` package on
+PyPI will soon be used by the igraph project.
+
+The authors of `jgraph` (ex-igraph) and `python-igraph` (soon-to-be igraph)
+project have agreed on a transition period during which old projects still
+using `igraph` as a PyPI dependency will receive an error while trying to
+import igraph in their own projects. The transition period ends on Sep 1 2021.
+
+Things to do now
+----------------
+
+If you wanted to install the graph visualization library by Patrick Fuller,
+please install `jgraph` instead. You have time until Sep 1 2021 to do this.
+
+If you wanted to install the Python interface of the igraph network analysis
+library, please install `python-igraph` instead. After Sep 1 2021, you can
+start installing `igraph` instead. `python-igraph` will keep on working at
+least until Sep 1 2022.
+
---
+++ packages/igraph/igraph-0.1.14.tar.gz/igraph-0.1.14/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name="igraph",
+    version="0.1.14",
+    description="View graph data structures in the IPython notebook (deprecated).",
+    url="http://github.com/patrickfuller/jgraph/",
+    license="MIT",
+    author="Patrick Fuller, Tamas Nepusz",
+    author_email="patrickfuller@gmail.com, ntamas@gmail.com",
+    package_dir={'igraph': 'igraph'},
+    packages=['igraph'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Education',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: IPython',
+        'Topic :: Education :: Computer Aided Instruction (CAI)'
+    ],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
+)
+
