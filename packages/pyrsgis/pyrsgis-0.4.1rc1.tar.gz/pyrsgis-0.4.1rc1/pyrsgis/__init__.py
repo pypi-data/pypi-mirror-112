cd1c0f2ac03a3350cc0ad3b6b2dfb3988173c55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:48 +0000

    Add package pyrsgis/0.4.1rc1/pyrsgis-0.4.1rc1.tar.gz

---
+++ packages/pyrsgis/pyrsgis-0.4.1rc1.tar.gz/pyrsgis-0.4.1rc1/pyrsgis/__init__.py
@@ -0,0 +1,12 @@
+"""
+pyrsgis - Python for Remote Sensing and GIS
+author: pratkrt<at>gmail.com
+Compatible with Python versions 3+
+"""
+
+name = 'pyrsgis'
+__version__ = "0.4.1rc1"
+
+
+#Importing all functions from beta
+from .beta import *
