1539a0fb66e51b4df5d90395d240610cccdf8d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:33 +0000

    Add package otumba/0.1/otumba-0.1.tar.gz

---
+++ packages/otumba/otumba-0.1.tar.gz/otumba-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='otumba',
+      version='0.1',
+      description='Gaussian amd Binomial distributions',
+      packages=['otumba'],
+      author = 'Pedro Coronel Santeliz',
+      author_email = 'pelsanmexico@gmail.com',
+      zip_safe=False)
