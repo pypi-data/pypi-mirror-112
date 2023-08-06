9b5690dcea77fcba036965b1ffaaa8c173d573e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:38 +0000

    Add package functional-streams/1.5.0/functional_streams-1.5.0-py3-none-any.whl

---
+++ packages/functional-streams/functional_streams-1.5.0-py3-none-any.whl/streams/utilities.py
@@ -0,0 +1,13 @@
+
+
+default_reduce_initializer = 0
+
+def generator_from_list(data):
+    for row in data:
+        yield row
+
+
+def generator_from_list_of_lists(toplist):
+    for values in toplist:
+        for value in values:
+            yield value
