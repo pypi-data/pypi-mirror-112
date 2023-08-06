a51c1e449d21c66b352a660bbf75c7673197b765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:01 +0000

    Add package rashsetup/0.2.4/RashSetup-0.2.4-py3-none-any.whl

---
+++ packages/rashsetup/RashSetup-0.2.4-py3-none-any.whl/RashSetup/RashScrappers/RashScrappers/spiders/README.py
@@ -0,0 +1,43 @@
+import scrapy
+
+
+class ReadmeSpider(scrapy.Spider):
+    name = 'README'
+    allowed_domains = ['github.com']
+
+    def __init__(
+            self, pipe, url
+    ):
+        super().__init__()
+
+        self.start_urls = url
+        self.pipe = pipe
+
+    def start_requests(self):
+        yield scrapy.Request(
+            self.start_urls, errback=self.error_pipe
+        )
+
+    def error_pipe(self, *args):
+
+        self.pipe["result"] = {
+            "failed": True,
+            "exception": str(args[0]),
+            "result": False
+        }
+
+    def parse(self, response, *args):
+        got = response.xpath("//div[@id='readme']")
+
+        result = got.extract()
+
+        self.pipe["result"] = {
+            "failed": not bool(result[0]),
+            "result": result[0] if result else False,
+            "exception": ""
+        }
+
+        yield {
+            "extracted": response.request.url,
+            "success": bool(result)
+        }
