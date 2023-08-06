6227d65cc16530e4b29e0f75b6da1ef1262a9610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:25 +0000

    Add package crypto-bot/3.3.9/crypto_bot-3.3.9-py3-none-any.whl

---
+++ packages/crypto-bot/crypto_bot-3.3.9-py3-none-any.whl/crypto_bot/app.py
@@ -0,0 +1,66 @@
+# Init application
+import asyncio
+import sys
+
+from crypto_bot.bots import price_bot, bot_globals, info_bot
+from crypto_bot.config import ConfigLoader, init_logger
+from crypto_bot.exchanges import Exchange
+from crypto_bot.price_indexer import PriceIndexer
+from crypto_bot.twitter_collector import TwitterCollector
+
+try:
+    cfg = sys.argv[1]
+except:
+    cfg = "config.yml"
+
+bot_globals.config_loader = ConfigLoader(cfg)
+config = bot_globals.config_loader.active_config
+
+logger = init_logger(config['process']['log_level'])
+logger.info("Config loaded")
+
+exchanges = [Exchange.create(c, dict(d)) for c, d in config['exchanges'].items()]
+bot_globals.indexer = indexer = PriceIndexer(exchanges, config['process']['update_rate'])
+indexer.wait_exchanges()
+
+logger.info("Start Bots")
+price_bots = config['discord'].get('price_bots')
+info_bots = config['discord'].get('info_bots')
+twitter_cfg = config.get('twitter')
+twitter_collector = TwitterCollector(twitter_cfg) if twitter_cfg else None
+
+loop = asyncio.get_event_loop()
+bot_list = []
+
+# Load price bots
+if price_bots:
+    logger.info("Preload initial coins")
+    init_coins = price_bots['instances'].values()
+    for c in init_coins:
+        indexer.add_new_coin(c)
+
+    for i, c in enumerate(price_bots['instances'].items()):
+        chat_id = str(i + 1) if i + 1 > 9 else "0{}".format(i + 1)
+        bot = price_bot.create_bot(*c, price_bots.get('price_bot_avatar'),
+                                   chat_id, price_bots['command_roles'], indexer)
+        loop.create_task(bot.start())
+        bot_list.append(bot)
+
+# Load info bots
+if info_bots:
+    for token, cfg in info_bots.items():
+
+        cfg['token'] = token
+        countdowns = cfg.get('countdowns') or {}
+        cfg['countdowns'] = []
+        for c in countdowns:
+            alert = config['discord']['countdowns'][c]
+            alert['channels'] = countdowns[c]
+            cfg['countdowns'].append(alert)
+
+    bot = info_bot.create_bot(cfg, indexer, twitter_collector)
+    loop.create_task(bot.start())
+    bot_list.append(bot)
+
+indexer.run()
+loop.run_forever()
