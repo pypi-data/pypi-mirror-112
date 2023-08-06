6374056542e78e7c88dfa0aecf7e42443951ec1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:07 +0000

    Add package rgb/0.0.3/rgb-0.0.3.tar.gz

---
+++ packages/rgb/rgb-0.0.3.tar.gz/rgb-0.0.3/rgb/printer.py
@@ -0,0 +1,23 @@
+from .Color import Color
+
+
+def rgbprint(text, color=None):
+    if color is None:
+        print(text)
+        return
+
+    if type(color) == str:
+        color = Color(color)
+    elif not isinstance(color, Color):
+        raise ValueError("colorize() requires color specified with a hex code or a Color() instance.")
+    
+    print(colorize(text, color))
+
+
+def colorize(text, color):
+    if type(color) == str:
+        color = Color(color)
+    elif not isinstance(color, Color):
+        raise ValueError("colorize() requires color specified with a hex code or a Color() instance.")
+
+    return f"\033[38;2;{color.r};{color.g};{color.b}m{text}\033[0m"
