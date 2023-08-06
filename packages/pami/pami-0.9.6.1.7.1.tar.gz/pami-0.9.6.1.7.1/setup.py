b6632c60bd13ed50385b8f96d10122cc6da3e356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:47 +0000

    Add package pami/0.9.6.1.7.1/pami-0.9.6.1.7.1.tar.gz

---
+++ packages/pami/pami-0.9.6.1.7.1.tar.gz/pami-0.9.6.1.7.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'pami',
+    version = '0.9.6.1.7.1',
+    author = 'Rage Uday Kiran',
+    author_email = 'uday.rage@gmail.com',
+    description = 'This software is the product of the University of Aizu, Aizu-Wakamatsu, Fukushima, Japan.',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/udayRage/PAMI',
+    license='GPLv3',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+	'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires = '>=3.6',
+)
