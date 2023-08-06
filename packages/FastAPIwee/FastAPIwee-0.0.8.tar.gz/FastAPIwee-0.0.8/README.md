61d087cf8f9b02b1ee889e2c41d01da6c0419858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:52 +0000

    Add package FastAPIwee/0.0.8/FastAPIwee-0.0.8.tar.gz

---
+++ packages/FastAPIwee/FastAPIwee-0.0.8.tar.gz/FastAPIwee-0.0.8/README.md
@@ -0,0 +1,44 @@
+# FastAPIwee
+FastAPI + PeeWee = <3
+
+## Using
+
+Python >= 3.6 :snake:
+
+## Installation
+
+```python
+pip install FastAPIwee
+```
+
+:tada:
+
+## Documentation
+
+Documentation can be found here: https://fastapiwee.qqmber.wtf
+
+## Development
+
+### Documentation
+
+Documentation is made with [MkDocs](https://www.mkdocs.org) and [Material for MkDocs](https://squidfunk.github.io/mkdocs-material/).
+
+To run MkDocs server:
+
+- Go to mkdocs directory
+
+    ```bash
+    cd mkdocs
+    ```
+
+- Run server
+
+    ```bash
+    mkdocs serve
+    ```
+
+To build use:
+
+```bash
+mkdocs build
+```
---
+++ packages/FastAPIwee/FastAPIwee-0.0.8.tar.gz/FastAPIwee-0.0.8/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+with open("VERSION", "r") as version_f:
+    version = version_f.read()
+
+setuptools.setup(
+    name="FastAPIwee",
+    version=version,
+    author="German Gensetskyi",
+    author_email="Ignis2497@gmail.com",
+    description="FastAPIwee - FastAPI + PeeWee = <3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ignisor/FastAPIwee",
+    project_urls={
+        'Documentation': 'https://fastapiwee.qqmber.wtf',
+    },
+    packages=setuptools.find_packages(exclude=('tests', )),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'fastapi==0.66.0',
+        'pydantic==1.8.2',
+        'peewee==3.14.4',
+    ],
+)
