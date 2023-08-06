588aab8d80872ef6697cbd1fc06e0c9dd132b2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:29 +0000

    Add package matrix-alg-basic/0.1/matrix-alg-basic-0.1.tar.gz

---
+++ packages/matrix-alg-basic/matrix-alg-basic-0.1.tar.gz/matrix-alg-basic-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='matrix-alg-basic',
+      version='0.1',
+      description='Basic Matrix Algebra Operation',
+      packages=['matrix-alg-basic'],
+      author= 'Linn Forsman',
+      author_email = 'linnforsman@protonmail.com',
+      zip_safe=False)
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
