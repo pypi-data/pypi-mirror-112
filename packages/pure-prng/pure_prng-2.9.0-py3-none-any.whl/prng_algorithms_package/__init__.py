ee7f8952bb2212c8117c10c10e613af2e339b485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:00 +0000

    Add package pure-prng/2.9.0/pure_prng-2.9.0-py3-none-any.whl

---
+++ packages/pure-prng/pure_prng-2.9.0-py3-none-any.whl/prng_algorithms_package/__init__.py
@@ -0,0 +1,23 @@
+'''
+prng_algorithms - This package is a collection of PRNG algorithms.
+Copyright (C) 2021  sosei
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+from prng_algorithms_package.ICG_module import *
+from prng_algorithms_package.LCG64_32_ext_module import *
+from prng_algorithms_package.XSM64_module import *
+from prng_algorithms_package.SplitMix64_module import *
+from prng_algorithms_package.Ran64_module import *
