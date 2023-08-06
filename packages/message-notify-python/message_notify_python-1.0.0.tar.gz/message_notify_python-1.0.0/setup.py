9948f5a2e51fd266cd58daa5dea72acb9578aba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:28 +0000

    Add package message-notify-python/1.0.0/message_notify_python-1.0.0.tar.gz

---
+++ packages/message-notify-python/message_notify_python-1.0.0.tar.gz/message_notify_python-1.0.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+requirements = open('requirements.txt').readlines() # 依赖文件
+
+print(requirements)
+
+setuptools.setup(
+    name="message_notify_python",
+    version="1.0.0",
+    author="alvin",
+    author_email="pingtang000@foxmail.com",
+    description="message notify",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qqingdou/message_notify_python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'pycryptodome>=3.10.1',
+        'requests>=2.25.1',
+    ]
+)
