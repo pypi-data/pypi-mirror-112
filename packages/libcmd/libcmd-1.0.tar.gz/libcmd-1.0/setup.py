1dec3b32c9b1d3e1fba6c2691f4830c8a5cdf610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:53 +0000

    Add package libcmd/1.0/libcmd-1.0.tar.gz

---
+++ packages/libcmd/libcmd-1.0.tar.gz/libcmd-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='libcmd',
+      version='1.0',
+      description='Create commands and call them across a socket.',
+      url='https://github.com/ylmonkey/libcmd',
+      author='403',
+      author_email='chodecode69@gmail.com',
+      license='my meat',
+      packages=['libcmd'],
+      zip_safe=False)
