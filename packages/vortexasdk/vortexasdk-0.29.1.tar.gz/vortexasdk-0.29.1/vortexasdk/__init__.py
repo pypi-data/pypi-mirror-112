ba72dbd07f613a5924301a2881744d58471606c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:53 +0000

    Add package vortexasdk/0.29.1/vortexasdk-0.29.1.tar.gz

---
+++ packages/vortexasdk/vortexasdk-0.29.1.tar.gz/vortexasdk-0.29.1/vortexasdk/__init__.py
@@ -0,0 +1,30 @@
+"""Vortexa Python SDK."""
+# noinspection PyUnresolvedReferences
+from vortexasdk.endpoints import (
+    Attributes,
+    CargoMovements,
+    CargoTimeSeries,
+    Corporations,
+    Geographies,
+    Products,
+    VesselMovements,
+    Vessels,
+    TonneMilesBreakdown,
+    VesselAvailabilitySearch,
+    VesselAvailabilityTimeseries,
+    VesselAvailabilityBreakdown,
+    FleetUtilisationTimeseries,
+    FleetUtilisationSpeedBreakdown,
+    FleetUtilisationAvgDistanceTimeseries,
+    FleetUtilisationCapacityTimeseries,
+    FleetUtilisationQuantityTimeseries,
+    FleetUtilisationDestinationBreakdown,
+    FleetUtilisationOriginBreakdown,
+    EIAForecasts,
+)
+
+# noinspection PyUnresolvedReferences
+from vortexasdk.version import __version__
+
+# noinspection PyUnresolvedReferences
+from vortexasdk.check_setup import run_all_checks
