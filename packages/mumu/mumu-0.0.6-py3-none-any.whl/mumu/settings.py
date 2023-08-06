d8f18a9610630598adc25a0744dadb64c75e7623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:47 +0000

    Add package mumu/0.0.6/mumu-0.0.6-py3-none-any.whl

---
+++ packages/mumu/mumu-0.0.6-py3-none-any.whl/mumu/settings.py
@@ -0,0 +1,26 @@
+databases = {
+    'impala': {
+        'type': 'impala',
+        'host': ['10.9.8.253', '10.9.8.253', '10.9.8.253', '10.9.8.253', '10.9.8.253', '10.9.8.253'],
+        'port': 25005,
+        'user': 'yumingmin',
+        'password': 'yumingmin',
+        'auth_mechanism': 'PLAIN'
+    },
+    'impala_bak': {
+        'type': 'impala',
+        'host': ['10.9.8.38', '10.9.8.42', '10.9.8.38', '10.9.8.42', '10.9.8.38', '10.9.8.42'],
+        'port': 21050,
+        'user': 'yumingmin',
+        'password': 'yumingmin',
+        'auth_mechanism': 'PLAIN'
+    },
+    'hive': {
+        'type': 'hive',
+        'host': ['10.9.8.252', '10.9.8.252', '10.9.8.252', '10.9.8.252', '10.9.8.252', '10.9.8.252'],
+        'port': 25005,
+        'user': 'yumingmin',
+        'password': 'yumingmin',
+        'auth_mechanism': 'PLAIN'
+    }
+}
