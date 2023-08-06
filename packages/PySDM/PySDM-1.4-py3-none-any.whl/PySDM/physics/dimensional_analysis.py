ea58df3725bf324b5e024558ea1ae4fb84e2e890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:23 +0000

    Add package PySDM/1.4/PySDM-1.4-py3-none-any.whl

---
+++ packages/PySDM/PySDM-1.4-py3-none-any.whl/PySDM/physics/dimensional_analysis.py
@@ -0,0 +1,17 @@
+from importlib import reload
+from PySDM.physics import constants
+from PySDM.physics import formulae
+from PySDM.physics.impl import flag
+
+
+class DimensionalAnalysis:
+
+    def __enter__(*_):
+        flag.DIMENSIONAL_ANALYSIS = True
+        reload(constants)
+        reload(formulae)
+
+    def __exit__(*_):
+        flag.DIMENSIONAL_ANALYSIS = False
+        reload(constants)
+        reload(formulae)
---
+++ packages/PySDM/PySDM-1.4-py3-none-any.whl/PySDM/products/dynamics/__init__.py
@@ -0,0 +1,5 @@
+from .coalescence import *
+from .condensation import *
+from .displacement import *
+from .dynamic_wall_time import DynamicWallTime
+from .aqueous_chemistry import *
