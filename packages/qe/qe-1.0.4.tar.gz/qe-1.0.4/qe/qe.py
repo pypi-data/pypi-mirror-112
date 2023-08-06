009daab9085f4e41c4520f272b8d9d024a0637de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:01 +0000

    Add package qe/1.0.4/qe-1.0.4.tar.gz

---
+++ packages/qe/qe-1.0.4.tar.gz/qe-1.0.4/qe/qe.py
@@ -0,0 +1,36 @@
+import uuid
+import os
+import pandas as pd
+from tempfile import mkstemp
+
+
+def qe(df, applyTable = True, name=None):
+    fh, abs_path = mkstemp(suffix='.xlsx')
+
+    if isinstance(df, pd.Series):
+        df = pd.DataFrame(df)
+
+    if applyTable:
+        writer = pd.ExcelWriter(abs_path, engine='xlsxwriter')
+        df.to_excel(writer, sheet_name='Sheet1')
+        worksheet = writer.sheets['Sheet1']
+
+        index_name = df.index.name
+        if index_name is None: index_name = ''
+        c = [{'header': str(index_name)}] + [{'header': str(x)} for x in df.columns]
+
+        worksheet.add_table(0, 0, len(df), len(df.columns), {'columns': c})
+
+        writer.save()
+
+    else:
+        df.to_excel(abs_path)
+
+    os.close(fh)
+    os.system("start Excel " + abs_path)
+
+
+if __name__ == '__main__':
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    df = pd.DataFrame(data=d)
+    qe(df)
