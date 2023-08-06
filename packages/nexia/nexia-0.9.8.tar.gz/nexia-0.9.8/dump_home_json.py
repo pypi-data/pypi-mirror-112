503c528f7f6d53f0c220dd77769fca1985a70aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:06 +0000

    Add package nexia/0.9.8/nexia-0.9.8.tar.gz

---
+++ packages/nexia/nexia-0.9.8.tar.gz/nexia-0.9.8/dump_home_json.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Nexia Climate Device Access"""
+
+import argparse
+
+from nexia.home import NexiaHome
+from nexia.const import BRAND_NEXIA
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--brand", type=str, help="Brand (nexia or asair).")
+parser.add_argument("--username", type=str, help="Your username/email address.")
+parser.add_argument("--password", type=str, help="Your password.")
+args = parser.parse_args()
+brand = args.brand or BRAND_NEXIA
+
+if args.username and args.password:
+    nexia_home = NexiaHome(username=args.username, password=args.password, brand=brand)
+else:
+    parser.print_help()
+    exit()
+
+print(nexia_home.devices_json)
+print(nexia_home.automations_json)
