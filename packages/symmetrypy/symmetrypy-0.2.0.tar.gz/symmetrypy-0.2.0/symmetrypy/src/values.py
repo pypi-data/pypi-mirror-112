710039636ff8ba4c8f7bd79dd8117afd7947c75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:31 +0000

    Add package symmetrypy/0.2.0/symmetrypy-0.2.0.tar.gz

---
+++ packages/symmetrypy/symmetrypy-0.2.0.tar.gz/symmetrypy-0.2.0/symmetrypy/src/values.py
@@ -0,0 +1,7 @@
+
+TOKEN_PROGRAM_ID = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+
+class cluster_endpoints:
+    MAINNET_BETA = "https://api.mainnet-beta.solana.com"
+    MAINNET_BETA_SERUM = "https://solana-api.projectserum.com"
+    
