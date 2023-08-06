2fcef40031be518521dbb49072ac14158d31a464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:58 +0000

    Add package bm-data-eng/0.1.1/bm_data_eng-0.1.1-py3-none-any.whl

---
+++ packages/bm-data-eng/bm_data_eng-0.1.1-py3-none-any.whl/bm_data_eng/s3_to_gcs.py
@@ -0,0 +1,13 @@
+def transfer_to_gcs(
+        s3, s3_bucket, s3_key, gcs, gcs_bucket, gcs_key):
+    source_obj = s3.get_object(Bucket=s3_bucket, Key=s3_key)['Body'].read()
+    target_blob = gcs.get_bucket(gcs_bucket).blob(gcs_key)
+    target_blob.upload_from_string(source_obj)
+
+
+def gcs_uri(bucket, key):
+    return f'gs://{bucket}/{strip_slash(key)}'
+
+
+def strip_slash(s):
+    return s if not s.startswith('/') else s[1:]
