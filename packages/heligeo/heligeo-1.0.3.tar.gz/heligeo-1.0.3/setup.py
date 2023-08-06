2c5f0980bc0d386e2bc72c96408f1ab6cfa0b50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:53 +0000

    Add package heligeo/1.0.3/heligeo-1.0.3.tar.gz

---
+++ packages/heligeo/heligeo-1.0.3.tar.gz/heligeo-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import pathlib
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="heligeo",
+    version = '1.0.3',
+    description="Python client for requests to heligeo API services",
+    long_description = README,
+    long_description_content_type="text/markdown",
+    author="nandan",
+    author_email="mail2nandan18@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=["heligeo"],
+    install_requires=["requests"]
+)
