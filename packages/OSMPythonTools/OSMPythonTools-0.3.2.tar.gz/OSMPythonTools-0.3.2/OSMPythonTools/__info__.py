ada5cfc283219019fcb5ccfa48f970cdfea4a08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:02 +0000

    Add package OSMPythonTools/0.3.2/OSMPythonTools-0.3.2.tar.gz

---
+++ packages/OSMPythonTools/OSMPythonTools-0.3.2.tar.gz/OSMPythonTools-0.3.2/OSMPythonTools/__info__.py
@@ -0,0 +1,3 @@
+pkgName = 'OSMPythonTools'
+pkgVersion = '0.3.2'
+pkgUrl = 'https://github.com/mocnik-science/osm-python-tools'
---
+++ packages/OSMPythonTools/OSMPythonTools-0.3.2.tar.gz/OSMPythonTools-0.3.2/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+pkgName='OSMPythonTools'
+pkgVersion='0.3.2'
+pkgUrl='https://github.com/mocnik-science/osm-python-tools'
+
+with open('./OSMPythonTools/__info__.py', 'w') as f:
+    f.write('pkgName = \'%s\'\n' % pkgName)
+    f.write('pkgVersion = \'%s\'\n' % pkgVersion)
+    f.write('pkgUrl = \'%s\'\n' % pkgUrl)
+
+setuptools.setup(
+    name=pkgName,
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'geojson',
+        'lxml',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'pytest',
+        'pytest-sugar',
+        'ujson',
+        'xarray',
+    ],
+    version=pkgVersion,
+    author='Franz-Benjamin Mocnik',
+    author_email='mail@mocnik-science.net',
+    description='A library to access OpenStreetMap related services',
+    license='GPL-3',
+    url=pkgUrl,
+    download_url='',
+    keywords=['OpenStreetMap', 'OSM', 'service', 'overpass', 'nominatim'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+    ],
+)
