8fd8cfba1a455e9486041b5888b69b18acd35a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:10 +0000

    Add package varz/0.7.2/varz-0.7.2.tar.gz

---
+++ packages/varz/varz-0.7.2.tar.gz/varz-0.7.2/tests/test_minimise.py
@@ -0,0 +1,170 @@
+import jax.numpy as jnp
+import lab as B
+import numpy as np
+import pytest
+import tensorflow as tf
+import torch
+import varz.autograd
+import varz.jax
+import varz.tensorflow
+import varz.torch
+from varz import Vars
+from varz.minimise import _convert_and_validate_names
+
+from .util import approx, Value, OutStream
+
+_rate = 5e-2
+_minimise_method_params = []
+for dtype_name in ["float32", "float64"]:
+    _minimise_method_params.extend(
+        [
+            (getattr(np, dtype_name), varz.minimise_l_bfgs_b, {}),
+            (
+                getattr(np, dtype_name),
+                varz.minimise_adam,
+                {"rate": _rate, "local_rates": False},
+            ),
+            (
+                getattr(np, dtype_name),
+                varz.minimise_adam,
+                {"rate": _rate, "local_rates": True},
+            ),
+        ]
+    )
+    for backend in [tf, torch, jnp]:
+        for jit in [False, True]:
+            _minimise_method_params.extend(
+                [
+                    (
+                        getattr(backend, dtype_name),
+                        varz.minimise_l_bfgs_b,
+                        {"jit": jit},
+                    ),
+                    (
+                        getattr(backend, dtype_name),
+                        varz.minimise_adam,
+                        {"rate": _rate, "jit": jit, "local_rates": False},
+                    ),
+                    (
+                        getattr(backend, dtype_name),
+                        varz.minimise_adam,
+                        {"rate": _rate, "jit": jit, "local_rates": True},
+                    ),
+                ]
+            )
+
+
+@pytest.fixture(params=_minimise_method_params)
+def minimise_method(request):
+    yield request.param
+
+
+def test_convert_and_validate_names():
+    assert _convert_and_validate_names(None) == []
+    assert _convert_and_validate_names([]) == []
+    assert _convert_and_validate_names("test") == ["test"]
+    assert _convert_and_validate_names(["a", "b"]) == ["a", "b"]
+    with pytest.raises(ValueError):
+        _convert_and_validate_names(["a", ["b"]])
+    with pytest.raises(ValueError):
+        _convert_and_validate_names([1, "a"])
+
+
+def test_docstring(minimise_method):
+    assert minimise_method.__doc__ is not None
+
+
+def test_no_jit_autograd():
+    vs = Vars(dtype=np.float64)
+
+    def f(vs_):
+        return vs_.ubnd(name="x")
+
+    with pytest.raises(ValueError):
+        varz.autograd.minimise_l_bfgs_b(f, vs, jit=True)
+    with pytest.raises(ValueError):
+        varz.autograd.minimise_adam(f, vs, jit=True)
+
+
+def test_minimise(minimise_method):
+    dtype, minimise, kw_args = minimise_method
+    vs = Vars(dtype=dtype)
+
+    # Initialise a variable that is not used.
+    vs.ubnd(name="other")
+
+    # Define some objective.
+    def f(vs_):
+        return (-3 - vs_.pos(name="x", init=1.0)) ** 2
+
+    # Minimise it, until convergence.
+    val_opt = minimise(f, vs, iters=2000, **kw_args)
+
+    # Check for equality up to two digits.
+    approx(val_opt, 9, atol=1e-2)
+    approx(vs["x"], 0, atol=1e-2)
+
+
+def test_minimise_disconnected_gradient(minimise_method):
+    dtype, minimise, kw_args = minimise_method
+    vs = Vars(dtype=dtype)
+    vs.ubnd(name="x")
+
+    # Check that optimiser runs for objective that returns the constant zero.
+    minimise(lambda v: B.cast(v.dtype, 0), vs, **kw_args)
+
+
+def test_minimise_exception(minimise_method):
+    dtype, minimise, kw_args = minimise_method
+    vs = Vars(dtype=dtype)
+
+    # Skip this tests when the JIT is used, because tracing may then fail.
+    if "jit" in kw_args and kw_args["jit"]:
+        return
+
+    first_call = Value(True)
+
+    # Define an objective that sometimes fails after the first call.
+    def f(vs_):
+        if first_call.val:
+            first_call.val = False
+        else:
+            if np.random.rand() > 0.5:
+                raise Exception("Fail!")
+        return vs_.ubnd(name="x", init=5.0) ** 2
+
+    # Check that the optimiser runs.
+    minimise(f, vs, **kw_args)
+
+
+def test_minimise_zero_calls(minimise_method):
+    dtype, minimise, kw_args = minimise_method
+    vs = Vars(dtype=dtype)
+
+    calls = Value(0)
+
+    def f(vs_):
+        calls.val += 1
+        return vs_.ubnd(name="x", init=5.0) ** 2
+
+    # Check that running the optimiser for zero iterations only incurs a
+    # single call.
+    minimise(f, vs, iters=0, **kw_args)
+    assert calls.val == 1
+
+
+def test_minimise_trace(minimise_method):
+    dtype, minimise, kw_args = minimise_method
+
+    def f(vs_):
+        return vs_.ubnd(name="x") ** 2
+
+    # Test that `trace=False` prints nothing.
+    with OutStream() as stream:
+        minimise(f, Vars(dtype=dtype), trace=False, **kw_args)
+        assert stream.output == ""
+
+    # Test that `trace=False` prints something.
+    with OutStream() as stream:
+        minimise(f, Vars(dtype=dtype), trace=True, **kw_args)
+        assert stream.output != ""
---
+++ packages/varz/varz-0.7.2.tar.gz/varz-0.7.2/varz/torch/minimise.py
@@ -0,0 +1,56 @@
+import logging
+
+import lab as B
+import torch
+import torch.autograd as autograd
+
+from ..minimise import make_l_bfgs_b, make_adam, exception
+
+__all__ = ["minimise_l_bfgs_b", "minimise_adam"]
+
+log = logging.getLogger(__name__)
+
+
+def _wrap_f(vs, names, f, jit, _convert):
+    # Differentiable assignments will overwrite the variables, so make a copy with
+    # detached variables.
+    vs_copy = vs.copy(detach=True)
+
+    # Keep track of function evaluations.
+    f_evals = []
+
+    def f_vectorised(x):
+        vs_copy.set_latent_vector(x, *names, differentiable=True)
+        return f(vs_copy)
+
+    if jit:
+        # It appears that PyTorch is not able to JIT through `autograd.grad`, so we
+        # must already JIT here.
+        f_vectorised = B.jit(f_vectorised)
+
+    def f_value_and_grad(x):
+        x.requires_grad_(True)
+        obj_value = f_vectorised(x)
+        grad = autograd.grad(obj_value, x)[0]
+        return obj_value, grad
+
+    def f_wrapped(x):
+        x = B.cast(vs.dtype, x)
+
+        # Compute objective function value and gradient.
+        try:
+            obj_value, grad = f_value_and_grad(x)
+        except Exception as e:
+            return exception(x, e)
+
+        # Perform requested conversion.
+        obj_value, grad = _convert(obj_value, grad)
+
+        f_evals.append(obj_value)
+        return obj_value, grad
+
+    return f_evals, f_wrapped
+
+
+minimise_l_bfgs_b = make_l_bfgs_b(_wrap_f)
+minimise_adam = make_adam(_wrap_f)
