8496247e3abda302936f7081778ba4002edc0668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:38 +0000

    Add package minushalf/1.4/minushalf-1.4-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/minushalf/minushalf-1.4-cp39-cp39-manylinux2014_x86_64.whl/minushalf/data/softwares.py
@@ -0,0 +1,24 @@
+"""
+List softwares supported
+by the CLI
+"""
+from enum import Enum, unique
+
+
+@unique
+class Softwares(Enum):
+    """
+    Enum type for the softwares supported by the program
+    """
+
+    vasp = "VASP"
+
+    def __str__(self):
+        return str(self.name)
+
+    @staticmethod
+    def to_list():
+        """
+        Generate list of available softwares
+        """
+        return list(map(lambda element: element.value, Softwares))
