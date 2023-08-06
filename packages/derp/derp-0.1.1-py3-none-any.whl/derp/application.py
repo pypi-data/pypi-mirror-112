42336c9c7692a6def7dd7dd5678831fa6dbbfefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:11 +0000

    Add package derp/0.1.1/derp-0.1.1-py3-none-any.whl

---
+++ packages/derp/derp-0.1.1-py3-none-any.whl/derp/application.py
@@ -0,0 +1,112 @@
+import os
+import re
+from typing import Optional, List, Dict
+from derp.version_number import VersionNumber
+from derp.walker import collect_deprecation_errors
+
+
+class Application:
+    """Abstract the application as a class.
+
+    This class is stateful. It stores results as it runs and is meant to be exited
+    after it runs, not reused.
+
+    Parameters
+    ----------
+    target: str
+        path to the file or directory to scan for deprecations
+    version: str
+        version number, either as a string or a path to a file that contains the version number
+    """
+
+    def __init__(self, target: str, version: str):
+        self.target = target
+        self.version = version
+        self.current_version: Optional[VersionNumber] = None
+        self.file_paths: Optional[List[str]] = None
+        self.failures: Dict[str, List[str]] = None
+        self.catastrophic_failure = False
+
+    def _initialize_absolute_paths(self):
+        """Initialize a list of all paths to inspect."""
+        all_files = []
+        target_path = os.path.join(os.getcwd(), self.target)
+        if os.path.isfile(target_path):
+            all_files = [target_path]
+        elif os.path.isdir(target_path):
+            for path, subdirs, files in os.walk(target_path):
+                for name in files:
+                    this_path = os.path.abspath(os.path.join(path, name))
+                    all_files.append(this_path)
+        else:
+            raise ValueError(f"{self.target} must correspond to a file or directory")
+        all_files = [file for file in all_files if file.endswith(".py")]
+        if len(all_files) == 0:
+            raise ValueError(f"No python modules found at {self.target}")
+        self.file_paths = all_files
+
+    def _initialize_version_number(self):
+        """Initialize current version number either by parsing a file or a version string."""
+        if os.path.isfile(self.version):
+            with open(self.version) as fp:
+                file_text = fp.read()
+                # regex captures sequence of integers separated by periods, surrounded by quotes
+                pattern = "[\'\"]((?:\\d+\\.)*\\d+)[\'\"]"
+                matches = list(re.finditer(pattern, file_text))
+                if len(matches) == 0:
+                    raise ValueError(f"File {self.version} did not contain snippets that could "
+                                     f"be parsed as version numbers")
+                elif len(matches) == 1:
+                    version_string = matches[0].groups()[0]
+                    self.current_version = VersionNumber(version_string)
+                else:
+                    all_matches = [match.group() for match in matches]
+                    raise ValueError(f"File {self.version} contains multiple snippets that could"
+                                     f"be parsed as verison numbers: {all_matches}")
+        else:
+            self.current_version = VersionNumber(self.version)
+
+    def initialize(self):
+        """Set class attributes that are not passed in directly."""
+        self._initialize_version_number()
+        self._initialize_absolute_paths()
+
+    def run_checks(self):
+        """Run deprecation check against all files."""
+        assert isinstance(self.current_version, VersionNumber)
+        self.failures = dict()
+        for file_path in self.file_paths:
+            errors = collect_deprecation_errors(file_path, self.current_version)
+            if errors is not None and len(errors) > 0:
+                self.failures[file_path] = errors
+
+    def report(self):
+        """Print failures"""
+        if self.failures:
+            for path, errors in self.failures.items():
+                print(f"{path}:")
+                print('\t' + '\n\t'.join(errors))
+
+    def _run(self):
+        self.initialize()
+        self.run_checks()
+        self.report()
+
+    def run(self):
+        """Run the application, catching exceptions and keyboard interrupts"""
+        try:
+            self._run()
+        except (ValueError, AssertionError) as exc:
+            print(exc)
+            self.catastrophic_failure = True
+        except KeyboardInterrupt:
+            print("Caught keyboard interrupt from user")
+            self.catastrophic_failure = True
+
+    def exit(self) -> int:
+        """Return exit code, 0 for success or 1 for failure"""
+        found_errors = self.failures is not None and len(self.failures) > 0
+        if self.catastrophic_failure or found_errors:
+            return 1
+        else:
+            return 0
---
+++ packages/derp/derp-0.1.1-py3-none-any.whl/derp/deprecation.py
@@ -0,0 +1,130 @@
+"""Classes corresponding to way a developer might mark something as deprecated.
+Currently, the only supported option is to use the @deprecated decorator from python's
+deprecation library. This is captured with PythonDeprecation. Additional types of deprecation
+can be added by creating new classes that extend Deprecation, and including them in the list
+``DEPRECATION_TYPE_LIST`` in __init__.py.
+"""
+
+import ast
+from abc import abstractmethod, ABC
+from typing import List, Optional
+from derp.version_number import VersionNumber
+
+
+class Deprecation(ABC):
+    """Abstract representation of a way to mark deprecations.
+    There are two things any child class ust implement.
+    * An initialization method that takes a node of type ast.AST and throws a ValueError
+        if it can't be parsed. Deprecations should only be initialized within a
+        try-except block.
+    * A check_error method that takes the current version and returns a string if there is
+        an error, otherwise returns None.
+    """
+
+    @abstractmethod
+    def check_error(self, current_version: VersionNumber) -> Optional[str]:
+        """Return a user-facing error message if the deprecation is invalid."""
+        raise NotImplementedError
+
+
+class PythonDeprecation(Deprecation):
+    """Attempt to parse a decorator from the deprecation library.
+
+    Because we don't know if a node corresponds to a deprecation until we parse it,
+    we use exceptions to control the flow of the program.
+    ValueErrors are typical when initializing PythonDeprecation, and so it should only
+    be initialized within a try-except block.
+
+    Parameters
+    ----------
+    decorator: ast.AST
+        An ast node that is expected to correspond to a decorator
+
+    Raises
+    ------
+    ValueError
+        If the node cannot be parsed as a PythonDeprecation
+
+    """
+
+    def __init__(self, decorator: ast.AST):
+        if isinstance(decorator, ast.Call):
+            try:
+                func: ast.Name = decorator.func
+                name: str = func.id
+                if not name == "deprecated":
+                    raise ValueError("Decorator does not follow Python Deprecation format")
+                keyword_dict = dict()
+                keywords: List[ast.keyword] = decorator.keywords
+                for keyword in keywords:
+                    try:
+                        arg = keyword.arg
+                        value = keyword.value.s
+                        keyword_dict[arg] = value
+                    except AttributeError:
+                        pass
+                self.deprecated_in = keyword_dict.get("deprecated_in", None)
+                self.removed_in = keyword_dict.get("removed_in", None)
+            except AttributeError:
+                raise ValueError("deprecation decorator must have \'func\' and \'name\' fields")
+        else:
+            raise ValueError("not a deprecation decorator")
+
+    def check_error(self, current_version: VersionNumber) -> Optional[str]:
+        """Return a user-facing error message if the deprecation is invalid.
+
+        In order to be valid, a PythonDeprecation must have fields 'deprecated_in'
+        and 'removed_in' both specified. Additionally, the current version of the software
+        must be less than the expected removal version.
+
+        Parameters
+        ----------
+        current_version: VersionNumber
+            the current version of the software, against which to check the deprecation
+
+        """
+        if self.deprecated_in is None or self.removed_in is None:
+            return "Both 'deprecated_in' and 'removed_in' must be specified"
+        try:
+            removed_version = VersionNumber(self.removed_in)
+            if current_version >= removed_version:
+                return "Current version, {}, exceeds expected removal version, {}" \
+                    .format(current_version.version, removed_version.version)
+        except ValueError as e:
+            return f"Encountered an exception when parsing a version number: {e}"
+
+
+class WrappedDeprecation:
+    """A wrapper class that combines a deprecation and information about the node that contains it.
+
+    This is useful for creating coherent error messages.
+
+    Parameters
+    ----------
+    name: str
+        the name of the method or class that has the deprecation decorator
+    deprecation: PythonDeprecation
+        a representation of the deprecation decorator
+    """
+
+    def __init__(self, name: str, deprecation: PythonDeprecation):
+        self.name = name
+        self.deprecation = deprecation
+
+    def check_error(self, current_version: VersionNumber) -> Optional[str]:
+        """Return an error message, if the deprecation is invalid for the current version.
+
+        Parameters
+        ----------
+        current_version: VersionNumber
+            the current version of the software, against which to check the deprecation
+
+        Returns
+        -------
+        Optional[str]
+            A human-readable error message, if the deprecation is invalid for this version
+
+        """
+        deprecation_error = self.deprecation.check_error(current_version)
+        if deprecation_error is not None:
+            return "{}: {}".format(self.name, deprecation_error)
