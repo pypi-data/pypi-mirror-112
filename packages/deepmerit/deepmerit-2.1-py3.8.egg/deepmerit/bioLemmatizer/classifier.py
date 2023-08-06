18ac32efc8e9b14bfb169ed9090aabd7d2b1bb1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:46 +0000

    Add package deepmerit/2.1/deepmerit-2.1-py3.8.egg

---
+++ packages/deepmerit/deepmerit-2.1-py3.8.egg/deepmerit/bioLemmatizer/classifier.py
@@ -0,0 +1,150 @@
+from pytorch_pretrained_bert.tokenization import BertTokenizer, WordpieceTokenizer
+from pytorch_pretrained_bert.modeling import BertForPreTraining, BertPreTrainedModel, BertModel, BertConfig, BertForMaskedLM, BertForSequenceClassification
+from pathlib import Path
+import torch
+import re
+from torch import Tensor
+from torch.nn import BCEWithLogitsLoss
+from fastai.text import Tokenizer, Vocab
+import pandas as pd
+from difflib import SequenceMatcher
+import string
+import collections
+import os
+import pdb
+from tqdm import tqdm, trange
+import sys
+import random
+import numpy as np
+from sklearn.model_selection import train_test_split
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+import logging
+from sklearn.metrics import roc_curve, auc
+
+
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data.distributed import DistributedSampler
+from pytorch_pretrained_bert.optimization import BertAdam
+
+class BertForMultiLabelSequenceClassification(BertPreTrainedModel):
+    def __init__(self, config, num_labels=2):
+        super(BertForMultiLabelSequenceClassification, self).__init__(config)
+        self.num_labels = num_labels
+        self.bert = BertModel(config)
+        self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = torch.nn.Linear(config.hidden_size, num_labels)
+        self.apply(self.init_bert_weights)
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None):
+        _, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
+        pooled_output = self.dropout(pooled_output) # Apply dropout on BERT output
+        logits = self.classifier(pooled_output) # Linear layer stacked on top of BERT for classification
+
+        if labels is not None:
+            loss_fct = BCEWithLogitsLoss() # Use Binary Cross entropy for multilabel task.
+            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1, self.num_labels))
+            return loss
+        else:
+            return logits
+        
+    def freeze_bert_encoder(self):
+        for param in self.bert.parameters():
+            param.requires_grad = False
+    
+    def unfreeze_bert_encoder(self):
+        for param in self.bert.parameters():
+            param.requires_grad = True
+
+class DataProcessor(object):
+    """Base class for data converters for sequence classification data sets."""
+
+    def get_train_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the train set."""
+        raise NotImplementedError()
+
+    def get_dev_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the dev set."""
+        raise NotImplementedError()
+    
+    def get_test_examples(self, data_dir, data_file_name, size=-1):
+        """Gets a collection of `InputExample`s for the dev set."""
+        raise NotImplementedError() 
+
+    def get_labels(self):
+        """Gets the list of labels for this data set."""
+        raise NotImplementedError()
+        
+class MultiLabelTextProcessor(DataProcessor):
+    
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.labels = None
+    
+    
+    def get_train_examples(self, data_dir, size=-1):
+        filename = 'train.csv'
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir, filename)))
+        if size == -1:
+            data_df = pd.read_csv(os.path.join(data_dir, filename))
+            return self._create_examples(data_df, "train")
+        else:
+            data_df = pd.read_csv(os.path.join(data_dir, filename))
+            return self._create_examples(data_df.sample(size), "train")
+        
+    def get_dev_examples(self, data_dir, size=-1):
+        """See base class."""
+        filename = 'val.csv'
+        if size == -1:
+            data_df = pd.read_csv(os.path.join(data_dir, filename))
+            return self._create_examples(data_df, "dev")
+        else:
+            data_df = pd.read_csv(os.path.join(data_dir, filename))
+            return self._create_examples(data_df.sample(size), "dev")
+    
+    def get_test_examples(self, data_dir, data_file_name, size=-1):
+        data_df = pd.read_csv(os.path.join(data_dir, data_file_name))
+        if size == -1:
+            return self._create_examples(data_df, "test")
+        else:
+            return self._create_examples(data_df.sample(size), "test")
+
+    def get_labels(self):
+        """See base class."""
+        if self.labels == None:
+            self.labels = list(pd.read_csv(os.path.join(self.data_dir, "classes.txt"),header=None).values[0])
+        return self.labels
+
+    def _create_examples(self, df, set_type, labels_available=True):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, row) in enumerate(df.values):
+            guid = row[0]
+            text_a = row[1]
+            if labels_available:
+                labels = row[2:]
+            else:
+                labels = []
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, labels=labels))
+        return examples
+    
+class InputExample(object):
+
+    def __init__(self, guid, text_a, text_b=None, labels=None):
+        
+        self.guid = guid
+        self.text_a = text_a
+        self.text_b = text_b
+        self.labels = labels
+
+
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(self, input_ids, input_mask, segment_ids, label_ids):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_ids = label_ids
