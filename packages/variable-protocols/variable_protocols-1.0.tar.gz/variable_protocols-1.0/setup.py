63adef6a11405c0c64a0ddba31b3df7f90df7aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:45 +0000

    Add package variable-protocols/1.0/variable_protocols-1.0.tar.gz

---
+++ packages/variable-protocols/variable_protocols-1.0.tar.gz/variable_protocols-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='variable_protocols',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/cwhy/variable_protocols',
+    license='GPL v3',
+    author='cwhy',
+    author_email='chenyu.nus@gmail.com',
+    description='A protocal for machine learning variables'
+)
