0f23fe3331d4ccc4a57e4b7a12fc71e2f2836b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:05 +0000

    Add package amba-event-stream/0.0.10/amba-event-stream-0.0.10.tar.gz

---
+++ packages/amba-event-stream/amba-event-stream-0.0.10.tar.gz/amba-event-stream-0.0.10/event_stream/event_stream_producer.py
@@ -0,0 +1,26 @@
+import logging
+
+from kafka import KafkaProducer
+
+from .event_stream_base import EventStreamBase
+
+
+class EventStreamProducer(EventStreamBase):
+    # active_producer = {"bla": "stuff"}
+    producer = False
+    log = "a EventStreamProducer"
+
+    def publish(self, event):
+        topic_event = self.get_topic_name_event(event)
+
+        if not self.producer:
+            self.create_producer()
+
+        value = event.get_json()
+        # logging.warning(self.log + "v %s" % value)
+        self.producer.send(topic_event, value=value.encode('utf-8'))
+        self.producer.flush()
+        logging.warning(self.log + 'Message published successfully to topic %s' % topic_event)
+
+    def create_producer(self):
+        self.producer = KafkaProducer(bootstrap_servers=self.bootstrap_servers, api_version=self.api_version)
