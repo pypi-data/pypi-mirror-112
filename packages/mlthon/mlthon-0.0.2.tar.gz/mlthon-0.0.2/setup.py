de5ce49d7ec03be390104f7fd34e53b15bb689e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:48 +0000

    Add package mlthon/0.0.2/mlthon-0.0.2.tar.gz

---
+++ packages/mlthon/mlthon-0.0.2.tar.gz/mlthon-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.0.2'
+
+setup(
+    name="mlthon",
+    version=__version__,
+    description="This package makes machine learning easier!",
+    author="Pritish Mishra",
+    author_email="pritishjan@gmail.com",
+    url="https://github.com/pritishmishra703/MLthon.git",
+    download_url="https://github.com/pritishmishra703/MLthon/tarball/master",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib']
+)
