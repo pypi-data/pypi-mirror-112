40a4afd07818bc5508c6d632636cfdbedeb73f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:10 +0000

    Add package gladpy/0.0.2/gladpy-0.0.2.tar.gz

---
+++ packages/gladpy/gladpy-0.0.2.tar.gz/gladpy-0.0.2/src/gladpy/gladpy.py
@@ -0,0 +1,30 @@
+def getLength(object):
+    Length = len(object)
+    return Length
+def anchorStr(object, name):
+    ANCHORED = "<a name='" + name + "'>" + object + "</a>"
+    return ANCHORED
+def Multiply(integer, multi):
+    MULTI = integer * multi
+    return MULTI
+def Add(integer, add):
+    ADD = integer + add
+    return ADD
+def Subtract(integer, subt):
+    SUBT = integer - subt
+    return SUBT
+def Divide(integer, div):
+    DIV = integer / div
+    return DIV
+def bigStr(string):
+    BIG = "<big>" + string + "</big>"
+    return BIG
+def boldStr(string):
+    BOLD = "<b>" + string + "</b>"
+    return BOLD
+def blinkStr(string):
+    BLINK = "<blink>" + string + "</blink>"
+    return BLINK
+def getCharStr(string, position):
+    CHAR = string[position]
+    return CHAR
