e52b15402dc73ab8b21523e855c67396e5d2a665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:46 +0000

    Add package legal-pre-processing/0.1.0/legal_pre_processing-0.1.0-py3-none-any.whl

---
+++ packages/legal-pre-processing/legal_pre_processing-0.1.0-py3-none-any.whl/legal_pre_processing/load_data.py
@@ -0,0 +1,21 @@
+import json
+from typing import NewType
+from glob import glob
+
+DictsPathType = NewType('DictsPath', str)
+
+def open_file_json(path):
+    with open(path,'r') as f:
+        return json.load(f)
+
+class LoadDicts:
+    def __init__(self, dict_path: DictsPathType = './data'):
+        Dicts_glob = glob(f"{dict_path}/*.json")
+        self.List = []
+        self.Dict = {}
+        for path_json in Dicts_glob:
+            name = path_json.split('/')[-1].replace('.json','')
+            self.List.append(name)
+            self.Dict[name] = open_file_json(path_json)
+            setattr(self, name, self.Dict[name])
+
