362d7a677256c645c41a2e73c267365411b742d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:12 +0000

    Add package lol-dto/2.0.0a2/lol_dto-2.0.0a2.tar.gz

---
+++ packages/lol-dto/lol_dto-2.0.0a2.tar.gz/lol_dto-2.0.0a2/lol_dto/classes/game/__init__.py
@@ -0,0 +1,21 @@
+from lol_dto.classes.game.lol_game import LolGame, LolGameTeams, LolPickBan
+from lol_dto.classes.game.lol_game_team import LolGameTeam, LolGameTeamEndOfGameStats
+from lol_dto.classes.game.lol_game_player import (
+    LolGamePlayer,
+    LolGamePlayerRune,
+    LolGamePlayerItem,
+    LolGamePlayerSnapshot,
+    LolGamePlayerSummonerSpell,
+    LolGamePlayerEndOfGameStats,
+    LolGamePlayerLargeMonsterKill
+)
+from lol_dto.classes.game.lol_game_event import (
+    LolEvent,
+    LolGameKill,
+    LolGameTeamEpicMonsterKill,
+    LolGameTeamBuildingKill,
+    LolGamePlayerWardEvent,
+    LolGamePlayerItemEvent,
+    LolGamePlayerSkillLevelUpEvent,
+)
+from lol_dto.classes.game.position import Position
