8422dfeee1a2e699f41d5046eae2fc19a58a3040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:37 +0000

    Add package trading-com-dados/0.0.1/trading_com_dados-0.0.1.tar.gz

---
+++ packages/trading-com-dados/trading_com_dados-0.0.1.tar.gz/trading_com_dados-0.0.1/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='trading_com_dados',
+    version='0.0.1',
+    license='MIT License',
+    author='Fabio Minutti Teixeira',
+    long_description=readme,
+    author_email='fabiomt92@hotmail.com',
+    keywords='',
+    description='Pacote trading com dados teste',
+    packages=['trading_com_dados'],
+    install_requires=[],)
+
+        
