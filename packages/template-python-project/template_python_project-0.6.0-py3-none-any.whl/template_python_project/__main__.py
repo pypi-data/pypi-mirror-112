7bcf0e5f8791c23eec2f97aa56911bcb68532cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:31 +0000

    Add package template-python-project/0.6.0/template_python_project-0.6.0-py3-none-any.whl

---
+++ packages/template-python-project/template_python_project-0.6.0-py3-none-any.whl/template_python_project/__main__.py
@@ -0,0 +1,6 @@
+# pylint: skip-file
+
+from . import main
+
+if __name__ == '__main__':
+    main.main()
---
+++ packages/template-python-project/template_python_project-0.6.0-py3-none-any.whl/template_python_project/core/compute.py
@@ -0,0 +1,5 @@
+def multiply(value1, value2):
+    return value1 * value2
+
+def add(value1, value2):
+    return value1 + value2
