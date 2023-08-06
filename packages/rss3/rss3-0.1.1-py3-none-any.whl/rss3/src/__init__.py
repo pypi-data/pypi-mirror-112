d58875d9a7cfa289bec8c55f9437ec65160a2cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:23 +0000

    Add package rss3/0.1.1/rss3-0.1.1-py3-none-any.whl

---
+++ packages/rss3/rss3-0.1.1-py3-none-any.whl/rss3/src/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py.py    
+@Contact :   leetao94cn@gmail.com
+@Description：
+@Modify Time      @Author    @Version    @Description
+------------      -------    --------    -----------
+2021/6/24 9:02 下午   leetao      1.0         None
+"""
+
+# import lib
