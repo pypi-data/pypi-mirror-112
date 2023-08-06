66b9e9c3332d1ded73be2f3a6f5d60698f2fcf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:21 +0000

    Add package sunnah-api/0.3.1/sunnah-api-0.3.1.tar.gz

---
+++ packages/sunnah-api/sunnah-api-0.3.1.tar.gz/sunnah-api-0.3.1/sunnah_api/__init__.py
@@ -0,0 +1,12 @@
+from .adaptor import (
+    ApiAdaptor,
+    ApiError,
+)
+from .base import (
+    CollectionGroup,
+    BookGroup,
+    ChapterGroup,
+    Collection,
+    Book,
+    HadithGroup,
+)
