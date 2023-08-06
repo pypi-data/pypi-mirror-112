24b0456ee1b473c435d268f7231417e52311c1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:36 +0000

    Add package lbcgo/0.1.3/LBCgo-0.1.3-py3-none-any.whl

---
+++ packages/lbcgo/LBCgo-0.1.3-py3-none-any.whl/lbcgo/__init__.py
@@ -0,0 +1,7 @@
+"""LBCgo: Data reduction package for the Large Binocular Telescope's Large Binocular Camera (LBC).
+
+Requires astromatic.net software sextractor, scamp, swarp."""
+__version__ = '0.1.3'
+
+from .lbcproc import *
+from .lbcregister import *
