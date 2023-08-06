64b400e61a0706f17182f0f0bcf193cfb863278b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:01 +0000

    Add package femagroundmotions/2.0.0/femagroundmotions-2.0.0.tar.gz

---
+++ packages/femagroundmotions/femagroundmotions-2.0.0.tar.gz/femagroundmotions-2.0.0/src/femagroundmotions/__init__.py
@@ -0,0 +1,7 @@
+import importlib.resources
+
+__version__ = importlib.resources.read_text(__name__, '__version__')
+
+from .atcparse import AtcParser
+from .hdf5 import from_hdf5, to_hdf5
+from .spectra import ResponseSpectrumBuilder
