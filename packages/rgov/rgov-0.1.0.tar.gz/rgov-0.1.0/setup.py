ce830827a521a0db529276f9e8f859a1d12a38d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:42 +0000

    Add package rgov/0.1.0/rgov-0.1.0.tar.gz

---
+++ packages/rgov/rgov-0.1.0.tar.gz/rgov-0.1.0/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rgov', 'rgov.commands']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cleo>=0.8.1,<0.9.0',
+ 'fake-useragent>=0.1.11,<0.2.0',
+ 'grequests>=0.6.0,<0.7.0',
+ 'html5lib>=1.1,<2.0',
+ 'lxml>=4.6.3,<5.0.0',
+ 'numpy>=1.21.0,<2.0.0',
+ 'python-daemon>=2.3.0,<3.0.0',
+ 'python-language-server>=0.36.2,<0.37.0',
+ 'python-lsp-server>=1.0.1,<2.0.0',
+ 'requests>=2.25.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['rgov = rgov.application:main']}
+
+setup_kwargs = {
+    'name': 'rgov',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Jordan Sweet',
+    'author_email': 'jsbmgcontact@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
