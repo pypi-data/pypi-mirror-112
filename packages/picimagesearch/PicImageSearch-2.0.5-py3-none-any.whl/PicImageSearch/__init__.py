1db535c66fd77835bc204abdaa397d400a2b9c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:26 +0000

    Add package picimagesearch/2.0.5/PicImageSearch-2.0.5-py3-none-any.whl

---
+++ packages/picimagesearch/PicImageSearch-2.0.5-py3-none-any.whl/PicImageSearch/__init__.py
@@ -0,0 +1,11 @@
+from .saucenao import SauceNAO
+from .tracemoe import TraceMoe
+from .ascii2d import Ascii2D
+from .iqdb import Iqdb
+from .google import Google
+from .baidu import BaiDu
+
+__author__ = 'kitUIN'
+__license__ = 'MIT License'
+__contributors__ = ['kitUIN', 'lleans']
+__email__ = 'kulujun@gmail.com'
