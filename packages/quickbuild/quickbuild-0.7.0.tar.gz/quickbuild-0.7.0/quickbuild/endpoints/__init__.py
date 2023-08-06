bce5a99d571ec008afc19bc20c3c863b3cb30eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:05 +0000

    Add package quickbuild/0.7.0/quickbuild-0.7.0.tar.gz

---
+++ packages/quickbuild/quickbuild-0.7.0.tar.gz/quickbuild-0.7.0/quickbuild/endpoints/__init__.py
@@ -0,0 +1,25 @@
+from .agents import Agents
+from .audits import Audits
+from .builds import Builds
+from .configurations import Configurations
+from .dashboards import Dashboards
+from .groups import Groups
+from .memberships import Memberships
+from .requests import Requests
+from .system import System
+from .tokens import Tokens
+from .users import Users
+
+__all__ = (
+    'Agents',
+    'Audits',
+    'Builds',
+    'Configurations',
+    'Dashboards',
+    'Groups',
+    'Memberships',
+    'Requests',
+    'System',
+    'Tokens',
+    'Users',
+)
