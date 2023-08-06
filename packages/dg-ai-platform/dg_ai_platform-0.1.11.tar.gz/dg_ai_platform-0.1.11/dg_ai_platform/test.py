334b5b1a4965265e3f3ca095b4c60a07b34cf4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:33 +0000

    Add package dg-ai-platform/0.1.11/dg_ai_platform-0.1.11.tar.gz

---
+++ packages/dg-ai-platform/dg_ai_platform-0.1.11.tar.gz/dg_ai_platform-0.1.11/dg_ai_platform/test.py
@@ -0,0 +1,5 @@
+from dg_ai_platform.example import ExampleTask
+from dg_ai_platform.dg_platform import CaldronAI
+
+ca = CaldronAI('a9ac600fd2c33b6fae5a16769bce7a0c', 'ewj321hjewqk', ExampleTask)
+ca.run()
