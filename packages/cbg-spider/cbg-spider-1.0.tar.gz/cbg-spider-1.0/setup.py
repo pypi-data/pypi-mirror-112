162c4c9dbf8d7ca8b8aebaea87c13da8a426213a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:54 +0000

    Add package cbg-spider/1.0/cbg-spider-1.0.tar.gz

---
+++ packages/cbg-spider/cbg-spider-1.0.tar.gz/cbg-spider-1.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+ 
+VERSION ='1.0'
+ 
+from setuptools import setup
+ 
+setup(
+    name='cbg-spider',
+    version=VERSION,
+    author='Albus McGonagall',
+    author_email='cabbagelang@hotmail.com',
+    packages=['./'],
+        scripts=[],
+        package_data={},
+        install_requires=[],
+    )
