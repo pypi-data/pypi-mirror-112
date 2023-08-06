f90a40da46c0a61cae8a6f597ecc978757b33f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:40 +0000

    Add package editorconfig-checker/2.3.51/editorconfig_checker-2.3.51-py3-none-any.whl

---
+++ packages/editorconfig-checker/editorconfig_checker-2.3.51-py3-none-any.whl/editorconfig_checker/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# FIXME: Align this version with the one in the core.
+#        `51` patch number was used as workaround to keep `2.3.6` valid.
+__version__ = '2.3.51'
