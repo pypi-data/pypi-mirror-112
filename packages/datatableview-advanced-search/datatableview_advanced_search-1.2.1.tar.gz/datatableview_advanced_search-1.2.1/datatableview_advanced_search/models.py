4d230bee9c16b6a2764050c8b1b359935aa29210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:15 +0000

    Add package datatableview-advanced-search/1.2.1/datatableview_advanced_search-1.2.1.tar.gz

---
+++ packages/datatableview-advanced-search/datatableview_advanced_search-1.2.1.tar.gz/datatableview_advanced_search-1.2.1/datatableview_advanced_search/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""models.py: Django datatableview_advanced_search"""
+
+import logging
+
+__author__ = 'Steven Klass'
+__date__ = '3/1/18 9:22 AM'
+__copyright__ = 'Copyright 2018 IC Manage. All rights reserved.'
+__credits__ = ['Steven Klass', ]
+
+log = logging.getLogger(__name__)
+
