279b9d658a4b8e6200fe531087c79acd0b250d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:20 +0000

    Add package batmouse/0.1.0/batmouse-0.1.0.tar.gz

---
+++ packages/batmouse/batmouse-0.1.0.tar.gz/batmouse-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['batmouse']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'batmouse',
+    'version': '0.1.0',
+    'description': 'Python library for querying mouse battery levels.',
+    'long_description': None,
+    'author': 'Tarcísio Eduardo Moreira Crocomo',
+    'author_email': 'tarcisioe@pm.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
